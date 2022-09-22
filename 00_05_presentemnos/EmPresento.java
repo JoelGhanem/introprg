6b4f6f987de0a937be02241368b34264a784f576
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-09-22 17:02:42 +0200

    he enviat una versió en català abans d'adonar-me de que el teu sistema me'l donaría per nul  ∾ prgtest resultat 00_04: FALLA

---
+++ 00_05_presentemnos/EmPresento.java
@@ -0,0 +1,6 @@
+/*Programa que saluda am Món*/
+public class EmPresento {
+    public static void main(String[]args) {
+            System.out.println("Hola Món, sóc el futur millor desenvolupador del Món");
+    }
+}
