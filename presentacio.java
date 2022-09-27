265089e45bd1998770c9c4179ffc9dc89eee1b7b
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-09-27 20:10:18 +0200

    Exercici incialització ben fet

--- presentacio.java
+++
@@ -1,8 +0,0 @@
-
-
-/*Programa que saluda am Món*/
-public class EmPresento {
-    public static void main(String[]args) {
-            System.out.println("Hola Món, sóc el futur millor desenvolupador del Món");
-    }
-}
