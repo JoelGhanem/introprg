ab18a59dcde7077592ef2219d331eca1bbc97f9f
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-09-29 19:02:10 +0200

    31_15_primera prova  ∾ prgtest autocommit 31_15 #7

--- 31_15_dosenordre/DosEnOrdre.java
+++ 31_15_dosenordre/DosEnOrdre.java
@@ -3,13 +3,13 @@
  */
 public class DosEnOrdre {
     public static void main(String[] args) {
-        System.out.println("Primer?");
+        System.out.println("Primer");
         int primer = Integer.parseInt(Entrada.readLine());
 
 	System.out.println("Segon?");
 	int segon = Integer.parseInt(Entrada.readLine());
 
-	if (segon >= primer) {
+	if (segon > primer) {
         	System.out.println(segon + " i " + primer);
         } else {
 		System.out.println(primer + " i " + segon);
