04501c3c437f304f619e33e7d984c131fb112088
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-09-29 17:29:02 +0200

    31_15_primera prova

---
+++ 31_15_dosenordre/DosEnOrdre.java
@@ -0,0 +1,17 @@
+/*
+ * Programa que ordena dos nombres
+ */
+public class DosEnOrdre {
+    public static void main(String[] args) {
+        System.out.println("Primer?");
+        int primer = Integer.parseInt(Entrada.readLine());
+
+	System.out.println("Segon?");
+	int segon = Integer.parseInt(Entrada.readLine());
+
+        System.out.println(primer + " i " + segon);
+                 /*condicional amb if i else mostra el missatge corresponent.*/
+                 
+		
+    }
+}
