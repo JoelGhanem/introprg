4b7a440db798ac4c1daf42cd946106dc8f9bc5fa
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-10-01 14:28:56 +0200

    31_16_fet

--- 31_16_parellsenar/ParellSenar.java
+++ 31_16_parellsenar/ParellSenar.java
@@ -1,3 +1,8 @@
+/*
+*Programa que indica si el numero proporcionat com a primer argument es parell o senar.
+si no s especifica cap num el resultat es indeterminat
+*/
+
 public class ParellSenar {
     public static void main(String[] args) {
         int numero = Integer.parseInt(args[0]);
