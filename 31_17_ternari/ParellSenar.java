02ca05e44afbca3d16ef50786cccd9f221697d3e
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-10-01 14:58:30 +0200

    31_17_fet

--- 31_17_ternari/ParellSenar.java
+++ 31_17_ternari/ParellSenar.java
@@ -6,16 +6,11 @@
   public class ParellSenar {
       public static void main(String[] args) {
           int numero = Integer.parseInt(args[0]);
-          if (numero %2 == 0) {
-              System.out.println("El número " + numero + " és parell");
-          
-            string numero = (numero %2 == 0) ? : "El número és parell" : "El número és senar";
+           String ParellOSenar = (numero %2 == 0) ? "El número és parell" : "El número és senar";
           }
   
-      }
-  }
-~                                                                                                                                                                                                                  
-~                                                                                                                                                                                                                  
-~                                                                                                                                                                                                                  
-~                                                                                                                                                                                                                  
-~                                 
+      }                                                                 
+                                                                                                                                                                                                                  
+                                                                                                                                                                                                                  
+                                                                                                                                                                                                                  
+                                
