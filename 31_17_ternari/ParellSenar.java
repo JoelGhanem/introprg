5f1357d17122436d509191a9e64e2df34be16631
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-01 14:42:19 +0200

    31_16_fet  ∾ prgtest resultat 31_03: PASSA

---
+++ 31_17_ternari/ParellSenar.java
@@ -0,0 +1,21 @@
+ /*
+ *Programa que indica si el numero proporcionat com a primer argument es parell o senar.
+ si no s especifica cap num el resultat es indeterminat
+ */
+ 
+  public class ParellSenar {
+      public static void main(String[] args) {
+          int numero = Integer.parseInt(args[0]);
+          if (numero %2 == 0) {
+              System.out.println("El número " + numero + " és parell");
+          
+            string numero = (numero %2 == 0) ? : "El número és parell" : "El número és senar";
+          }
+  
+      }
+  }
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                 
