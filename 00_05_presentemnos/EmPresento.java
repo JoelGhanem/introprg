0edcad099f43c8f83aeff203a5b7462bfbfcd5f4
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-10-02 02:37:53 +0200

    00_05_fet

--- 00_05_presentemnos/EmPresento.java
+++ 00_05_presentemnos/EmPresento.java
@@ -1,4 +1,4 @@
-/*Programa que saluda am Món*/
+/*Programa que saluda am Món.*/
 public class EmPresento {
     public static void main(String[]args) {
             System.out.println("Hola Món, sóc el futur millor desenvolupador del Món");
