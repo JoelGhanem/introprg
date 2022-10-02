fded4509d498799720ff7b54c690b96f0f9ed9cf
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-02 15:46:00 +0200

    31_20_fet  ∾ prgtest autocommit 31_20 #1

--- 31_20_diessetmana_switch/DiaSetmanaSwitchExpr.java
+++ 31_20_diessetmana_switch/DiaSetmanaSwitchExpr.java
@@ -8,28 +8,34 @@
             switch (dia) {
 
                 case 1: 
-                   System.out.println("Dilluns");
+                    System.out.println("Dilluns");
+                        break;
                         
                 case 2: 
-                   System.out.println("Dimarts");
+                    System.out.println("Dimarts");
+                        break;
                        
                 case 3:
-                   System.out.println("Dimecres");
+                    System.out.println("Dimecres");
+                        break;
                 
                 case 4: 
-                System.out.println("Dijous");
+                    System.out.println("Dijous");
+                        break;
                  
                 case 5:
-                System.out.println("Divendres");
+                    System.out.println("Divendres");
+                        break;
                        
                 case 6:
-                   System.out.println("Dissabtes");
+                    System.out.println("Dissabtes");
+                        break;
                        
                 case 7:
-                   System.out.println("Diumenge");
-                    
+                    System.out.println("Diumenge");
+                        break;
                 default:
-                System.out.println("Error");
+                    System.out.println("Error");
                         
         }
     }
