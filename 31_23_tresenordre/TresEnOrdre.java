d9c79fc57b33a198339229818140a417b5e0fea6
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-03 18:19:59 +0200

    31_23_fet

--- 31_23_tresenordre/TresEnOrdre.java
+++ 31_23_tresenordre/TresEnOrdre.java
@@ -3,38 +3,38 @@ public class TresEnOrdre {
         System.out.println("Primer?");
         int primer = Integer.parseInt(Entrada.readLine());
         
-        System.out.println("Segonr?");
+        System.out.println("Segon?");
         int segon = Integer.parseInt(Entrada.readLine());
 
         System.out.println("Tercer?");
         int tercer = Integer.parseInt(Entrada.readLine());
 
     if (primer > segon && segon > tercer) {
-        System.out.println(tercer + "," + segon + " i " + primer);
+        System.out.println(tercer + ", " + segon + " i " + primer);
 
         }
 
     else if (primer > tercer && tercer > segon) {
-        System.out.println(segon + "," + tercer + " i " + primer);
+        System.out.println(segon + ", " + tercer + " i " + primer);
 
         }
 
 
     else if (segon > primer && primer > tercer) {
-        System.out.println(tercer + "," + primer + " i " + segon);
+        System.out.println(tercer + ", " + primer + " i " + segon);
 
         }
     else if (segon > tercer && tercer > primer) {
-        System.out.println(primer + "," + tercer + " i " + segon);
+        System.out.println(primer + ", " + tercer + " i " + segon);
 
         }
 
     else if (tercer > primer && primer > segon) {
-        System.out.println(segon + "," + primer + " i " + tercer);
+        System.out.println(segon + ", " + primer + " i " + tercer);
 
         }  
     else if (tercer > segon && segon > primer) {
-        System.out.println(primer + "," + segon + " i " + tercer);
+        System.out.println(primer + ", " + segon + " i " + tercer);
         }
     }
 }
