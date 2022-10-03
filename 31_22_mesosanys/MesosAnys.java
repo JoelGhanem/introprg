7daa5fb0799f2a0841593f1a6974f5e7f21ce69e
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-03 22:58:47 +0200

    Merge branch 'main' of github.com:JoelGhanem/introprg  ∾ prgtest autocommit 31_22 #0

--- 31_22_mesosanys/MesosAnys.java
+++ 31_22_mesosanys/MesosAnys.java
@@ -12,7 +12,7 @@
                 int mesAnterior = mes - 1;
 
                 int mesSeguent = mes + 1; 
-                System.out.println("Mes anterior " + mesAnterior  + "/" + any + " i mes següent " + mesSeguent +"/" + any); 
+                System.out.println("Mes anterior " + mesAnterior  + "/" + any + " i mes següent " + mesSeguent + "/" + any); 
     }
 }
                                                                                                                                                                                                                
