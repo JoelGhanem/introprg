4621a42c56c57629e6f900fa119f18d55944fa7f
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-10-04 13:07:52 +0200

    31_25_fet

---
+++ 31_25_pagaments/Pagament.java
@@ -0,0 +1,23 @@
+/*Programa que et demana el preu i et diu la diferencia de preus entre el que li has donat i el que has de pagar*/
+
+public class Pagament {
+    public static void main(String[] args) {
+    System.out.println("Preu?");
+    int preu = Integer.parseInt(Entrada.readLine());
+    System.out.println("paga?");
+    int paga = Integer.parseInt(Entrada.readLine());
+    int diferencia = preu - paga;
+    boolean diferenciaNegativa = paga > preu;
+    if (diferencia == 0) {
+        System.out.println("No sobra ni falta res");
+    } else if (diferencia > 0) {
+        System.out.println("Falten " + diferencia + "€");
+    } else if (diferenciaNegativa) {
+        System.out.println("sobren " + diferenciaNegativa + "€");
+    }
+    }   
+}
+
+
+
+
