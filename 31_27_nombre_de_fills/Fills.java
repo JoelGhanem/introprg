e49fcaccb7ac1fde3dc18330cf75aca0f1300f77
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-06 15:55:09 +0200

    31_27_fet

---
+++ 31_27_nombre_de_fills/Fills.java
@@ -0,0 +1,9 @@
+/*Programa que et demana quants fills tens i et respon segons el nombre*/
+public class Fills {
+    public static void main(String[] args) {
+    int numFills = Integer.parseInt(Entrada.readLine());
+    if (numFills == 1) {
+        System.out.println("Compte de no mimar-lo massa!");
+    } 
+}
+}
