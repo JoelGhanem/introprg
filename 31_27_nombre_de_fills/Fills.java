e5b3f4337088eb95cd673448e35f6a79a90d64fb
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-07 21:43:17 +0200

    31_27_fet  ∾ prgtest autocommit 31_27 #8

--- 31_27_nombre_de_fills/Fills.java
+++ 31_27_nombre_de_fills/Fills.java
@@ -1,30 +1,30 @@
 /*Programa que et demana quants fills tens i et respon segons el nombre*/
 public class Fills {
     public static void main(String[] args) {
-    System.out.println("Quants fills tens?");
-    int numFills = Integer.parseInt(Entrada.readLine());
-    switch (numFills) {
-        case -1:
-            System.out.println("No pots tenir menys de 0 fills!");
+        System.out.println("Quants fills tens?");
+        int numFills = Integer.parseInt(Entrada.readLine());
+        switch (numFills) {
+            case -1:
+                System.out.println("No pots tenir menys de 0 fills!");
                 break;
-        case 0:
-            System.out.println("Tot el que t'has estalviat en bolquers!");
+            case 0:
+                System.out.println("Tot el que t'has estalviat en bolquers!");
                 break;
-        case 1:
-            System.out.println("Compte de no mimar-lo massa!");
+            case 1:
+                System.out.println("Compte de no mimar-lo massa!");
                 break;
-        case 2:
-            System.out.println("No t'avorreixes a casa, eh?");
+            case 2:
+                System.out.println("No t'avorreixes a casa, eh?");
                 break;
-        case 4: 
-            System.out.println("No t'avorreixes a casa, eh?");
+            case 4: 
+                System.out.println("No t'avorreixes a casa, eh?");
                 break;
-        case 5: 
-            System.out.println("Tu sí fas país!");
+            case 5: 
+                System.out.println("Tu sí fas país!");
                 break;
-        case 6: 
-            System.out.println("Tu sí fas país!");
+            case 6: 
+                System.out.println("Tu sí fas país!");
                 break;
-}    
-}
+        }    
+    }
 }
