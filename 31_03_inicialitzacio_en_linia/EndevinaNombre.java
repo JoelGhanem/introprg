9b8917a6605a100e3989ff35a2a28a0f9c432670
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-09 00:55:58 +0200

    31_03_fet

--- 31_03_inicialitzacio_en_linia/EndevinaNombre.java
+++ 31_03_inicialitzacio_en_linia/EndevinaNombre.java
@@ -1,80 +1,41 @@
- /*
+/*
+ *
+ * Aquest programa es capaç d'endevinar
+ *
+ * Per executar, fes
+ * $java EndevinaNombre 42
+ */
 
-     * Aquest programa mostra capacitats d'endevinació
+public class EndevinaNombre {
+public static void main(String[] args) {
 
-     *
+int numeroPensat = Integer.parseInt(args[0]); // variable + declaració
+int numero = numeroPensat * 23; // inicialització de la variable numero
 
-     * Per executar-ho, fes
+// pensa un número
+System.out.print("El número pensat és ");
+System.out.println(numeroPensat);
 
-     * $ java EndevinaNombre 42
+// Surt la multiplicació
+System.out.print("Quan el multipliques per 3 obtens ");
+System.out.println(numero);
 
-     */
+// es suma 6
+numero = numero + 6;
+System.out.print("Quan li sumes 6 arriba a ");
+System.out.println(numero);
 
+// es divideix per 3
+numero = numero / 3;
+System.out.print("Un cop dividit per 3 queda ");
+System.out.println(numero);
 
-    public class EndevinaNombre {
+// es resta el número pensat
+numero = numero - numeroPensat;
+System.out.print("Finalment, en restar-li el valor inicial, queda ");
+System.out.println(numero);
 
-
-        public static void main(String[] args) {
-
-
-            int numeroPensat; 
-
-            int numero; 
-
-            numeroPensat = Integer.parseInt(args[0]); 
-
-            numero = numeroPensat * 3;
-               
-
-            // pensa un número
-
-            
-
-            System.out.print("El número pensat és ");
-
-            System.out.println(numeroPensat);
-
-
-            // Multiplica'l per 3
-
-            
-
-            System.out.print("Quan el multipliques per 3 obtens ");
-
-            System.out.println(numero);
-
-
-            // Suma-li 6
-
-            numero = numero + 6;
-
-            System.out.print("Quan li sumes 6 arriba a ");
-
-            System.out.println(numero);
-
-
-            // divideix-ho tot per 3
-
-            numero = numero / 3;
-
-            System.out.print("Un cop dividit per 3 queda ");
-
-            System.out.println(numero);
-
-
-            // resta-li el número pensat
-
-            numero = numero - numeroPensat;
-
-            System.out.print("Finalment, en restar-li el valor inicial, queda ");
-
-            System.out.println(numero);
-
-
-            // compara el resultat
-
-            System.out.println("A que el resultat és 2?");
-
-        }
-
-    }
+// compara el resultat
+System.out.print("A que el resultat és 2?");
+}
+}
