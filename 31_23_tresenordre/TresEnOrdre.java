86d2dcf3ca529af8006f72066010c3082f77af29
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-10 18:30:55 +0200

    31_23_maco  ∾ prgtest autocommit 31_23 #2

--- 31_23_tresenordre/TresEnOrdre.java
+++ 31_23_tresenordre/TresEnOrdre.java
@@ -1,45 +1,20 @@
-/* Programa que rep 3 numeros i tels retorna en ordre ascendent
-*/
-/* Declaracio de variables 
-*/
+/*Programa que et retorna tres numeros en ordre ascendent */
 public class TresEnOrdre {
     public static void main(String[] args) {
         System.out.println("Primer?");
-        int primer = Integer.parseInt(Entrada.readLine());
-        
-        System.out.println("Segon?");
-        int segon = Integer.parseInt(Entrada.readLine());
-
-        System.out.println("Tercer?");
-        int tercer = Integer.parseInt(Entrada.readLine());
-/* Part del programa que retorna els numeros en ordre :
-*/
-    if (primer > segon && segon > tercer) {
-        System.out.println(tercer + ", " + segon + " i " + primer);
-
-        }
-
-    else if (primer > tercer && tercer > segon) {
-        System.out.println(segon + ", " + tercer + " i " + primer);
-
-        }
-
-
-    else if (segon > primer && primer > tercer) {
-        System.out.println(tercer + ", " + primer + " i " + segon);
-
-        }
-    else if (segon > tercer && tercer > primer) {
-        System.out.println(primer + ", " + tercer + " i " + segon);
-
-        }
-
-    else if (tercer > primer && primer > segon) {
-        System.out.println(segon + ", " + primer + " i " + tercer);
-
-        }  
-    else if (tercer > segon && segon > primer) {
-        System.out.println(primer + ", " + segon + " i " + tercer);
-        }
+         int a = Integer.parseInt(Entrada.readLine());
+         System.out.println("Segon?");
+         int b = Integer.parseInt(Entrada.readLine());
+         System.out.println("Tercer?");
+         int c = Integer.parseInt(Entrada.readLine());
+         if (b < a) {
+         int temp = a;
+         b = a;
+         b = temp;
+         } 
+         System.out.println(a + b);
     }
 }
+
+
+
