94d47fffb038611e1e57239b1d9c4304438166fd
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-10 18:44:42 +0200

    31_23_maco  ∾ prgtest autocommit 31_23 #5

--- 31_23_tresenordre/TresEnOrdre.java
+++ 31_23_tresenordre/TresEnOrdre.java
@@ -2,16 +2,26 @@
 public class TresEnOrdre {
     public static void main(String[] args) {
         System.out.println("Primer?");
-         int a = Integer.parseInt(Entrada.readLine());
-         System.out.println("Segon?");
-         int b = Integer.parseInt(Entrada.readLine());
-         System.out.println("Tercer?");
-         int c = Integer.parseInt(Entrada.readLine());
-         if (b < a) {
-         int temp = a;
-         b = a;
-         b = temp;
-         } 
+        int a = Integer.parseInt(Entrada.readLine());
+        System.out.println("Segon?");
+        int b = Integer.parseInt(Entrada.readLine());
+        System.out.println("Tercer?");
+        int c = Integer.parseInt(Entrada.readLine());
+        if (b < a) {
+            int temp = a;
+            b = a;
+            b = temp;
+        }
+        if (c < a) {
+            int temp = a;
+            c = a;
+            c = temp;
+        }
+        if (b < a) {
+            int temp = a;
+            b = a;
+            a = temp;
+        }
          System.out.println(a + ", " + b +" i " + c);
     }
 }
