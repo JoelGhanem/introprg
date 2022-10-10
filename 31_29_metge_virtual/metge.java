4f05fd150c0d40a6560efab1f4456026de138c6c
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-10 03:27:38 +0200

    31_29_fet

--- 31_29_metge_virtual/metge.java
+++ 31_29_metge_virtual/metge.java
@@ -1,42 +1,43 @@
 /*Programa que fa de metge*/
+/*Per facilitar-te el treball de comprovació les opcions que hi han són
+ * No
+ * No i No
+ * No i Sí i No
+ * No i Sí i Sí
+ * Sí
+ * Sí i No
+ * Sí i Sí
+ * un 10 em faría el día la veritat :)*/
 public class metge {
-    public static void main(String args[]) {
-        System.out.println("Bon dia, en què puc ajudar-li?");
-        String charla = Entrada.readLine();
-        System.out.println("Presenta esternuts i mal de cap");
+    public static void main(String[] args) {
+        System.out.println("Bon dia");
+        System.out.println("Presenta vostè esternuts i mal de cap");
         String simptomes = Entrada.readLine();
-        boolean si = true;
-        si.toString();
-        boolean no = false;
-        no.toString();
-        switch (simptomes) {
-        case (simptomes.equal(si)): 
+        if (simptomes.equals ("Sí")) {
             System.out.println("Presenta vostè problemes d'estòmac a part dels esternuts i el mal de cap?");
-                break;
-            String malDePanxa = Entrada.readLine();
-        case (malDePanxa.equals(si)): 
+        }   String malDePanxa = Entrada.readLine();
+        if (simptomes.equals("Sí") && (malDePanxa.equals("Sí"))) {
             System.out.println("Premi 1gr de paracetamol cada 8 hores i recordi beure molta aigua");
-                break;  
-        case (malDePanxa.equals(no)): 
-            System.out.println("Premi 500mg d'àcid acetil salicílic (AAS) cada 6 horesi recordi prendre molta aigua");
-                break;
-        case (simptomes.equals(no)):
+            System.out.println("Ádeu, que vagi molt bé, espero que millori aviat");
+        }if (simptomes.equals("Sí") && (malDePanxa.equals("No"))) {
+             System.out.println("Premi 500mg d'àcid acetil salicílic (AAS) cada 6 hores i recordi prendre molta aigua");
+            System.out.println("Ádeu, que vagi molt bé, espero que millori aviat");
+        }   
+        if (simptomes.equals("No")) {
             System.out.println("Presenta molta tos?");
-                break;
-           String tos = Entrada.readLine();
-        case (tos.equals(no)):
+        }   String tos = Entrada.readLine();
+        if (simptomes.equals("No") && (tos.equals("Sí"))) {
             System.out.println("Es vostè menor de 12 anys?");
-                break;
-            String edat = Entrada.readLine();
-        case (edat.equals(no)):
+        }   String edat = Entrada.readLine();
+        if (tos.equals("Sí") && (edat.equals("No"))) {
             System.out.println("Premi un carmel d'eucaliptus cada 2 hores");
-                break;
-        case (edat.equals(si)):
+            System.out.println("Ádeu, que vagi molt bé, espero que millori aviat");
+        }   else if (tos.equals("Sí") && (edat.equals("Sí"))) {
             System.out.println("Prem un carmel de mel cada 2 hores");
-                break;
-        case (tos.equals(no)):
+            System.out.println("Ádeu, que vagi molt bé, espero que millori aviat");
+        }   else if (simptomes.equals("No") && (tos.equals("No"))) {
             System.out.println("Millor vingui el Dilluns a la meva consulta per realitzar un examen");
-                break;
         }
     }
 }
+
