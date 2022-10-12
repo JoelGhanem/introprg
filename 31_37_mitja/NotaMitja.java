e0370ecc235ee92d198c4cc1205c20feb885e8ea
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-12 16:16:45 +0200

    31_37_fet  ∾ prgtest autocommit 31_37 #9

--- 31_37_mitja/NotaMitja.java
+++ 31_37_mitja/NotaMitja.java
@@ -16,8 +16,9 @@ public class NotaMitja {
             suma = suma + valor;
             numDeNotes = numDeNotes + 1;
         }
-           System.out.println("La mitja de les notes vàlides és " + suma /numDeNotes );
-        
+        if (valor >= 0 && valor <= 100) {
+            System.out.println("La mitja de les notes vàlides és " + suma /numDeNotes );
+        }
     }
 }
 
