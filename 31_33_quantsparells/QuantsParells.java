32301f51c20118a405faa9911d15c2dd186d6bc6
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-13 18:22:40 +0200

    31_33_fet

--- 31_33_quantsparells/QuantsParells.java
+++ 31_33_quantsparells/QuantsParells.java
@@ -7,7 +7,7 @@ public class QuantsParells {
         int numValorSenar = 0;
         System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
-        while (valor >= 0) {
+        while (valor > 0) {
         System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
         if (valor % 2 == 0) {
