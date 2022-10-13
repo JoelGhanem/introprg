03f89241d63c76836d584e926469fadcb8adc683
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-13 17:25:04 +0200

    31_37_fet  ∾ prgtest autocommit 31_37 #1

--- 31_37_mitja/NotaMitja.java
+++ 31_37_mitja/NotaMitja.java
@@ -1,8 +1,8 @@
 /*Programa que et diu nla nota mitja*/
 public class NotaMitja {
     public static void main(String[] args) {
-        System.out.println("Introdueix un valor");
         int valor = 0;
+        System.out.println("Introdueix un valor");
         valor = Integer.parseInt(Entrada.readLine());
         if ((valor < 0) && (valor > 100)) {
              System.out.println("Cap nota vàlida introduïda");
