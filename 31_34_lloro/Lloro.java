621519dd5c77f45ad4dccc8da90fbcf446b3fe6b
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-13 20:49:19 +0200

    31_37_hecho  ∾ prgtest autocommit 31_34 #9

--- 31_34_lloro/Lloro.java
+++ 31_34_lloro/Lloro.java
@@ -2,14 +2,15 @@
 public class Lloro {
     public static void main(String[] args) {
         System.out.println("El lloro espera paraula:");
-        String paraula = "0"; 
+        String paraula = Entrada.readLine();; 
+        if (paraula.isEmpty() != false) {
+            System.out.println("Adéu");
+        }
         while ((paraula.isEmpty() == false)  && (paraula.isBlank() == false))  {
             paraula = Entrada.readLine();
             System.out.println("El lloro repeteix: " + paraula);
             System.out.println("El lloro espera paraula:");
-                if (paraula.isEmpty() != false) {
-                        System.out.println("Adéu");
-                    }
+            System.out.println("Adéu");
         }
     }
 }
