e0894553906e2fcd391e144c88d25b071ef1a361
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-14 15:21:37 +0200

    31_34_fet  ∾ prgtest autocommit 31_34 #2

--- 31_34_lloro/Lloro.java
+++ 31_34_lloro/Lloro.java
@@ -1,13 +1,13 @@
 /*Programa que actua com un lloro*/
 public class Lloro {
     public static void main(String[] args) {
-        String paraula = Entrada.readLine(); 
+        String paraula; 
         String paraulaLloro = "";
         System.out.println("El lloro espera paraula:");
         paraula = Entrada.readLine();
         while ((paraula.isEmpty() == false)  && (paraula.isBlank() == false))  {
             paraulaLloro = paraula; 
-            System.out.println("El lloro repeteix: " + paraula);
+            System.out.println("El lloro repeteix: " + paraulaLloro);
             System.out.println("El lloro espera paraula:");
             paraula = Entrada.readLine();
         }
