3f8ebbe1aacf036de928b46e9df249aff33085ca
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-14 15:35:11 +0200

    31_34_fet  ∾ prgtest autocommit 31_33 #2

--- 31_33_quantsparells/QuantsParells.java
+++ 31_33_quantsparells/QuantsParells.java
@@ -4,25 +4,17 @@ public class QuantsParells {
         int valor = 0;
         int valorParell = 0;
         int numValorParell = 0;
-        int numValorSenar = 0;
         int valorNegatiu = 0;
-        if (valor < 0) {
-            System.out.println("");
-        }
+        System.out.println("Introdueix un valor");
+        valor = Integer.parseInt(Entrada.readLine());
         while (valor >= 0) {
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
             if (valor % 2 == 0) {
                 valor = valorParell;
                 numValorParell = numValorParell + 1;
-                if (valor % 2 != 0) {
-                    valor = numValorSenar;
-                    if (valor < 0) {
-                        valorNegatiu = valor;
                     }
                 }
-            }
-        }
         System.out.println("Nombre de parells introduïts: " +  numValorParell);
     }
 }
