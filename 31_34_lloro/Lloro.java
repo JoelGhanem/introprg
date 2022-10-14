2ba5dc3b7ee91784776d0cf985480d4ab4cf6639
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-14 10:59:15 +0200

    31_34_fet

--- 31_34_lloro/Lloro.java
+++ 31_34_lloro/Lloro.java
@@ -2,16 +2,16 @@
 public class Lloro {
     public static void main(String[] args) {
         System.out.println("El lloro espera paraula:");
-        String paraula = Entrada.readLine();; 
+        String paraula = Entrada.readLine(); 
+        paraula = Entrada.readLine();
+        if (paraula.isEmpty() != false) {
+            System.out.print("");
+        }
         while ((paraula.isEmpty() == false)  && (paraula.isBlank() == false))  {
             paraula = Entrada.readLine();
-        if (paraula.isEmpty() != false) {
-            System.out.println("Adéu");
-        }
             System.out.println("El lloro repeteix: " + paraula);
             System.out.println("El lloro espera paraula:");
-            System.out.println("Adéu");
         }
+        System.out.println("Adéu");
     }
 }
-
