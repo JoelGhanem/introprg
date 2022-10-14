333cf2cd7b65fdb283c395924de6f456e5c54dd7
prgtest <moises.gomez@iesjoandaustria.org>
2022-10-15 01:45:48 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 31_37 #10

--- 31_37_mitja/NotaMitja.java
+++ 31_37_mitja/NotaMitja.java
@@ -6,12 +6,12 @@ public class NotaMitja {
         System.out.println("Introdueix un valor");
         valor1 = Integer.parseInt(Entrada.readLine());
         //Hago valor 1 para que no me de problemas con el final
-        if (valor1 < 0 || valor1 > 100) {
+        if (((valor1 < 0) == true) || ((valor1 > 100))) {
             System.out.println("Cap nota vàlida introduïda");
         }   
         int suma = 0;
         int numDeNotes = 0;
-        while (((valor1 >= 0) == true) && ((valor1 <= 100 == true))) {
+        while (((valor1 >= 0) == true) && ((valor1 <= 100) == true)) {
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
             if ((valor < 0 != true) && (valor > 100 != true)) {
@@ -19,7 +19,7 @@ public class NotaMitja {
                 numDeNotes = numDeNotes + 1;
             }
         }
-        if ((valor < 0 != true) && (valor > 100 != true)) {
+        if (((valor < 0) == false) && ((valor > 100) == false)) {
             System.out.println("La mitja de les notes vàlides és " + suma /numDeNotes );
         }
         if (valor > 100) {
