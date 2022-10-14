88db875cd93a8030ebfca0a939182446dd2f3179
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-14 15:16:01 +0200

    31_34_fet

--- 31_34_lloro/Lloro.java
+++ 31_34_lloro/Lloro.java
@@ -1,15 +1,18 @@
 /*Programa que actua com un lloro*/
 public class Lloro {
     public static void main(String[] args) {
-        System.out.println("El lloro espera paraula:");
         String paraula = Entrada.readLine(); 
+        String paraulaLloro = "";
+        System.out.println("El lloro espera paraula:");
         paraula = Entrada.readLine();
         if (paraula.isEmpty() == true) {
             System.out.print("");
         }
         while ((paraula.isEmpty() == false)  && (paraula.isBlank() == false))  {
+            paraulaLloro = paraula; 
             System.out.println("El lloro repeteix: " + paraula);
             System.out.println("El lloro espera paraula:");
+            paraula = Entrada.readLine();
         }
         System.out.println("Adéu");
     }
