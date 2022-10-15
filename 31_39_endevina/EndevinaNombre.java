2a8abbbcf86ce3b21273498b60ed6a08add7912f
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-15 12:19:55 +0200

    31_39_fet

---
+++ 31_39_endevina/EndevinaNombre.java
@@ -0,0 +1,27 @@
+// Programa que et fa jugar amb ell per que endevis un nombre
+public class EndevinaNombre {
+    public static void main(String[] args) {
+        int numero = 0;
+        int numeroPensat = 42;
+        System.out.println("Ves introduint enters entre 1 i 100 fins que encertis el que jo he pensat");
+        numero = Integer.parseInt(Entrada.readLine());
+        while (numero < numeroPensat) {
+            System.out.println("És més gran que " + numero);
+            System.out.println("Introdueix un valor");
+            if ((numero <= 0) == true) {
+                System.out.println("Com a mínim 1");
+                System.out.println("Introdueix un valor");
+            }
+        }
+        while (numero > numeroPensat) {
+            System.out.println("És més petit que " + numero);
+            if ((numero > 100) == true) {
+                System.out.println("Com a màxim 100");
+                System.out.println("Introdueix un valor");
+            }
+        }
+        if (numero == numeroPensat) {
+            System.out.println("Has encertat!");
+        }
+    }
+}
