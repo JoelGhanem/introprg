5a55f0095a2ebdd50145fc64301c2bbd54b4d35e
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-17 18:27:17 +0200

    31_37_fet

--- 31_37_mitja/NotaMitja.java
+++ 31_37_mitja/NotaMitja.java
@@ -6,12 +6,15 @@ public class NotaMitja {
         System.out.println("Introdueix un valor");
         valor1 = Integer.parseInt(Entrada.readLine());
         //Hago valor 1 para que no me de problemas con el final
-        if (((valor1 < 0) == true) || ((valor1 > 100))) {
+        if (((valor1 < 0) == true) || ((valor1 > 100) == true)) {
             System.out.println("Cap nota vàlida introduïda");
         }   
+        if  (((valor1 >= 0) == true) && ((valor1 <= 100) == true)) {
+            valor1 = valor;
+        }
         int suma = 0;
         int numDeNotes = 0;
-        while (((valor1 >= 0) == true) && ((valor1 <= 100) == true)) {
+        while (((valor >= 0) == true) && ((valor <= 100) == true)) {
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
             if (((valor < 0) == false) || ((valor > 100) == false)) {
