898254090fc1ba8916205f901ce28e814d18758c
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-17 20:51:14 +0200

    31_39_fet  ∾ prgtest autocommit 31_39 #8

--- 31_39_endevina/EndevinaNombre.java
+++ 31_39_endevina/EndevinaNombre.java
@@ -6,9 +6,7 @@ public class EndevinaNombre {
         System.out.println("Ves introduint enters entre 1 i 100 fins que encertis el que jo he pensat");
         System.out.println("Introdueix un valor");
         numero = Integer.parseInt(Entrada.readLine());
-        if (numero == numeroPensat) {
-            System.out.println("Has encertat!");
-        }while ((numero != numeroPensat) == true) {
+        while ((numero != numeroPensat) == true) {
             System.out.println("Introdueix un valor");
             numero = Integer.parseInt(Entrada.readLine());
             if (numero > numeroPensat) {
