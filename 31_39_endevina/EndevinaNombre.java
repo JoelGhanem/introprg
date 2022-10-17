50460282d4221672a5962aafa20e68bb37f51b06
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-17 20:50:49 +0200

    31_39_fet  ∾ prgtest autocommit 31_39 #7

--- 31_39_endevina/EndevinaNombre.java
+++ 31_39_endevina/EndevinaNombre.java
@@ -8,24 +8,34 @@ public class EndevinaNombre {
         numero = Integer.parseInt(Entrada.readLine());
         if (numero == numeroPensat) {
             System.out.println("Has encertat!");
-
         }while ((numero != numeroPensat) == true) {
+            System.out.println("Introdueix un valor");
+            numero = Integer.parseInt(Entrada.readLine());
             if (numero > numeroPensat) {
                 System.out.println("És més petit que " + numero);
+                System.out.println("Introdueix un valor");
+                numero = Integer.parseInt(Entrada.readLine());
             }
             if (numero < numeroPensat) {
                 System.out.println("És més gran que " + numero);
+                System.out.println("Introdueix un valor");
+                numero = Integer.parseInt(Entrada.readLine());
             }
             if ((numero <= 0) == true) {
                 System.out.println("Com a mínim 1");
+                System.out.println("Introdueix un valor");
+                numero = Integer.parseInt(Entrada.readLine());
             }
             if ((numero > 100) == true) {
                 System.out.println("Com a màxim 100");
+                System.out.println("Introdueix un valor");
+                numero = Integer.parseInt(Entrada.readLine());
             }
             System.out.println("Introdueix un valor");
-            if (numero == numeroPensat) {
-                System.out.println("Has encertat!");
-            }
+            numero = Integer.parseInt(Entrada.readLine());
+        }
+        if (numero == numeroPensat) {
+            System.out.println("Has encertat!");
         }
     }
 }
