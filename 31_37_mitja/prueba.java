38d1b21c095ddccedc5ed4d906c39b18909fdb1b
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-17 23:54:45 +0200

    31_37_FET  ∾ prgtest autocommit 31_37 #15

--- 31_37_mitja/prueba.java
+++ 31_37_mitja/prueba.java
@@ -1,10 +1,10 @@
 /*Programa que et diu nla nota mitja*/
 public class prueba {
     public static void main(String[] args) {
-        int nota = 0;
-        int notaValida = 0;
         int suma = 0;
         int numDeNotes = 0;
+        int nota = 0;
+        int notaValida= 0;
         System.out.println("Introdueix un valor");
         nota = Integer.parseInt(Entrada.readLine());
         if ((nota >= 0) && (nota <= 100)) {
