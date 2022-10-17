1ab05361fac79e3abff61f07054e7eb4473cefea
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-17 18:45:48 +0200

    31_37_fet  ∾ prgtest autocommit 31_37 #8

--- 31_37_mitja/NotaMitja.java
+++ 31_37_mitja/NotaMitja.java
@@ -1,17 +1,17 @@
 /*Programa que et diu nla nota mitja*/
 public class NotaMitja {
     public static void main(String[] args) {
-        int valor = 0;
+        int valor = -1;
         int valor1 = 0;
         System.out.println("Introdueix un valor");
         valor1 = Integer.parseInt(Entrada.readLine());
         //Hago valor 1 para que no me de problemas con el final
-        if (((valor1 < 0) == true) || ((valor1 > 100) == true)) {
+        if (((valor1 < 0) == true) || ((valor1 > 100))) {
             System.out.println("Cap nota vàlida introduïda");
         }   
         int suma = 0;
         int numDeNotes = 0;
-        while (((valor >= 0) == true) && ((valor <= 100) == true)) {
+        while (((valor1 >= 0) == true) && ((valor1 <= 100) == true)) {
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
             if (((valor < 0) == false) || ((valor > 100) == false)) {
