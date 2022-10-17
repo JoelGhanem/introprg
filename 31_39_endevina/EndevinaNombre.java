1da79affb04e2720fe1bae889b073bca957f6fbe
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-17 20:39:29 +0200

    31_39_fet

--- 31_39_endevina/EndevinaNombre.java
+++ 31_39_endevina/EndevinaNombre.java
@@ -6,21 +6,22 @@ public class EndevinaNombre {
         System.out.println("Ves introduint enters entre 1 i 100 fins que encertis el que jo he pensat");
         System.out.println("Introdueix un valor");
         numero = Integer.parseInt(Entrada.readLine());
-        while (numero < numeroPensat) {
+        while (numero != numeroPensat) {
+            if (numero > numeroPensat) {
+            System.out.println("És més petit que " + numero);
+            }
+            if (numero < numeroPensat) {
             System.out.println("És més gran que " + numero);
-            System.out.println("Introdueix un valor");
+            }
             if ((numero <= 0) == true) {
                 System.out.println("Com a mínim 1");
-                System.out.println("Introdueix un valor");
             }
-        }
-            System.out.println("És més petit que " + numero);
             if ((numero > 100) == true) {
                 System.out.println("Com a màxim 100");
-                System.out.println("Introdueix un valor");
             }
+            System.out.println("Introdueix un valor");
         }
-        if (numero == numeroPensat) {
+        if (numeroPensat == numero) {
             System.out.println("Has encertat!");
         }
     }
