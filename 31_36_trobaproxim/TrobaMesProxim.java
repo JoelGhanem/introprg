b20ea9df796537de8633b68c27c3a5ce9daefefc
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-18 19:52:55 +0200

    31_36_fet  ∾ prgtest autocommit 31_36 #2

--- 31_36_trobaproxim/TrobaMesProxim.java
+++ 31_36_trobaproxim/TrobaMesProxim.java
@@ -4,19 +4,18 @@ public class TrobaMesProxim {
         int ancora = 0;
         int valor = 0;
         System.out.println("Introdueix l'àncora");
-            ancora = Integer.parseInt(Entrada.readLine());
+        ancora = Integer.parseInt(Entrada.readLine());
         if (ancora >= 0) {
-         while (valor >= 0) {
-            System.out.println("Introdueix un valor");
-            valor = Integer.parseInt(Entrada.readLine());
-        }   
-        }
-
-        if (ancora <= 0) {
-            System.out.println("Àncora no vàlida");
+            while (valor >= 0) {
+                System.out.println("Introdueix un valor");
+                valor = Integer.parseInt(Entrada.readLine());
+            }   
         }
         if (valor <= 0) {
             System.out.println("No s'ha introduït cap valor positiu");
+        }
+        if (ancora <= 0) {
+            System.out.println("Àncora no vàlida");
         }
     }
 }
