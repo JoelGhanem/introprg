cb94bc06d3a4e93fa41e07f8e00924a89c287466
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-18 19:13:11 +0200

    31_37_FET  ∾ prgtest autocommit 31_37 #20

--- 31_37_mitja/NotaMitja.java
+++ 31_37_mitja/NotaMitja.java
@@ -1,40 +1,23 @@
 /*Programa que et diu nla nota mitja*/
 public class NotaMitja {
     public static void main(String[] args) {
-        int nota = -1;
-        int nota1 = 0;
-        int notaValida = 0;
-        int notaNoValida = 0;
-        System.out.println("Introdueix un valor");
-        nota1 = Integer.parseInt(Entrada.readLine());
-        //Hago valor 1 para que no me de problemas con el final
-        if (((nota1 < 0) == true) || ((nota1 > 100))) {
-            System.out.println("Cap nota vàlida introduïda");
-        }   
-        if (((nota1 >= 0) == true) && ((nota1 < 100))) {
-            nota  = nota1;
-        }
         int suma = 0;
         int numDeNotes = 0;
-        while (((nota >= 0) == true) && ((nota <= 100) == true)) {
-            System.out.println("Introdueix un valor");
-            nota = Integer.parseInt(Entrada.readLine());
-            if (((nota >= 0) == true) && ((nota <= 100) == true)) {
-                notaValida = nota;
+        int nota = 0;
+        int notaMedia = 0;
+        System.out.println("Introdueix un valor");
+        nota = Integer.parseInt(Entrada.readLine());
+        if (nota >= 0 && nota <= 100) {
+            while (nota >= 0 && nota <= 100) {
+                suma = suma + nota;
+                numDeNotes = numDeNotes + 1;
+                System.out.println("Introdueix un valor");
+                nota = Integer.parseInt(Entrada.readLine());
             }
-            suma = notaValida + suma;
-            numDeNotes++;
-            if (((nota < 0) ==true) || ((nota > 100) == true)) {
-                notaNoValida = nota;
-
-            }
-        }
-        if (notaValida == nota) {
-        int notaMedia = suma / numDeNotes;
-        System.out.println("La mitja de les notes vàlides és " + notaMedia);
-        }
-        if ((notaNoValida == nota)) {
-            System.out.println("La mitja de les notes vàlides és " + suma );
+            notaMedia = suma / numDeNotes;
+            System.out.println("La mitja de les notes vàlides és " + notaMedia);
+        } else {
+            System.out.println("Cap nota vàlida introduïda");
         }
     }
 }
