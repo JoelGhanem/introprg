8a53bf272f7ec247afd0d80a5e28cfb88ba0c289
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-18 19:21:57 +0200

    31_37_FET  ∾ prgtest autocommit 31_39 #4

--- 31_39_endevina/EndevinaNombre.java
+++ 31_39_endevina/EndevinaNombre.java
@@ -12,23 +12,15 @@ public class EndevinaNombre {
                 numero = Integer.parseInt(Entrada.readLine());
                 if (numero > numeroPensat) {
                     System.out.println("És més petit que " + numero);
-                    System.out.println("Introdueix un valor");
-                    numero = Integer.parseInt(Entrada.readLine());
                 }
                 if (numero < numeroPensat) {
                     System.out.println("És més gran que " + numero);
-                    System.out.println("Introdueix un valor");
-                    numero = Integer.parseInt(Entrada.readLine());
                 }
                 if ((numero <= 0) == true) {
                     System.out.println("Com a mínim 1");
-                    System.out.println("Introdueix un valor");
-                    numero = Integer.parseInt(Entrada.readLine());
                 }
                 if ((numero > 100) == true) {
                     System.out.println("Com a màxim 100");
-                    System.out.println("Introdueix un valor");
-                    numero = Integer.parseInt(Entrada.readLine());
                 }
             }
         }
