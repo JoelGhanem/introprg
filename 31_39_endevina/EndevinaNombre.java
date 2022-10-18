14666d062289c496a17e88e70a2e5a212530c730
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-18 19:19:25 +0200

    31_37_FET  ∾ prgtest autocommit 31_39 #1

--- 31_39_endevina/EndevinaNombre.java
+++ 31_39_endevina/EndevinaNombre.java
@@ -1,39 +1,41 @@
 // Programa que et fa jugar amb ell per que endevis un nombre
 public class EndevinaNombre {
     public static void main(String[] args) {
-        int numero = 0;
+        int numero = 1;
         int numeroPensat = 42;
         System.out.println("Ves introduint enters entre 1 i 100 fins que encertis el que jo he pensat");
         System.out.println("Introdueix un valor");
         numero = Integer.parseInt(Entrada.readLine());
-        while ((numero != numeroPensat) == true) {
-            System.out.println("Introdueix un valor");
-            numero = Integer.parseInt(Entrada.readLine());
-            if (numero > numeroPensat) {
-                System.out.println("És més petit que " + numero);
+        if (numero != numeroPensat) {
+            while (numero != numeroPensat) {
+                System.out.println("Introdueix un valor");
+                numero = Integer.parseInt(Entrada.readLine());
+                if (numero > numeroPensat) {
+                    System.out.println("És més petit que " + numero);
+                    System.out.println("Introdueix un valor");
+                    numero = Integer.parseInt(Entrada.readLine());
+                }
+                if (numero < numeroPensat) {
+                    System.out.println("És més gran que " + numero);
+                    System.out.println("Introdueix un valor");
+                    numero = Integer.parseInt(Entrada.readLine());
+                }
+                if ((numero <= 0) == true) {
+                    System.out.println("Com a mínim 1");
+                    System.out.println("Introdueix un valor");
+                    numero = Integer.parseInt(Entrada.readLine());
+                }
+                if ((numero > 100) == true) {
+                    System.out.println("Com a màxim 100");
+                    System.out.println("Introdueix un valor");
+                    numero = Integer.parseInt(Entrada.readLine());
+                }
                 System.out.println("Introdueix un valor");
                 numero = Integer.parseInt(Entrada.readLine());
             }
-            if (numero < numeroPensat) {
-                System.out.println("És més gran que " + numero);
-                System.out.println("Introdueix un valor");
-                numero = Integer.parseInt(Entrada.readLine());
+            if (numero == numeroPensat) {
+                System.out.println("Has encertat!");
             }
-            if ((numero <= 0) == true) {
-                System.out.println("Com a mínim 1");
-                System.out.println("Introdueix un valor");
-                numero = Integer.parseInt(Entrada.readLine());
-            }
-            if ((numero > 100) == true) {
-                System.out.println("Com a màxim 100");
-                System.out.println("Introdueix un valor");
-                numero = Integer.parseInt(Entrada.readLine());
-            }
-            System.out.println("Introdueix un valor");
-            numero = Integer.parseInt(Entrada.readLine());
-        }
-        if (numero == numeroPensat) {
-            System.out.println("Has encertat!");
         }
     }
 }
