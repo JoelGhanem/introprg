2f23c3597618141b0dc044fd5b95ef0c68feebce
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-18 19:50:38 +0200

    31_36_fet

--- 31_36_trobaproxim/TrobaMesProxim.java
+++ 31_36_trobaproxim/TrobaMesProxim.java
@@ -4,15 +4,17 @@ public class TrobaMesProxim {
         int ancora = 0;
         int valor = 0;
         System.out.println("Introdueix l'àncora");
-         while (ancora >= 0) {
+            ancora = Integer.parseInt(Entrada.readLine());
+        if (ancora >= 0) {
+         while (valor >= 0) {
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
         }   
-            ancora = Integer.parseInt(Entrada.readLine());
+        }
+
         if (ancora <= 0) {
             System.out.println("Àncora no vàlida");
         }
-            valor = Integer.parseInt(Entrada.readLine());
         if (valor <= 0) {
             System.out.println("Àncora no vàlida");
         }
