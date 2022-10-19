c8dfe68b6dde0605a70239e9766bb6653d9c656a
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-19 10:18:04 +0200

    31_42_fet

---
+++ 31_42_deu_primers_decreixent/DeuPrimersDecreixent.java
@@ -0,0 +1,12 @@
+// Programa que conta del 10 a l'1
+public class DeuPrimersDecreixent {
+    public static void main (String[] args) {
+        int i = 10;
+        while (i > 0) {
+            i -= 1;
+                if (i > 0) {
+                    System.out.println(i);
+                }
+        }
+    }
+}
