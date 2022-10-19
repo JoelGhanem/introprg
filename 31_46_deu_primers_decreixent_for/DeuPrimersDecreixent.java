77ad68156ffc590a06de73d8cbe6dacaab6f6f51
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-19 19:45:41 +0200

    31_46_fet

---
+++ 31_46_deu_primers_decreixent_for/DeuPrimersDecreixent.java
@@ -0,0 +1,8 @@
+//Desenvolupa un programa que escrigui els primers 10 números naturals començant pel més gran. És a dir: 10 9 8 … 1
+public class DeuPrimersDecreixent {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
