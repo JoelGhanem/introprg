ed9874334f5ca3ee0a6aa85b39103af2b077adb4
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-19 10:21:25 +0200

    31_41_fet  ∾ prgtest autocommit 31_41 #1

--- 31_41_deu_primers/DeuPrimers.java
+++ 31_41_deu_primers/DeuPrimers.java
@@ -1,7 +1,7 @@
 //Programa que suma d'un en un fins que arriba a 10
 public class DeuPrimers {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 1;
         while (i <=10) {
             System.out.println(i);
             i += 1;
