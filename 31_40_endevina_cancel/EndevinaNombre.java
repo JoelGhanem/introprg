7ad5eb18157c67d0a31d05fb3b598ac5c672ad65
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-19 10:12:32 +0200

    31_41_fet

---
+++ 31_40_endevina_cancel/EndevinaNombre.java
@@ -0,0 +1,41 @@
+// Programa que et fa jugar amb ell per que endevis un nombre
+public class EndevinaNombre {
+    public static void main(String[] args) {
+        int numero = 1;
+        int numeroPensat = 42;
+        System.out.println("Ves introduint enters entre 1 i 100 fins que encertis el q    ue jo he pensat");
+        System.out.println("Introdueix un valor");
+        numero = Integer.parseInt(Entrada.readLine());
+        if (numero != numeroPensat) {
+            while (numero != numeroPensat) {
+                if (numero <= 0) {
+                    System.out.println("Com a mínim 1");
+                    System.out.println("Introdueix un valor");
+                    numero = Integer.parseInt(Entrada.readLine());
+                }
+                else if (numero > 100) {
+                    System.out.println("Com a màxim 100");
+                    System.out.println("Introdueix un valor");
+                    numero = Integer.parseInt(Entrada.readLine());
+                }
+                else if (numero > numeroPensat) {
+                    System.out.println("És més petit que " + numero);
+                    System.out.println("Introdueix un valor");
+                    numero = Integer.parseInt(Entrada.readLine());
+                }
+                else if (numero < numeroPensat) {
+                    System.out.println("És més gran que " + numero);
+                    System.out.println("Introdueix un valor");
+                    numero = Integer.parseInt(Entrada.readLine());
+                }
+            }
+            if (numero = "") {
+                System.out.println("Cancel·lat!");
+            }
+        }
+        if (numero == numeroPensat) {
+            System.out.println("Has encertat!");
+        }
+
+    }
+}
---
+++ 31_41_deu_primers/DeuPrimers.java
@@ -0,0 +1,10 @@
+//Programa que suma d'un en un fins que arriba a 10
+public class DeuPrimers {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <=10) {
+            i += 1;
+            System.out.println(i);
+        }
+    }
+}
