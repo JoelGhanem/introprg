44ed1eb9546033fe052ed23202372a3072b8264f
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-19 19:49:41 +0200

    31_47_fet

---
+++ 31_47_primers_parells_for/PrimersParells.java
@@ -0,0 +1,8 @@
+//Desenvolupa un programa que escrigui els primers números naturals que siguin parells fins el 10. És a dir: 2 4 6 8 10
+public class PrimersParells {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
