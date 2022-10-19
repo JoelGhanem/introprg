724e086c395ece95c326197d799b61bca3f5350b
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-19 10:49:17 +0200

    31_44_fet

---
+++ 31_44_de_principi_a_fi/EntersIniciFi.java
@@ -0,0 +1,19 @@
+//Programa que et demana tres nombres enters, i escriu els valor entre el primer i el segon tenin en compte el tercer
+public class EntersIniciFi {
+    public static void main(String[] args) {
+        int valorInicial = 0;
+        int valorFinal = 0;
+        int salt = 0;
+        System.out.println("Valor inicial?");
+        valorInicial = Integer.parseInt(Entrada.readLine());
+        System.out.println("Valor final?");
+        valorFinal = Integer.parseInt(Entrada.readLine());
+        System.out.println("Salt?");
+        salt = Integer.parseInt(Entrada.readLine());
+        while (valorInicial > valorFinal) {
+            valorInicial = valorInicial + salt;
+            System.out.println(valorInicial);
+        }
+    }
+}
+
