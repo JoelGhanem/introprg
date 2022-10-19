7015bb3a94ce82819b5bd40dd0da42fa2d1dd148
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-19 10:27:34 +0200

    31_43_fet

---
+++ 31_43_primers_parells/PrimersParells.java
@@ -0,0 +1,10 @@
+//Programa que suma de 2 en 2 del 2 al 10
+public class PrimersParells {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {
+            i = i + 2;
+            System.out.println(i);
+        }
+    }
+}
