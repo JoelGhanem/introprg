a5d7b3177a07f4ba40b45a1c3d7e1e44b1d326aa
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-19 10:21:01 +0200

    31_41_fet

--- 31_41_deu_primers/DeuPrimers.java
+++ 31_41_deu_primers/DeuPrimers.java
@@ -3,10 +3,8 @@ public class DeuPrimers {
     public static void main(String[] args) {
         int i = 0;
         while (i <=10) {
-            i += 1;
-            if (i <= 10) {
             System.out.println(i);
-            }
+            i += 1;
         }
     }
 }
