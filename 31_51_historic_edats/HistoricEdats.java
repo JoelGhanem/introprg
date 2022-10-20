0df83fab978496c3c65708d573f3f35bcff88cf2
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-20 15:10:51 +0200

    31_51_fet  ∾ prgtest autocommit 31_51 #24

--- 31_51_historic_edats/HistoricEdats.java
+++ 31_51_historic_edats/HistoricEdats.java
@@ -13,7 +13,7 @@ public class HistoricEdats {
         any = Integer.parseInt(Entrada.readLine());
         int anyNaix = (any - edat);
         System.out.println("El " + anyNaix + " vau néixer");
-        int n = 0;
+        int n = 1;
         anyNaix = anyNaix + 1;
         System.out.println("El " + anyNaix + " teníeu 1 any");
         anyNaix = anyNaix + 1;
