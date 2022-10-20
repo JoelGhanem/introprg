4bcf36ce19bea3df0636b75927320c84c4f30871
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-20 16:14:56 +0200

    31_18_fet

--- 31_38_dowhile/SequenciaCreixent.java
+++ 31_38_dowhile/SequenciaCreixent.java
@@ -2,14 +2,19 @@
 public class SequenciaCreixent {
     public static void main(String[] args) {
         int valor = 0;
-        int sequencia = 0;
+        int valor1 = 0;
+        int sequencia = -1;
         int tmp;
         do {
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
             tmp = valor;
             sequencia += 1;
-        }    while (valor > tmp);
+            System.out.println("Introdueix un valor");
+            valor1 = Integer.parseInt(Entrada.readLine());
+            sequencia += 1;
+        } 
+        while (valor1 > valor);
         System.out.println("Longitud de la següència creixent: " + sequencia);
     }
 }
