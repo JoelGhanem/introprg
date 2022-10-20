e5acfd453c9b07613adefd9b51258a342853b894
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-20 12:25:38 +0200

    31_50_fet

--- 31_50_mostra_interval/MostraInterval.java
+++ 31_50_mostra_interval/MostraInterval.java
@@ -3,12 +3,17 @@ public class MostraInterval {
     public static void main(String[] args) {
         int inici;
         int final1;
-            System.out.println("inici?");
+        System.out.println("inici?");
         inici = Integer.parseInt(Entrada.readLine());
         System.out.println("final?");
         final1 = Integer.parseInt(Entrada.readLine());
-        for (int i = inici; i <= final1; i++) {
+        if (inici < final1) {
+            for (int i = inici; i <= final1; i++) {
+                System.out.println(i);
+            }
+        }
+    }if (inici > final1) {
+        for (int i = final1; i <= inici; i--) {
             System.out.println(i);
         }
     }
-}
