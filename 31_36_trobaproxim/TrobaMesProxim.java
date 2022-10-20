5f66c0b8695a54d2ccb459a41a22ffd041998daf
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-20 18:38:40 +0200

    31_52_fet  ∾ prgtest autocommit 31_36 #1

--- 31_36_trobaproxim/TrobaMesProxim.java
+++ 31_36_trobaproxim/TrobaMesProxim.java
@@ -1,27 +1,45 @@
-/*Programa que et retorna el valor mes proxim a un numero*/
+//Programa que t arruina la vida
 public class TrobaMesProxim {
-    public static void main(String[] args) {
-        int ancora = 0;
-        int valor = 0;
-        int valorProxim = 0;
-        System.out.println("Introdueix l'àncora");
+    public static void maion(String[] args) {
+        int ancora;
+        int valor1;
+        int valor2;
+        int diferencia1;
+        int diferencia2;
+        int diferenciaPetita;
+        System.out.println("introdueix l'àncora");
         ancora = Integer.parseInt(Entrada.readLine());
         if (ancora >= 0) {
             System.out.println("Introdueix un valor");
-            valor = Integer.parseInt(Entrada.readLine());
-            if (valor <= 0) {
+            valor1 = Integer.parseInt(Entrada.readLine());
+            if (valor1 >= 0) {
+                while (valor1 >= 0) {
+                    System.out.println("Introdueix un valor");
+                    valor1 = Integer.parseInt(Entrada.readLine());
+                    diferencia1 = ancora - valor1;
+                    Math.abs(diferencia1);
+                    System.out.println("Introdueix un valor");
+                    valor2= Integer.parseInt(Entrada.readLine());
+                    diferencia2 = ancora - valor2;
+                    Math.abs(diferencia2);
+                    if (diferencia1 > diferencia2); {
+                        diferencia1 = diferencia2;
+                        diferenciaPetita = diferencia1;
+                    }
+                    if (diferencia1 < diferencia2) {
+                        diferenciaPetita = diferencia1;
+                    }
+
+                }
+            }
+            if (valor1 < 0) {
                 System.out.println("No s'ha introduït cap valor positiu");
             }
-            while (valor >= 0) {
-                valorProxim = (ancora - valor);
-
-                System.out.println("Introdueix un valor");
-                valor = Integer.parseInt(Entrada.readLine());
-            }   
-            System.out.println("El valor introduït més pròxim a " + ancora + " és " + valorProxim);
         }
-        if (ancora <= 0) {
+        if (ancora < 0) {
             System.out.println("Àncora no vàlida");
+
         }
     }
+
 }
