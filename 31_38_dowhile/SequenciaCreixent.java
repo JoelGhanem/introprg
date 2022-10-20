0369cbe428342eed4f788c3b5fd8e241d15534d3
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-20 15:35:00 +0200

    31_51_fet  ∾ prgtest autocommit 31_51 #27

--- 31_38_dowhile/SequenciaCreixent.java
+++ 31_38_dowhile/SequenciaCreixent.java
@@ -1,17 +1,15 @@
 //Programa que te muestra la racha de tu secuencia creciente
 public class SequenciaCreixent {
     public static void main(String[] args) {
-        int valor1 = 0;
         int valor = 0;
         int sequencia = 0;
-        // El programa te un problema que crec que no es pot solucionar amb els coneixements actuals i es que només funciona bé quan la cuantitad de valors introduits es parella ja que si no el programa no acaba de executar-se i no pot llegir si la seguencia ha sigut correcte
+        int tmp;
         do {
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
-            System.out.println("Introdueix un valor");
-            valor1 = Integer.parseInt(Entrada.readLine());
+            tmp = valor;
             sequencia += 1;
-        }    while (valor1 > valor);
+        }    while (valor > tmp);
         System.out.println("Longitud de la següència creixent: " + sequencia);
     }
 }
