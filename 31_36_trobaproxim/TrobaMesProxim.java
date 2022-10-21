df72b83b1097ceb4af72a9fd82025624a3fda163
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-22 01:16:06 +0200

    31_40_fet  ∾ prgtest autocommit 31_36 #30

--- 31_36_trobaproxim/TrobaMesProxim.java
+++ 31_36_trobaproxim/TrobaMesProxim.java
@@ -6,14 +6,14 @@ public class TrobaMesProxim {
         int dif = 0;
         int dif1 = 0;
         int difPetita = 0;
-        int valorReal;
+        int valorReal = -1;
         int valorReal1;
         System.out.println("Introdueix l'àncora");
         ancora = Integer.parseInt(Entrada.readLine());
         if (ancora >= 0) {
             System.out.println("Introdueix un valor");
             val = Integer.parseInt(Entrada.readLine());
-             if (val < 0) {
+            if (val < 0) {
                 System.out.println("No s'ha introduït cap valor positiu");
             }
             while (val >= 0) {
@@ -26,19 +26,21 @@ public class TrobaMesProxim {
                 dif1 = Math.abs(dif1);
                 valorReal1 = val;
                 //este if es pq si no lo hago me detecta el valor -1 como un valor aceptable ya que el sistema hace todo el procedimiento antes de comprobar si el valor es positivo;
-                if (val >= 0) 
-                if (dif < dif1) {
-                    difPetita = dif;
-                    difPetita = valorReal;
-                }
+                if (val >= 0) {
+                    if (dif < dif1) {
+                        difPetita = dif;
+                        difPetita = valorReal;
+                    }
                     else if (dif > dif1) {
                         dif = dif1;
                         difPetita = dif;
-                    difPetita = valorReal1;
+                        difPetita = valorReal1;
                     }
-                
+                }
             }
-            System.out.println("El valor introduït més pròxim a " + ancora + " és " +  difPetita);
+            if (valorReal >= 0) {
+                System.out.println("El valor introduït més pròxim a " + ancora + " és " +  difPetita);
+            }
         }
         else if (ancora < 0) {
             System.out.println("Àncora no vàlida");
