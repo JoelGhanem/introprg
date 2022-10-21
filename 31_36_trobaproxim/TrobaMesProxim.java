30971cd262ac7ef78e6ab0f002cd1f12663cb01b
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-21 11:30:54 +0200

    31_40_fet  ∾ prgtest autocommit 31_36 #1

--- 31_36_trobaproxim/TrobaMesProxim.java
+++ 31_36_trobaproxim/TrobaMesProxim.java
@@ -3,6 +3,7 @@ public class TrobaMesProxim {
     public static void main(String[] args) {
         int ancora = 0;
         int valor1 = 0;
+        int valor2 = 0;
         int dif1 = 0;
         int dif2 = 0;
         int difPetita = 0;
@@ -24,8 +25,8 @@ public class TrobaMesProxim {
                     dif1 = ancora - valor1;
                     Math.abs(dif1);
                     System.out.println("Introdueix un valor");
-                    valor1 = Integer.parseInt(Entrada.readLine());
-                    dif2 = ancora - valor1;
+                    valor2 = valor1;
+                    dif2 = ancora - valor2;
                     Math.abs(dif2);
                     if (dif1 > dif2); {
                         dif1 = dif2;
