552f62d97964d604a663d39ab215e29616187342
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-25 19:50:33 +0200

    31_57_estoy trifuerte

---
+++ 31_57_trilal/Trilal.java
@@ -0,0 +1,58 @@
+//segunda vez que hago el ejercicio
+public class Trilal {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("quants?");
+        num = Integer.parseInt(Entrada.readLine());
+        for (int quantitat = 0; quantitat != num; quantitat++) {
+            for (int li = 1; li <= 10; li++) {
+                for (int puntito = 19; puntito >= li ; puntito--) {
+                    System.out.print(".");
+                }
+                for (int coP = 10 - li ; coP <= 9; coP++) {
+                    System.out.print(coP);
+                }
+                for (int coN = 8; coN >= 10 - li; coN--) {
+                    System.out.print(coN);
+                }
+                for (int punticos = 19; punticos >= li; punticos--) {
+                    System.out.print(".");
+                }
+                System.out.println();
+            }
+            for (int li = 1; li <= 10; li++) {
+                for (int puntito = 9; puntito >= li ; puntito--) {
+                    System.out.print(".");
+                }
+                for (int coP = 10 - li ; coP <= 9; coP++) {
+                    System.out.print(coP);
+                }
+                for (int coN = 8; coN >= 10 - li; coN--) {
+                    System.out.print(coN);
+                }
+                for (int punticos = 9; punticos >= li; punticos--) {
+                    System.out.print(".");
+                }
+                for (int puntito = 1; puntito <= 1; puntito++) {
+                    System.out.print(".");
+                }
+                for (int puntito = 9; puntito >= li ; puntito--) {
+                    System.out.print(".");
+                }
+                for (int coP = 10 - li ; coP <= 9; coP++) {
+                    System.out.print(coP);
+                }
+                for (int coN = 8; coN >= 10 - li; coN--) {
+                    System.out.print(coN);
+                }
+                for (int punticos = 9; punticos >= li; punticos--) {
+                    System.out.print(".");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+
+
