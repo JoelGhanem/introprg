a29f1106327202ca3d50b415e56783917a926ab6
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-27 15:59:45 +0200

    31_59_treballant  ∾ prgtest autocommit 31_59 #2

--- 31_59_taula_multiplicar/TaulaMultiplicar.java
+++ 31_59_taula_multiplicar/TaulaMultiplicar.java
@@ -1,22 +1,22 @@
 //multiplicaciones 
 public class TaulaMultiplicar {
     public static void main(String[] args) {
-        System.out.println("El primer argument és " + args[1]);
-        int primer = Integer.parseInt(args[1]);
-        System.out.println("El segon argument és  " + args[2]);
-        int segon = Integer.parseInt(args[2]);
-        System.out.println("El tercer argument és " + args[3]);
-        int tercer = Integer.parseInt(args[3]);
-        System.out.println("El quart argument és  " + args[4]);
-        int quart = Integer.parseInt(args[4]);
+        System.out.println("El primer argument és " + args[0]);
+        int primer = Integer.parseInt(args[0]);
+        System.out.println("El segon argument és  " + args[1]);
+        int segon = Integer.parseInt(args[1]);
+        System.out.println("El tercer argument és " + args[2]);
+        int tercer = Integer.parseInt(args[2]);
+        System.out.println("El quart argument és  " + args[3]);
+        int quart = Integer.parseInt(args[3]);
         if (primer >= segon) {
-            for (int uno = Integer.parseInt(args[1]); uno <= segon; uno++) { 
+            for (int uno = Integer.parseInt(args[0]); uno <= segon; uno++) { 
                 System.out.println(uno + " x " + tercer + " = " + uno*tercer);
                     System.out.println(uno + " x " + quart + " = " + uno*quart);
             }
         }
         else if (primer < segon) {
-        for (int dos = Integer.parseInt(args[2]); dos <= primer; dos++) {
+        for (int dos = Integer.parseInt(args[1]); dos <= primer; dos++) {
             System.out.println(dos + " x " + tercer + " = " + dos*tercer);
                 System.out.println(dos + " x " + quart + " = " + dos*quart);
         }
