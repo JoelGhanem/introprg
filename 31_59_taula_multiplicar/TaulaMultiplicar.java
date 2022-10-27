e1b6da9c3aac6a839390983c85b3240caffc492d
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-27 16:12:30 +0200

    31_59_treballant  ∾ prgtest autocommit 31_59 #5

--- 31_59_taula_multiplicar/TaulaMultiplicar.java
+++ 31_59_taula_multiplicar/TaulaMultiplicar.java
@@ -1,13 +1,9 @@
 //multiplicaciones 
 public class TaulaMultiplicar {
     public static void main(String[] args) {
-        System.out.println("El primer argument és " + args[0]);
         int primer = Integer.parseInt(args[0]);
-        System.out.println("El segon argument és  " + args[1]);
         int segon = Integer.parseInt(args[1]);
-        System.out.println("El tercer argument és " + args[2]);
         int tercer = Integer.parseInt(args[2]);
-        System.out.println("El quart argument és  " + args[3]);
         int quart = Integer.parseInt(args[3]);
         if (primer >= segon) {
             if (tercer != quart) {
