14b3ca7e550dc89e1ac9ab26900cb47df401a8e8
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-10-29 18:55:09 +0200

    31_61_enproceso

---
+++ 31_61_informestring/InformeString.java
@@ -0,0 +1,19 @@
+// Programa que te ayuda con los strings
+public class InformeString {
+    public static void main(String[] args) {
+        System.out.println("Text principal");
+        String primer = Entrada.readLine();
+        System.out.println("Text secundari");
+        String segon = Entrada.readLine();
+        System.out.println("Número positiu?");
+        int num = Integer.parseInt(Entrada.readLine());
+        System.out.println("\"" + primer + "\" .length(): " + primer.length());
+        System.out.println("\"" + primer + "\" .starstWith(" + segon + "): " + primer.startsWith(segon));
+        System.out.println("\"" + primer + "\" .endstWith(" + segon + "): " + primer.endsWith(segon));
+        System.out.println("\"" + primer + "\" .equals(" + segon + "): " + primer.equals(segon));
+        System.out.println("\"" + primer + "\" .equalsIgnoreCase(" + segon + "): " + primer.equalsIgnoreCase(segon));
+        System.out.println("\"" + primer + "\" .isBlank(): " + primer.isBlank());
+        System.out.println("\"" + primer + "\" .isEmpty(): " + primer.isEmpty());
+    }
+}
+
