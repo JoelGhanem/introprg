ae0f436bf12509033416a9931afbc32694ca7f38
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-31 18:46:27 +0100

    31_63_fet  ∾ prgtest autocommit 31_63 #10

--- 31_63_manipulant_strings/ManipulaString.java
+++ 31_63_manipulant_strings/ManipulaString.java
@@ -9,7 +9,7 @@ public class ManipulaString {
         System.out.println("La penúltima lletra és '" + paraula.charAt(paraula.length()-2) + "'");
         System.out.println("La paraula en majúscules és \"" + paraula.toUpperCase() + "\"");
         System.out.println("La composició de quatre primeres és \"" + paraula.substring(0,4) + "\"");
-        System.out.println("La composició de quatre  és \"" + paraula.charAt(paraula.length()-1) + paraula.substring(0,3) + "\"");
+        System.out.println("La composició de quatre és \"" + paraula.charAt(paraula.length()-1) + paraula.substring(0,3) + "\"");
 
     }
 }
