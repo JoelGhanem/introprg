7cf7b1e01ba7e118af97932136c88ba4fa8dbf09
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-31 18:36:38 +0100

    31_63_fet  ∾ prgtest autocommit 31_63 #3

--- 31_63_manipulant_strings/ManipulaString.java
+++ 31_63_manipulant_strings/ManipulaString.java
@@ -4,8 +4,8 @@ public class ManipulaString {
         System.out.println("Paraula?");
         String  paraula = Entrada.readLine();
         System.out.println("La longitud és " + paraula.length());
-        System.out.println("La segona lletra és " + paraula.charAt(1));
-        System.out.println("La darrera lletra és " + paraula.charAt(paraula.length()-1));
+        System.out.println("La segona lletra és '" + paraula.charAt(1) + "'");
+        System.out.println("La darrera lletra és " + paraula.charAt(paraula.length()-1) + "'");
         //la penultima
         System.out.println("La paraula en majúscules és " + paraula.toUpperCase());
 
