171a7fbb433a31e65594d11daa9d760bc10b6290
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-10-31 18:11:34 +0100

    31_62_fet  ∾ prgtest autocommit 31_62 #4

--- 31_62_informecaracter/InformeCaracter.java
+++ 31_62_informecaracter/InformeCaracter.java
@@ -8,7 +8,15 @@ public class InformeCaracter {
             System.out.println("Caràcter buit");
         }else {
             System.out.println("Character.getName('" + uno + "'): " + Character.getName(uno));
-            
+            System.out.println("Character.isDigit('" + uno + "'): " + Character.isDigit(uno));
+            System.out.println("Character.isJavaIdentifierStart('" + uno + "'): " + Character.isJavaIdentifierStart(uno));
+            System.out.println("Character.isJavaIdentifierPart('" + uno + "'): " + Character.isJavaIdentifierPart(uno));
+            System.out.println("Character.isLetter('" + uno + "'): " + Character.isLetter(uno));
+            System.out.println("Character.isLowerCase('" + uno + "'): " + Character.isLowerCase(uno));
+            System.out.println("Character.isUpperCase('" + uno + "'): " + Character.isUpperCase(uno));
+            System.out.println("Character.isWhitespace('" + uno + "'): " + Character.isWhitespace(uno));
+            System.out.println("Character.toLowerCase('" + uno + "'): " + Character.toLowerCase(uno));
+            System.out.println("Character.toUpperCase('" + uno + "'): " + Character.toUpperCase(uno));
         }
     }
 }
