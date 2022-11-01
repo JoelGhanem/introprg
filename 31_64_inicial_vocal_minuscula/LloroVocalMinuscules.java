1de434a668dab6ec043f3f0b4ebb31e33ea5f6e9
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-01 14:13:07 +0100

    31_64_fet  ∾ prgtest autocommit 31_64 #1

--- 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
+++ 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -5,14 +5,16 @@ public class LloroVocalMinuscules {
         String paraula = Entrada.readLine();
         char primera = paraula.charAt(0);
         String paraulaLloro;
-        if (Character.isLowerCase(primera)){
+        if (paraula.isEmpty() == false){
+            if (Character.isLowerCase(primera)){
                 while ((paraula.isEmpty() == false)  && (paraula.isBlank() == false))  {
                     paraulaLloro = paraula; 
                     System.out.println("El lloro repeteix: " + paraulaLloro);
                     System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules:");
                     paraula = Entrada.readLine();
                 }
+            }
         }
-                System.out.println("Adéu");
+        System.out.println("Adéu");
     }
 }
