e009acd74f24358b35d300db359ba5982ae33950
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-11-01 14:12:00 +0100

    31_64_fet

---
+++ 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -0,0 +1,18 @@
+//Programa del loro pero mas tiquismiquis
+public class LloroVocalMinuscules {
+    public static void main(String[] args) {
+        System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
+        String paraula = Entrada.readLine();
+        char primera = paraula.charAt(0);
+        String paraulaLloro;
+        if (Character.isLowerCase(primera)){
+                while ((paraula.isEmpty() == false)  && (paraula.isBlank() == false))  {
+                    paraulaLloro = paraula; 
+                    System.out.println("El lloro repeteix: " + paraulaLloro);
+                    System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules:");
+                    paraula = Entrada.readLine();
+                }
+        }
+                System.out.println("Adéu");
+    }
+}
