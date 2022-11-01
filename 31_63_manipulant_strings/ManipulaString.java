219c56c108f66b1b7fbe6a97cc7e8bcc6e037c4b
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-01 13:43:43 +0100

    31_63_fet  ∾ prgtest autocommit 31_63 #17

--- 31_63_manipulant_strings/ManipulaString.java
+++ 31_63_manipulant_strings/ManipulaString.java
@@ -3,9 +3,7 @@ public class ManipulaString {
     public static void main(String[] args) {
         System.out.println("Paraula?");
         String  paraula = Entrada.readLine();
-        if (paraula.length() == 0) {
-            System.out.println("caràcter buit");
-        }
+        if (paraula.length() != 0) {
         System.out.println("La longitud és " + paraula.length());
         System.out.println("La segona lletra és '" + paraula.charAt(1) + "'");
         System.out.println("La darrera lletra és '" + paraula.charAt(paraula.length()-1) + "'");
@@ -16,5 +14,9 @@ public class ManipulaString {
         char mayus = paraula.charAt(paraula.length()-1);
         String minus = paraula.substring(0,3);
         System.out.println("La composició de quatre corregint majúscules és \"" + Character.toUpperCase(mayus) + minus.toLowerCase() + "\"");
+        }
+        else { 
+            System.out.println("Caràcter buit");
+        }
     }
 }
