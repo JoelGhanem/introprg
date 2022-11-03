9449cbb058ea8ac3c050eda37b566cb4394b9427
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-03 16:04:53 +0100

    31_67_fet  ∾ prgtest autocommit 31_67 #1

--- 31_67_segona_igual_penultima/SegonaIgualPenultima.java
+++ 31_67_segona_igual_penultima/SegonaIgualPenultima.java
@@ -3,11 +3,13 @@ public class SegonaIgualPenultima {
     public static void main(String[] args) {
         System.out.println("Ves introduïnt texts (finalitza amb enter sol)");
         String paraula = Entrada.readLine();
-        if (paraula.charAt(1) != paraula.charAt(paraula.length()-2)) {
-            System.out.println("Segona diferent de penúltima");
-        }
-        else if (paraula.charAt(1) == paraula.charAt(paraula.length()-2)) {
-            System.out.println("Segona igual a penúltima");
+        while (paraula.isEmpty() == false) {
+            if (paraula.charAt(1) != paraula.charAt(paraula.length()-2)) {
+                System.out.println("Segona diferent de penúltima");
+            }
+            else if (paraula.charAt(1) == paraula.charAt(paraula.length()-2)) {
+                System.out.println("Segona igual a penúltima");
+            }
         }
     }
 }
