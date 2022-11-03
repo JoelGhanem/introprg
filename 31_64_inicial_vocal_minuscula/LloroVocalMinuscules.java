58948b8c7dab460e4a840f736485ed084f898089
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-03 11:30:31 +0100

    Merge branch 'main' of github.com:JoelGhanem/introprg  ∾ prgtest autocommit 31_64 #11

--- 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
+++ 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -3,6 +3,11 @@ public class LloroVocalMinuscules {
     public static void main (String[] args) {
         String paraula;
         char uno;
+        char a = 'a';
+        char e = 'e';
+        char i = 'i';
+        char o = 'o'; 
+        char u = 'u';
         String paraulaLloro;
         System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
         paraula = Entrada.readLine();
@@ -10,15 +15,13 @@ public class LloroVocalMinuscules {
             uno = paraula.charAt(0);
             if (Character.isLowerCase(uno)) {
                 while ((paraula.isEmpty() == false) && (paraula.isBlank() == false)) {
-                    paraulaLloro = paraula; 
-                    uno = paraula.charAt(0);
-                    if (Character.isLowerCase(uno)) {
+                    if (uno == 'a' || uno == 'e' || uno == 'i' || uno == 'o' || uno == 'u') {
+                        paraulaLloro = paraula; 
+                        uno = paraula.charAt(0);
                         System.out.println("El lloro diu: " + paraulaLloro);
                     }
                     System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
                     paraula = Entrada.readLine();
-
-
                 }
             }
         }
