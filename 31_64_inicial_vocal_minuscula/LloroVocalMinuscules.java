191ec16841b9ccee74e77e13cdd366fb8aaf3eac
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-03 11:05:16 +0100

    Merge branch 'main' of github.com:JoelGhanem/introprg  ∾ prgtest autocommit 31_64 #8

--- 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
+++ 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -1,30 +1,24 @@
-//Programa del loro pero mas tiquismiquis
+// loro jodido
 public class LloroVocalMinuscules {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        String paraula;
+        char uno;
+        String paraulaLloro;
         System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
-        String paraula = Entrada.readLine();
-        char primera;
-        String paraulaLloro = ("loro");
-        if (paraula.isEmpty() == false){
-            primera = paraula.charAt(0);
-            if (Character.isUpperCase(primera) == true){
-                System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
-            }
-            else if (Character.isUpperCase(primera) == false) {
-                while ((paraula.isEmpty() == false)  && (paraula.isBlank() == false))  {
-                    paraula = Entrada.readLine();
-                    if (paraula.isEmpty() == false) {
-                        primera = paraula.charAt(0);
-                        if (Character.isLowerCase(primera)) {
-                            paraula = Entrada.readLine();
-                            paraulaLloro = paraula; 
-                        }
-                        System.out.println("El lloro diu: " + paraulaLloro);
-                        System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
-                    } 
+        paraula = Entrada.readLine();
+        uno = paraula.charAt(0);
+        if (Character.isLowerCase(uno)) {
+            while ((paraula.isEmpty() == false) && (paraula.isBlank() == false)) {
+                paraulaLloro = paraula; 
+                uno = paraula.charAt(0);
+                if (Character.isLowerCase(uno)) {
+                    System.out.println("El lloro diu: " + paraulaLloro);
                 }
+                System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
+                paraula = Entrada.readLine();
+
+
             }
         }
-        System.out.println("Adéu");
     }
 }
