845d7305f7c7ce35cb4f8619df7f4d177efbe6c4
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-11-03 11:46:35 +0100

    31_65_fet

---
+++ 31_65_acaba_vocal_minuscula/LloroVocalMinuscules.java
@@ -0,0 +1,30 @@
+// loro jodido
+public class LloroVocalMinuscules {
+    public static void main (String[] args) {
+        String paraula;
+        char fin;
+        String paraulaLloro;
+        System.out.println("El lloro pregunta paraula que finalitzi amb vocal en minúscules");
+        paraula = Entrada.readLine();
+        if (paraula.isEmpty() == false) {
+            fin = paraula.charAt(0);
+            if (Character.isLowerCase(fin)) {
+                while ((paraula.isEmpty() == false) && (paraula.isBlank() == false)) {
+                    paraulaLloro = paraula; 
+                    fin = paraula.charAt(paraula.length()-1);
+                    if (Character.isLowerCase(fin)) {
+                        if (fin == 'a' || fin == 'e' || fin == 'i' || fin == 'o' || fin == 'u') {
+                            System.out.println("El lloro diu: " + paraulaLloro);
+                        }
+                    }
+                    System.out.println("El lloro pregunta paraula que finalitzi amb vocal en minúscules");
+                    paraula = Entrada.readLine();
+
+
+                }
+            }
+        }
+        System.out.println("Adéu");
+    }
+}
+
