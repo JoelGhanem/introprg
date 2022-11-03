70d581cca3a4fbdde8683b68eed3fcd2fc7ed3fb
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-11-03 11:56:50 +0100

    31_66_fet

---
+++ 31_66_acaba_lletra/LloroAcabaLletra.java
@@ -0,0 +1,31 @@
+// loro jodido
+public class LloroAcabaLletra {
+    public static void main (String[] args) {
+        String paraula;
+        char fin;
+        String paraulaLloro;
+        System.out.println("El lloro pregunta paraula que finalitzi per vocal en minúscules");
+        paraula = Entrada.readLine();
+        if (paraula.isEmpty() == false) {
+            fin = paraula.charAt(paraula.length()-1);
+            if (Character.isLetter(fin)) {
+                while ((paraula.isEmpty() == false) && (paraula.isBlank() == false)) {
+                    paraulaLloro = paraula; 
+                    fin = paraula.charAt(paraula.length()-1);
+                    if (Character.isLetter(fin)) {
+                        if (fin != 'a' || fin != 'e' || fin != 'i' || fin != 'o' || fin != 'u' || fin != 'A' || fin != 'E' || fin != 'O' || fin != 'U') {
+                            System.out.println("El lloro diu: " + paraulaLloro);
+                        }
+                    }
+                    System.out.println("El lloro pregunta paraula que finalitzi per vocal en minúscules");
+                    paraula = Entrada.readLine();
+
+
+                }
+            }
+        }
+        System.out.println("Adéu");
+    }
+}
+
+
