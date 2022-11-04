a1ab39322a30eb0b767239b287420fdc6381cb4c
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-04 11:34:31 +0100

    31_70_fet  ∾ prgtest autocommit 31_70 #1

--- 31_70_analisi_caracter/AnalitzaCaracter.java
+++ 31_70_analisi_caracter/AnalitzaCaracter.java
@@ -8,24 +8,33 @@ public class AnalitzaCaracter {
         char car = text.charAt(text.length()-posicio);
         if (posicio >= 0) {
             car = text.charAt(posicio);
-        } 
-        else if (posicio < 0) {
+            if (posicio > text.length()) {
+                System.out.println("Fora de rang");
+            }
+            else if (Character.isLetter(car)) {
+                System.out.println("'" + car + "' és una lletra" );
+            }
+            else if (Character.isDigit(car)) {
+                System.out.println("'" + car + "' és un nombre" );
+            }
+            else if (Character.isLetter(car) == false) {
+                System.out.println("'" + car + "' és una altra cosa" );
+            }
+        }
+        if (posicio < 0) {
             car = text.charAt(text.length()-posicio);
-        }
-        else if (posicio > text.length()) {
-            System.out.println("Fora de rang");
-        }
-        else if (posicio < text.length()){
-            System.out.println("Fora de rang");
-        }
-        else if (Character.isLetter(car)) {
-            System.out.println("'" + car + "' és una lletra" );
-        }
-        else if (Character.isDigit(car)) {
-            System.out.println("'" + car + "' és un nombre" );
-        }
-        else if (Character.isLetter(car) == false) {
-            System.out.println("'" + car + "' és una altra cosa" );
+            if (posicio < text.length()){
+                System.out.println("Fora de rang");
+            }
+            else if (Character.isLetter(car)) {
+                System.out.println("'" + car + "' és una lletra" );
+            }
+            else if (Character.isDigit(car)) {
+                System.out.println("'" + car + "' és un nombre" );
+            }
+            else if (Character.isLetter(car) == false) {
+                System.out.println("'" + car + "' és una altra cosa" );
+            }
         }
     }
 }
