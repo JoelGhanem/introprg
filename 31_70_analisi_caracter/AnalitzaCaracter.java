b9d36f8e76ab39ff758c890ca80d924dec22f079
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-09 19:58:12 +0100

    31_70_fet  ∾ prgtest autocommit 31_70 #8

--- 31_70_analisi_caracter/AnalitzaCaracter.java
+++ 31_70_analisi_caracter/AnalitzaCaracter.java
@@ -3,7 +3,7 @@ public class AnalitzaCaracter {
     public static void main(String[] args) {
         System.out.println("Text?");
         String text = Entrada.readLine();
-        char car = 'J';
+        char car = text.charAt(0);
         System.out.println("Posició?");
         int posicio = Integer.parseInt(Entrada.readLine());
         int largo = text.length();
