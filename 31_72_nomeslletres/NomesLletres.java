c57150747bce537117434dc751e3f10e1c34ea4d
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-10 16:08:50 +0100

    31_72_fet  ∾ prgtest autocommit 31_72 #2

--- 31_72_nomeslletres/NomesLletres.java
+++ 31_72_nomeslletres/NomesLletres.java
@@ -7,7 +7,9 @@ public class NomesLletres {
             if (Character.isLetter(text.charAt(num))) {
             System.out.print(text.charAt(num));
             }
+            if (Character.isLetter(text.charAt(num))) {
             System.out.print(",");
+            }
         }
     }
 }
