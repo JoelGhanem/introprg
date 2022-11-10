bc71db58c098d8b24e3c0cda065df001bb65c269
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-11-10 17:13:54 +0100

    31_72_fet

--- 31_72_nomeslletres/NomesLletres.java
+++ 31_72_nomeslletres/NomesLletres.java
@@ -1,11 +1,12 @@
 // Descripcion de putisima madre
 public class NomesLletres {
     public static void main(String[] args) {
+        String limpia = "";
         System.out.println("Text?");
         String text = Entrada.readLine();
         for (int num = 0; num < text.length(); num++) {
             if (Character.isLetter(text.charAt(num))) {
-                String limpia = (Character.isLetter(text.charAt(num)) == true);
+                limpia = limpia + text.charAt(num); 
                 System.out.print(limpia);
                 for (int numero = 0; num < limpia.length(); numero ++) {
                     System.out.print(", ");
