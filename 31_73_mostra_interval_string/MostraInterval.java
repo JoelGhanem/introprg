70d929ca17d96be5fb772b1b86d16df1f90b6535
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-14 15:44:44 +0100

    31_73_fet  ∾ prgtest autocommit 31_73 #22

--- 31_73_mostra_interval_string/MostraInterval.java
+++ 31_73_mostra_interval_string/MostraInterval.java
@@ -10,9 +10,6 @@ public class MostraInterval {
         if (fi > text.length()) {
             fi = (text.length()-1);
         }
-        if (fi < 0) {
-            fi = 0;
-        }
         if (inici < 0) {
             for (int num = 0; num <= fi; num ++) {
                 System.out.println(text.charAt(num));
