f35a2f3671bf10fcf61903f49a8b8fef8428f818
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-11-14 18:20:01 +0100

    31_74_fet

---
+++ 31_74_cadena_continua/CadenaContinua.java
@@ -0,0 +1,15 @@
+//Desenvolupa un programa que demani un text i un nombre enter, i mostri tants caràcters del text com indiqui el nombre enters, començant pel primer. En cas que en faltin, el programa tornarà a mostrar el text a partir del primer caràcter fins que hagi aconseguit tots els caràcters demanats.
+public class CadenaContinua {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        System.out.println("Nombre?");
+        int nombre = Integer.parseInt(Entrada.readLine());
+        for (int num = 0; num <= nombre; num ++) {
+            if (num > nombre) {
+                num = 0;
+                System.out.println(text.charAt(num));
+            }
+        }
+    }
+}
