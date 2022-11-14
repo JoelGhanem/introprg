4c0298bd73a1192ae0e193b2e8fe32f355d31f95
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-11-14 15:17:49 +0100

    31_73_fet

---
+++ 31_73_mostra_interval_string/MostraInterval.java
@@ -0,0 +1,26 @@
+//desenvolupa un programa que demani un text i dos valors enters, i que mostri tots els caràcters que hi ha entre el primer i el segon, en l'ordre marcat per l'entrada.
+public class MostraInterval {
+    public static void main(String[] args) {
+        System.out.println("text?");
+        String text = Entrada.readLine();
+        System.out.println("inici?");
+        int inici = Integer.parseInt(Entrada.readLine());
+        System.out.println("final?");
+        int fi = Integer.parseInt(Entrada.readLine());
+        if (inici < fi) {
+            for (int num = inici; num <= fi; num ++) {
+                System.out.println(text.charAt(num));
+            }
+        }
+        else if (fi >= text.length()) {
+            for (int num = inici; num < text.length(); num ++) {
+                System.out.println(text.charAt(num));
+            }
+        }
+        else if (fi > inici) { 
+            for (int num = fi; num > inici; num --) {
+                System.out.println(text.charAt(num));
+            }
+        }
+    }
+}
