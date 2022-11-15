f6341c94f217c805781ee7248c767e86777d9a2e
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-15 16:11:58 +0100

    31_78_fet  ∾ prgtest autocommit 31_78 #13

--- 31_78_vocalsmajuscules/VocalsMajuscules.java
+++ 31_78_vocalsmajuscules/VocalsMajuscules.java
@@ -7,6 +7,7 @@ public class VocalsMajuscules {
         for (int num = 0; num < text.length();num++) {
         arreglao = text;
             if(text.charAt(num) == 'a' || text.charAt(num) == 101 || text.charAt(num) == 105 || text.charAt(num) == 111 || text.charAt(num) == 117) {
+                Character.toUpperCase(num);
                 arreglao = Character.toString(text.charAt(num));
             }else {
                 Character.toLowerCase(num);
