e8a8c919e8630576f05a1a9724858a40e1d9dfd8
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-15 10:58:38 +0100

    31_72_fet  ∾ prgtest autocommit 31_72 #4

--- 31_72_nomeslletres/NomesLletres.java
+++ 31_72_nomeslletres/NomesLletres.java
@@ -4,12 +4,16 @@ public class NomesLletres {
         String limpia;
         System.out.println("Text?");
         String text = Entrada.readLine();
+        int check = 0;
         if (Character.isLetter(text.charAt(0))) {
             System.out.print(text.charAt(0));
+            check = 1;
         }
         for (int num = 1; num < text.length(); num++) {
             if (Character.isLetter(text.charAt(num))) {
-                System.out.print(", " + text.charAt(num));
+                if (check == 1) {
+                    System.out.print(", " + text.charAt(num));
+                }
             }
         }
     }
