b3b5f3a2ff9f5e3d3324d8eb2aaed366b27d2df8
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-11-15 12:13:57 +0100

    31_76_fet

---
+++ 31_76_cerca_x/ConteDarrer.java
@@ -0,0 +1,18 @@
+//Desenvolupa un programa que vagi demanant texts fins que un no contingui el darrer caràcter del text anterior. El primer text és acceptat sempre a menys que estigui buit.
+public class ConteDarrer {
+    public static void main(String[] args) {
+        System.out.println("Introdueix texts (enter sol per finalitzar)");
+        String text = Entrada.readLine();
+        boolean conte = false;
+        char ultimo = text.charAt(text.length()-1);
+        if (!text.isEmpty()) {
+            for (int num = 0; num < text.length();num++) {
+                if (text.charAt(num) == ultimo) {
+                    System.out.println("bé");
+                    break;
+                }
+            }
+        }
+        System.out.println("Adéu");
+    }
+}  
