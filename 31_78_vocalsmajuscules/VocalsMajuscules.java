89e4d77f95528ab83ace54e1747e24b84ebe5c94
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-15 16:23:22 +0100

    31_78_fet  ∾ prgtest autocommit 31_78 #15

--- 31_78_vocalsmajuscules/VocalsMajuscules.java
+++ 31_78_vocalsmajuscules/VocalsMajuscules.java
@@ -4,11 +4,12 @@ public class VocalsMajuscules {
         System.out.println("Text?");
         String text = Entrada.readLine();
         String arreglao = "";
+        char algo;
         for (int num = 0; num < text.length();num++) {
         arreglao = text;
             if(text.charAt(num) == 'a' || text.charAt(num) == 101 || text.charAt(num) == 105 || text.charAt(num) == 111 || text.charAt(num) == 117) {
-                Character.toUpperCase(num);
-                arreglao = arreglao + text.charAt(num);
+                algo = Character.toUpperCase(text.charAt(num));
+                arreglao = arreglao + algo;
             }else {
                 Character.toLowerCase(num);
             }
