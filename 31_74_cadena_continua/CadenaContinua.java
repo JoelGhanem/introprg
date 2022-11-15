ec6cb36c63425acec47f9f46db3ad3cb86771d84
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-11-15 20:12:07 +0100

    31_74_fet

--- 31_74_cadena_continua/CadenaContinua.java
+++ 31_74_cadena_continua/CadenaContinua.java
@@ -12,14 +12,14 @@ public class CadenaContinua {
         else {
             System.out.println("Nombre?");
             numero = Integer.parseInt(Entrada.readLine());
-            for (int num = 0; num <= numero; num ++) {
+            for (int num = 1; num <= numero; num ++) {
                 cantidad = cantidad + 1;
                 if (num > largo) {
-                    num = 0;
+                    num = 1;
                     break;
                 }
                 if (cantidad <= numero) {
-                    System.out.print(text.charAt(num));
+                    System.out.print(text.charAt(num-1));
                 }
             }
         }
