2e5c05c0c0e4f97909cf7ce660b0290679e72fe9
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-15 23:24:19 +0100

    31_74_fet  ∾ prgtest autocommit 31_75 #7

--- 31_75_es_enter/EsEnter.java
+++ 31_75_es_enter/EsEnter.java
@@ -6,20 +6,27 @@ public class EsEnter {
         int largo = numero.length();
         char cero = numero.charAt(0);
         boolean check = false;
+        int numSimbols = 0;
         while (!numero.isEmpty()) {
             for (int num = 0; num < largo; num ++) {
-                if (Character.isLetter(numero.charAt(num))) {
-                    check = false;
-                }else {
-                    check = true;
+                if (numero.charAt(num) == '+' || numero.charAt(num) == '-') {
+                    numSimbols = numSimbols + 1;
+                    if (Character.isLetter(numero.charAt(num))) {
+                        check = false;
+                    }else {
+                        check = true;
+                    }
                 }
+                if (numSimbols > 1) {
+                    System.out.println("No és enter");
+                }
+                else if (check == true) {
+                    System.out.println("És enter");
+                } else  {
+                    System.out.println("No és enter");
+                }
+                numero = Entrada.readLine();
             }
-            if (check == true) {
-                System.out.println("És enter");
-            } else  {
-                System.out.println("No és enter");
-            }
-            numero = Entrada.readLine();
         }
         System.out.println("Adéu");
     }
