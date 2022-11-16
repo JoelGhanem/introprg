a1dcb3d4820cea724da81fb21bd9264840ca4a26
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-16 20:35:58 +0100

    31_79_fet  ∾ prgtest autocommit 31_79 #2

--- 31_79_comptavocals/ComptaVocals.java
+++ 31_79_comptavocals/ComptaVocals.java
@@ -5,14 +5,15 @@ public class ComptaVocals {
         String text = Entrada.readLine();
         String vocals = ("aeiouàéèíïóòúü");
         int numero = 0;
+        int numeroFinal = 0;
         for (int i = 0;i < text.length();i++) {
             for (int j = 0;j < vocals.length(); j++) {
                 if (text.charAt(i) == vocals.charAt(j)) {
                     numero +=1;
-                    System.out.println(numero);
                 }
             }
         }
+        System.out.println(numero);
     }
 }
 
