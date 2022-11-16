1c40600c93eed09b76b1568cca991be736696b78
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-16 22:11:55 +0100

    31_76_fet  ∾ prgtest autocommit 31_76 #4

--- 31_76_cerca_x/ConteDarrer.java
+++ 31_76_cerca_x/ConteDarrer.java
@@ -4,14 +4,16 @@ public class ConteDarrer {
         System.out.println("Introdueix texts (enter sol per finalitzar)");
         String text = Entrada.readLine();
         System.out.println("bé");
+        text = Entrada.readLine();
+        if (!text.isEmpty()) {
+            text = text.toLowerCase();
+        }
         char ultimaLetra = text.charAt(text.length()-1);
         boolean correcto = true;
         while (correcto == true) {
-            text = Entrada.readLine();
-            if (!text.isEmpty()) {
-                text = text.toLowerCase();
-            }
+            // Paso el boolean a falso para asegurasrme de q esta bien hecho
             correcto = false;
+            // Hago un for para que me revise todas las letras en busca de la ultima letra
             for (int i = 0; i < text.length(); i++) {
                 if (text.charAt(i) == ultimaLetra) {
                     correcto = true;
@@ -19,11 +21,16 @@ public class ConteDarrer {
                     correcto = false;
                 }
             }
+            //Si una de las letras es la ultima de antes 
             if (correcto == true) {
                 System.out.println("bé");
+                //Vuelvo a pedir el texto 
                 text = Entrada.readLine();
+                // paso el texto a minusculas
                 text = text.toLowerCase();
+                // Vuelvo a declarar la ultima letra
                 ultimaLetra = text.charAt(text.length()-1);
+                // Si no es correcto acaba
             }else {
                 break;
             }
