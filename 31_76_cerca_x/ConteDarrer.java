370fc1c5750de56418426786c29ad2ff6e07dca9
prgtest <moises.gomez@iesjoandaustria.org>
2022-11-17 16:09:24 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 31_76 #2

--- 31_76_cerca_x/ConteDarrer.java
+++ 31_76_cerca_x/ConteDarrer.java
@@ -3,35 +3,26 @@ public class ConteDarrer {
     public static void main(String[] args) {
         System.out.println("Introdueix texts (enter sol per finalitzar)");
         String text = Entrada.readLine();
-        System.out.println("bé");
         char ultimaLetra = text.charAt(text.length()-1);
         boolean correcto = true;
-        while (correcto == true) {
-            text = Entrada.readLine();
-            // Mientras el texto no est'e vacio pasalo a minusculas
-            if (!text.isEmpty()) {
-                // paso el texto a minusculas
-                text = text.toLowerCase();
-                // Vuelvo a declarar la ultima letra
-                ultimaLetra = text.charAt(text.length()-1);
-                System.out.println(ultimaLetra);
-            }
-            // Paso el boolean a falso para asegurasrme de q esta bien hecho
-            correcto = false;
-            // Hago un for para que me revise todas las letras en busca de la ultima letra
-            for (int i = 0; i < text.length(); i++) {
-                if (text.charAt(i) == ultimaLetra) {
-                    correcto = true;
-                    break;
-                }else {
-                    correcto = false;
-                }
-            }
-            //Si una de las letras es la ultima de antes 
-            if (correcto == true) {
+        if (text.isEmpty()) {
+            System.out.println("Adéu");
+        } else {
+            while (correcto == true) {
                 System.out.println("bé");
-            }else {
-                break;
+                text = Entrada.readLine(); // paso el texto a minusculas
+                text = text.toLowerCase(); // Vuelvo a declarar la ultima letra
+                ultimaLetra = text.charAt(text.length()-1);
+                System.out.println(ultimaLetra); // Hago un for para que me revise todas las letras en busca de la ultima letra
+                for (int i = 0; i < text.length(); i++) {
+                    if (text.charAt(i) == ultimaLetra) {
+                        correcto = true;
+                        ultimaLetra = text.charAt(text.length()-1);
+                        break;
+                    }else {
+                        correcto = false;
+                    }
+                }
             }
         }
         System.out.println("Adéu");
