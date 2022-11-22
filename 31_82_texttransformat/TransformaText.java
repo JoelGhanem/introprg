ff3ea612c7f089714017875c1efdd9c6053b315a
Joel <cf22joel.ghanem@iesjoandaustria.org>
2022-11-22 16:11:32 +0100

    31_82_fet

---
+++ 31_82_texttransformat/TransformaText.java
@@ -0,0 +1,49 @@
+//Desenvolupa un programa que demani un text i mostri una versió transformada segons les següents regles:
+public class TransformaText {
+    public static void main (String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        String vocalsCatalanes = ("aeiouàèéíïòóúü");
+        String numero = ("1234567890");
+        String cadena = "";
+        String vocales = "";
+        char vocal;
+        char letra;
+        boolean check = false;
+        boolean hayNum = false;
+        text.toLowerCase();
+        // un for  que recorre todo el text
+        for (int i = 0; i < text.length(); i++) {
+            // un for que recorre vocalsCatalanes para comprobar si aparecen
+            for (int j = 0; j < vocalsCatalanes.length(); j++) {
+                if (text.charAt(i) == vocalsCatalanes.charAt(j)) {
+                    check = true;
+                    break;
+                } else {
+                    check = false;
+                }
+            }
+            if (check) {
+                vocal = text.charAt(i);
+                vocales = vocales + vocal;
+                vocales = vocales.toLowerCase();
+                cadena = cadena + vocales;
+            } 
+            for (int j = 0; j < numero.length(); j++) {
+                if (text.charAt(i) == numero.charAt(j)) {
+                    hayNum = true;
+                } else {
+                    hayNum = false;
+                }
+            }
+            if (hayNum) {
+                cadena = "(" + numero + ")";
+            } 
+            if (Character.isLetter(text.charAt(i)) || Character.isDigit(text.charAt(i))) {
+                letra = text.charAt(i);
+                cadena = cadena + letra;
+            }
+        }
+        System.out.println(cadena);
+    }
+}
