d8056e11281c14ea7ed6b6e4d35499e72216c36d
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-22 15:07:28 +0100

    31_58_fet  ∾ prgtest autocommit 31_81 #4

--- 31_81_inicialsmajuscules/MajusculitzaInicials.java
+++ 31_81_inicialsmajuscules/MajusculitzaInicials.java
@@ -11,10 +11,6 @@ public class MajusculitzaInicials {
             cadena = cadena + letra;
         } 
         for (int i = 0; i < text.length();i++) {
-            if (Character.isLetter(text.charAt(0))) {
-                letra = Character.toLowerCase(text.charAt(0));
-                cadena = cadena + letra;
-            } 
             // Si no es letra haces la sigueinte mayuscula
             if (!Character.isLetter(text.charAt(i))) {
                 mayus = Character.toUpperCase(text.charAt(i + 1));
