ed72bf8f341367047d0f9b6343fad14e18af3a5e
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2022-11-24 18:20:51 +0100

    32_03_fet  ∾ prgtest autocommit 32_03 #1

--- 32_03_quadrats_modulars/Quadrats.java
+++ 32_03_quadrats_modulars/Quadrats.java
@@ -1,22 +1,26 @@
 //Desenvolupa una nova versió del programa anterior. Aquest cop, des de dibuixaQuadrats() es cridarà un mòdul anomenat dibuixaQuadrat() que farà la feina de dibuixar les difents línies del quadrat fent servir dibuixaLinia()
 public class Quadrats {
     public static void main(String[] args) {
+        dibuixaQuadrats();
         dibuixaQuadrat();
     }
-    public static void dibuixaQuadrat() {
+    public static void dibuixaQuadrats() {
         System.out.println("Quants?");
         String texto = Entrada.readLine();
         if (!texto.isEmpty()) {
             int quants = Integer.parseInt(texto);
             if (quants > 0) {
                 for (int q = 0; q < quants; q++) {
-                    for (int li = 0; li < 5; li++) { 
-                    dibuixaLinia();
-                    }
-                System.out.println();
+                    dibuixaQuadrat();
                 }
             }
         }
+    }
+    public static void dibuixaQuadrat() {
+        for (int li = 0; li < 5; li++) { 
+            dibuixaLinia();
+        }
+        System.out.println();
     }
     public static void dibuixaLinia() {
         for (int co = 0; co < 5; co ++) {
