21f298d4a55aaef349a4208a9f2b98e8864c1257
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-11-26 16:11:02 +0100

    32_05_fet

---
+++ 32_05_quadrat_parametritzat/Quadrat.java
@@ -0,0 +1,15 @@
+//Desenvolupa un programa que esperi un valor enter positiu en args[0] i mostri un quadrat de " X" amb el costat indicat pel valor rebut.
+public class Quadrat {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(args[0]);
+        for (int li = 0; li < numero; li++) {
+            dibuixaLinia(numero);
+            System.out.println();
+        }
+    }
+    public static void dibuixaLinia(int numero) {
+        for (int co = 0; co < numero; co++) {
+            System.out.print(" X");
+        }
+    }
+}
