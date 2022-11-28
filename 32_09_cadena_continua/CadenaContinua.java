e483759cdd8a2256bf5d702b4813022ce38cc533
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-11-28 08:39:55 +0100

    32_09_fet

---
+++ 32_09_cadena_continua/CadenaContinua.java
@@ -0,0 +1,21 @@
+//Desenvolupa una nova versió de l'exercici que mostrava una cadena contínua.//
+public class CadenaContinua {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        System.out.println("Nombre?");
+        int longitud = Integer.parseInt(Entrada.readLine());
+        mostraCadenaContinua(text, longitud);
+    }
+    public static void mostraCadenaContinua(String text, int longitud) {
+        int contador = -1;
+        for (int i = 0; i < longitud; i++) {
+            contador = contador + 1;
+            if (contador > text.length()) {
+                contador = 0; 
+            }
+            System.out.print(text.charAt(contador));
+        }
+    }
+}
+
