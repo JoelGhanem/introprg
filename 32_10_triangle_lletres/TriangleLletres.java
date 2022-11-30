ac3bf1e830c2c4f6d751aac4ebde07be93013b89
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-11-30 19:15:30 +0100

    32_10_fet

---
+++ 32_10_triangle_lletres/TriangleLletres.java
@@ -0,0 +1,27 @@
+//Desenvolupa una nova versió de l'exercici que dibuixava un triangle de lletres.//
+public class TriangleLletres {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        dibuixaTriangle(text);
+    }
+    public static void dibuixaTriangle(String text) {
+        int linia = 1;
+        for (int i = 0; i < text.length(); i++) {
+            dibuixaLinia(text, linia);
+            linia += 1;
+            System.out.println();
+        }
+    }
+    public static void dibuixaLinia(String text, int linia) {
+        for (int j = 0; j <= linia; j++) {
+            if (j == linia) {
+                System.out.print(text.charAt(j));
+            } else {
+                System.out.print(text.charAt(j) + ", ");
+            }
+        }
+    }
+}
+
+
