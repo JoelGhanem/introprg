37cca466ee324b98a46e3c2c69786bda2e0abc84
prgtest <moises.gomez@iesjoandaustria.org>
2022-12-15 16:29:07 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 32_16 #1

--- 32_17_codifica/CodificaBasic.java
+++ 32_17_codifica/CodificaBasic.java
@@ -10,38 +10,3 @@ public class CodificaBasic {
         }
     }
     public static void codifica(String text, int quants) {
-        String cadena = "";
-        String accents = "áàéèíìóòúù";
-        char letra = text.charAt(0);
-        if (!text.isEmpty()) {
-            if (quants > 0) {
-                //Separo la primer letra para poder tratarla mejor
-                if (Character.isUpperCase(text.charAt(0))) {
-                    cadena = cadena + text.charAt(0);
-                } else {
-                    letra = ((char)(letra + quants));
-                    cadena = cadena + letra;
-                }
-                // Recorro toda la cadena para cambiar los valores
-                for (int i = 1; i < text.length(); i++) {
-                    letra = text.charAt(i);
-                    if (Character.isLetter(letra)) {
-                        if (letra >= 'z') {
-                            letra = ((char)96);
-                        } else {
-                            cadena = cadena + letra;
-                        }
-                    } else {
-                        cadena = cadena + letra;
-                    }
-                }
-                System.out.println(cadena);
-            } else if (quants == 0) {
-                System.out.print(text);
-            } else {
-                System.out.println("No s'accepten números negatius");
-            }
-        }
-    }
-}
-
