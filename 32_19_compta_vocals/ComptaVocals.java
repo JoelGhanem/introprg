4e719f89004330a21331387f7c6cdc86abc53433
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-12-16 05:51:23 +0100

    32_19_fet

---
+++ 32_19_compta_vocals/ComptaVocals.java
@@ -0,0 +1,24 @@
+//Considera la següent versió simplificada del programa ComptaAiEs en la que comptem les ocurrències de 'a' i 'e' però no les comparem://
+public class ComptaVocals {
+    public static void main (String[] args) {
+        System.out.println("Introdueix un text");
+        String text = Entrada.readLine();
+        String vocals = ("aàeéèiíïoóòuúü");
+        for (int j = 0; j < vocals.length(); j++) {
+            int num = quantesOcurrencies(text, vocals.charAt(j));
+        }
+    }
+    public static void mostraOcurrencies(char lletra, int quantes) {
+        System.out.println("nombre de '" + lletra + "'s: " + quantes);
+    }
+    public static int quantesOcurrencies(String text, char lletra) {
+        int comptador = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == lletra) {
+                comptador += 1;
+            }
+        }
+        return comptador;
+    }
+}
+
