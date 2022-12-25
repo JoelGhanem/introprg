8dd392fd6f1117f5a17ea338c565edad772d0802
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2022-12-25 20:34:10 +0100

    32_26_fet

--- 32_26_mostra_interval_string/MostraInterval.java
+++ 32_26_mostra_interval_string/MostraInterval.java
@@ -1,13 +1,13 @@
 /*Desenvolupo un programa que em dona un a secció d'un String segons el text i la posició que li donem*/
 public class MostraInterval {
     public static void main(String[] args) {
-        System.out.println("Text?");
+        System.out.println("text?");
         String text = Entrada.readLine();
         System.out.println("inici?");
         int inici = Integer.parseInt(Entrada.readLine());
         System.out.println("final?");
         int fi = Integer.parseInt(Entrada.readLine());
         String interval = UtilString.intervalString(text, inici, fi);
-        System.out.println("interval");
+        System.out.println(interval);
     }
 }
