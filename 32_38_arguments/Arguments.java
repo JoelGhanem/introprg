6be42ee47f2086c3635c32ccb7ec2af3b5364596
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-01-10 23:36:52 +0100

    malo  ∾ prgtest autocommit 32_38 #0

--- 32_38_arguments/Arguments.java
+++ 32_38_arguments/Arguments.java
@@ -1,30 +1,15 @@
 /*En aquest exercici consisteix en fer un programa que analitzi els texts que es passen per la línia de comandes i distingeixi si són o no enters.*/
 public class Arguments {
     public static void main(String[] args){
-        for (int k = 0; k < args.length; k++) {
-            String text = text + args[k];
-        }
-        if (text.isEmpty()){
+        if (args.length == 0) {
             System.out.println("Cap argument");
         } else {
-            String part = "";
-            for (int i = 0; i < text.length(); i++) {
-                if (Character.isWhitespace(text.charAt(i))) {
-                    for (int j = i + 1; j < text.length(); j++) {
-                        if (Character.isWhitespace(text.charAt(j))) {
-                            int inici = text.charAt(i);
-                            int fi = text.charAt(j);
-                            UtilString.intervalString(text, inici, fi);
-                            String interval = UtilString.intervalString(text, inici, fi);
-                            UtilString.esEnter(interval);
-                            int contador = 0;
-                            if (UtilString.esEnter(interval)) {
-                                System.out.println("[" + contador + "] \"" + interval + "\": és enter");
-                            } else {
-                                System.out.println("[" + contador + "] \"" + interval + "\": no és enter");
-                            }
-                        }
-                    }
+            String enter = "";
+            for (int i = 0; i < args.length; i++) {
+                if (UtilString.esEnter(args[i])) {
+                    System.out.println("[" + i + "] \"" + args[i] + "és enter");
+                } else {
+                    System.out.println("[" + i + "] \"" + args[i] + "\": no és enter");
                 }
             }
         }
