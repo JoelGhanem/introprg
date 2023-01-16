826feb70d71e2946b075c967a17551f497aad364
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-01-16 18:51:03 +0100

    32_45_en_proces  ∾ prgtest autocommit 32_45 #11

--- 32_45_digitart/UtilTaula.java
+++ 32_45_digitart/UtilTaula.java
@@ -1,18 +1,16 @@
 //Biblioteca que ens ajuda amb les taules //
 public class UtilTaula {
     //Funció que espera una taula de NxM i dos caracters que retorna una amb les mateixes dimensions//
-    public static char[][] substitueix(char[][]origen, char inici, char fi) {
+    public static char[][] substitueix(char[][]origen, char inici, char fi) { 
+        char[][] resultat = new char[origen.length][origen[0].length];
         for (int i = 0; i < origen.length; i++) {
             for (int j = 0; j < origen[i].length; j++) {
-                if (origen[i][j] == inici) {
-                    origen[i][j] = fi;
+                resultat[i][j] = origen[i][j];
+                if (resultat[i][j] == inici) {
+                    resultat[i][j] = fi;
                 }
             }
         }
-        return origen;
+        return resultat;
     }
 }
-
-
-
-
