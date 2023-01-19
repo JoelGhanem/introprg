023c204cd45177d17d792895e8f14e48c5631a27
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-01-19 01:29:06 +0100

    32_51_enproces

---
+++ 32_51_extreu_nombres/ExtreuNombres.java
@@ -0,0 +1,21 @@
+//Desenvolupa un programa que demani un text i mostri només els valors numèrics que conté.
+public class ExtreuNombres {
+    public static void main(String[] args){
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        String nombres = extreuNombres(text);
+        System.out.println(nombres);
+    }
+    // extreu els nombres i els retorna concatenats
+    public static String extreuNombres(String text) {
+        // cas base
+        // XXX
+        // tracta pas actual
+        // XXX
+        // tracta pas recursiu
+        // XXX
+        // composa resultat
+        // XXX
+        return text;
+    }
+}
