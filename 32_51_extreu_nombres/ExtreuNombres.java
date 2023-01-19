17b05dd7d2886dbe77a9ca3610376648c6bd1b0a
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-01-19 01:56:31 +0100

    32_51_enproces  ∾ prgtest autocommit 32_51 #6

--- 32_51_extreu_nombres/ExtreuNombres.java
+++ 32_51_extreu_nombres/ExtreuNombres.java
@@ -1,4 +1,4 @@
-//Desenvolupa un programa que demani un text i mostri només els valors limpioèrics que conté.
+//Desenvolupa un programa que demani un text i mostri només els valors numèrics que conté.
 public class ExtreuNombres {
     public static void main(String[] args){
         System.out.println("Text?");
@@ -8,18 +8,16 @@ public class ExtreuNombres {
     }
     // extreu els nombres i els retorna concatenats
     public static String extreuNombres(String text) {
-        String limpio = "";
-        int num;
+        String num = "";
+        String restaNum = "";
         if (text.length() > 0) {
             char primer = text.charAt(0);
             if (Character.isDigit(primer)) {
-                limpio = limpio + text.charAt(primer);
-                num = Integer.parseInt(limpio);
-                limpio = String.valueOf(num);
+                num = num + primer;
             }
-            String restaText = text.substring(1);
-            System.out.println(limpio);
+            restaNum = text.substring(1);
+            extreuNombres(restaNum);
         }
-        return limpio;
+        return restaNum;
     }
 }
