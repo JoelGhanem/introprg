e431f12caee5a2b5e915ffa0224402b8c9ee18e7
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-01-19 11:15:44 +0100

    32_52_enproces

---
+++ 32_52_suma_digits/SumaDigits.java
@@ -0,0 +1,22 @@
+/* Desenvolupa un programa que demani un text i mostri la suma dels dígits que conté. */
+public class SumaDigits {
+    public static void main(String[] args){
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        int nombres = sumaDigits(text);
+        System.out.println(nombres);
+    }
+    // XXX
+    public static int sumaDigits(String text) {
+        int algo = 0;
+        // cas base
+        // XXX
+        // tracta pas actual
+        // XXX
+        // tracta pas recursiu
+        // XXX
+        // composa resultat
+        // XXX
+        return algo;
+    }
+}
