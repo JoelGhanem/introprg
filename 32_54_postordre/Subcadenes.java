51feb7b8f5db1816774c22460eb0bb1102cb8009
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-01-22 18:15:39 +0100

    32_54_enproces

--- 32_54_postordre/Subcadenes.java
+++ 32_54_postordre/Subcadenes.java
@@ -4,10 +4,29 @@ public class Subcadenes {
         System.out.printf("Text?%n");
         String text = Entrada.readLine();
         text = netejaText(text);
-        mostraText(text, text.length());
+        mostraText(text, 1);
     }
     public static String netejaText(String text) {
         if (text.length() == 0) {
-            return = "";
+            return "";
         }
-
+        char primer = text.charAt(0);
+        if (!Character.isLetter(primer)) {
+            return text.substring(1);
+        }
+        String textNetejat = netejaText(text.substring(1));
+        return primer + textNetejat;
+    }
+    public static void mostraText(String text, int largo) {
+        if (largo > text.length()) {
+            return;
+        }
+        if (largo == 1) {
+            System.out.printf("%4d: %s%n", largo, text.substring(0, largo));
+            mostraText(text, largo+1);
+        } else { 
+            System.out.printf("%4d: %s%n", largo, text.substring(0, largo));
+                mostraText(text.substring(1), largo);
+        }
+    }
+}
