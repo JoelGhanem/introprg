31b2c07de131af0cd7a8c2b0f202b8c59cf4de8a
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-01-25 01:34:31 +0100

    33_03_enproces

---
+++ 33_03_csv/NotaMitja.java
@@ -0,0 +1,22 @@
+/*En aquest exercici desenvoluparàs un programa que calcularà la nota mitja dels estudiants que apareixen al fitxer de text notes.csv, que presenta el format que apareix a l'exemple anterior, encara que possiblement amb més o menys estudiants i altres notes.*/
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class NotaMitja {
+    public static void main(String[] args) throws IOException {
+        String cami = "notes.csv";
+        BufferedReader input = new BufferedReader(new FileReader(cami));
+        String paraula = input.readLine();
+        String [] elements = paraula.split(",");
+        int numero = 0;
+        int suma = 0;
+        for (int i = 0; i < elements.length; i++) {
+            if (Character.isDigit(elements[i].charAt(0))) {
+                numero = Integer.parseInt(elements[i]);
+                suma = suma + numero; 
+            }
+        }
+        int mitja = (int) (suma/6.0);
+        System.out.println(elements[0] + " (" + mitja + " )");
+    }
+}
