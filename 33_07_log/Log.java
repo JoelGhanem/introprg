86e4e8e7bfe37e16db03ecb26797377ed1fca3be
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-02 17:27:41 +0100

    33_07  ∾ prgtest autocommit 33_07 #4

--- 33_07_log/Log.java
+++ 33_07_log/Log.java
@@ -2,39 +2,51 @@
 import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 public class Log {
     private static int comptador = 1;
-    public static void printError(String text) throws IOException {
+    public static String printError(String text) throws IOException {
         String cami = "./log.txt";
         BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
+        BufferedReader lectura = new BufferedReader(new FileReader(cami));
         sortida.write("[" + comptador + "]" + " ERROR: " + text);
         comptador++;
         sortida.newLine();
         sortida.close();
+        String leido = lectura.readLine();
+        lectura.close();
+        return leido;
     }
-    public static void printWarning(String text) throws IOException {
+    public static String printWarning(String text) throws IOException {
+        String algo = "";
         String cami = "./log.txt";
         BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
         sortida.write("[" + comptador + "]" + " WARNING: " + text);
         comptador++;
         sortida.newLine();
         sortida.close();
+        return algo;
     }
-    public static void printInfo(String text) throws IOException {
+    public static String printInfo(String text) throws IOException {
+        String algo = "";
         String cami = "./log.txt";
         BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
         sortida.write("[" + comptador + "]" + " INFO: " + text);
         comptador++;
         sortida.newLine();
         sortida.close();
+        return algo;
     }
-    public static void printDebug(String text)  throws IOException {
+    public static String printDebug(String text)  throws IOException {
+        String algo = "";
         String cami = "./log.txt";
         BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
         sortida.write("[" + comptador + "]" + " DEBUG: " + text);
         comptador++;
         sortida.newLine();
         sortida.close();
+        return algo;
     }
     public static void reset() {
         comptador = 1;
