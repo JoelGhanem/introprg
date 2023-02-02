96e3e0753eb5ca81080c3d59b66277bbaa8ca1ab
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-02 18:27:41 +0100

    33_07  ∾ prgtest autocommit 33_07 #6

--- 33_07_log/Log.java
+++ 33_07_log/Log.java
@@ -5,44 +5,38 @@ import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 public class Log {
-    private static int comptador = 1;
+    private static int comptador = 0;
     public static String printError(String text) throws IOException {
+        comptador++;
         String cami = "./log.txt";
         BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
         sortida.write("[" + comptador + "]" + " ERROR: " + text);
-        comptador++;
         sortida.newLine();
         sortida.close();
-        BufferedReader lectura = new BufferedReader(new FileReader(cami));
-        String leido = lectura.readLine();
-        lectura.close();
-        return leido;
+        return ("[" + comptador + "]" + " ERROR: " + text);
     }
     public static String printWarning(String text) throws IOException {
+        comptador++;
         String algo = "";
         String cami = "./log.txt";
         BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
         sortida.write("[" + comptador + "]" + " WARNING: " + text);
-        comptador++;
         sortida.newLine();
         sortida.close();
         BufferedReader lectura = new BufferedReader(new FileReader(cami));
         String leido = lectura.readLine();
         lectura.close();
-        return leido;
+        return ("[" + comptador + "]" + " WARNING: " + text);
     }
     public static String printInfo(String text) throws IOException {
+        comptador++;
         String algo = "";
         String cami = "./log.txt";
         BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
         sortida.write("[" + comptador + "]" + " INFO: " + text);
-        comptador++;
         sortida.newLine();
         sortida.close();
-        BufferedReader lectura = new BufferedReader(new FileReader(cami));
-        String leido = lectura.readLine();
-        lectura.close();
-        return leido;
+        return ("[" + comptador + "]" + " INFO: " + text);
     }
     public static String printDebug(String text)  throws IOException {
         String algo = "";
@@ -52,12 +46,9 @@ public class Log {
         comptador++;
         sortida.newLine();
         sortida.close();
-        BufferedReader lectura = new BufferedReader(new FileReader(cami));
-        String leido = lectura.readLine();
-        lectura.close();
-        return leido;
+        return ("[" + comptador + "]" + " DEBUG: " + text);
     }
     public static void reset() {
-        comptador = 1;
+        comptador = 0;
     }
 }
