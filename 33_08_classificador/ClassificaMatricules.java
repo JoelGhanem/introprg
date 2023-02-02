db0d5ccf325a51aad221f686fa7cc9079412b050
Joel <cf22joel.ghanem@iesjoandaustria.org>
2023-02-02 19:37:46 +0100

    33_08_enproces

---
+++ 33_08_classificador/ClassificaMatricules.java
@@ -0,0 +1,26 @@
+/*Programa que revisa les matricules i diu si son italianes o no segons el fitxer on les escriu*/
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+public class ClassificaMatricules{ 
+    public static void main (String[] args) {
+        String cami = "llegides.txt";
+        BufferedReader lectura = new BufferedReader(new FileReader(cami));
+        while (true) {
+        }
+    }
+    public static boolean italiana(String matricula) {
+        italiana = falso
+        return italiana
+    }
+    public static void matriculesItalianes(String text) {
+        String italianes = "italianes.txt";
+        BufferedWriter eItalianes = new BufferedWriter(new FileWriter(italianes));
+    }
+    public static void matriculesDesconegudes(String text) {
+        String desconegudes = "desconegudes.txt";
+        BufferedWriter eDesconegudes = new BufferedWriter(new FileWriter(desconegudes));
+    }
+}
