1f5586f23179a1505dc4464b89b977f348e865d9
Joel <cf22joel.ghanem@iesjoandaustria.org>
2023-02-02 17:11:32 +0100

    33_07

---
+++ 33_07_log/Log.java
@@ -0,0 +1,38 @@
+/*Biblioteca que ens ajuda a fer un log*/
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+public class Log {
+    private static int comptador = 1;
+    public static void printError(String text) throws IOException {
+        String cami = "./log.txt";
+        BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
+        sortida.write(text);
+        sortida.newLine();
+        sortida.close();
+    }
+    public static void printWarning(String text) throws IOException {
+        String cami = "./log.txt";
+        BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
+        sortida.write(text);
+        sortida.newLine();
+        sortida.close();
+    }
+    public static void printInfo(String text) throws IOException {
+        String cami = "./log.txt";
+        BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
+        sortida.write(text);
+        sortida.newLine();
+        sortida.close();
+    }
+    public static void printDebug(String text)  throws IOException {
+        String cami = "./log.txt";
+        BufferedWriter sortida = new BufferedWriter(new FileWriter(cami,true));
+        sortida.write(text);
+        sortida.newLine();
+        sortida.close();
+    }
+    public static void reset() {
+        comptador = 1;
+    }
+}
