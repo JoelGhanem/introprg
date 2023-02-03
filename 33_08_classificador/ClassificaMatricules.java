6637408194708db6f96dbc732075d13953d342e0
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-03 15:57:36 +0100

    enproceso  ∾ prgtest autocommit 33_08 #10

--- 33_08_classificador/ClassificaMatricules.java
+++ 33_08_classificador/ClassificaMatricules.java
@@ -33,18 +33,16 @@ public class ClassificaMatricules{
     public static boolean italiana(String matricula) throws IOException{
         matricula.trim();
         boolean italiana = false;
-        String confuses = "IOQU";
-        if (matricula.length() == 7) {
+        if (matricula.length() == 7) { //solo si la matricual es mayor a 7
             for (int i = 0; i < matricula.length(); i++) {
+                char sitio = matricula.charAt(i);
                 if ( i == 0 || i == 1) {
                     if (!Character.isUpperCase(matricula.charAt(i))) {
-                        for (int j = 0; j < confuses.length(); j++) {
-                            if (matricula.charAt(i) == matricula.charAt(j)) {
-                                return italiana;
-                            }
+                        if (sitio == 'I' || sitio == 'O'|| sitio == 'U' || sitio == 'Q') {
+                            return italiana;
                         }
-                        return italiana;
                     }
+                    return italiana;
                 }
                 if (i == 2 || i == 3 || i == 4) {
                     if (!Character.isDigit(matricula.charAt(i))) {
@@ -53,13 +51,11 @@ public class ClassificaMatricules{
                 }
                 if (i == 5 || i == 6) {
                     if (!Character.isUpperCase(matricula.charAt(i))) {
-                        for (int j = 0; j < confuses.length(); j++) {
-                            if (matricula.charAt(i) == confuses.charAt(j)) {
-                                return italiana;
-                            }
+                        if (sitio == 'I' || sitio == 'O'|| sitio == 'U' || sitio == 'Q') {
+                            return italiana;
                         }
-                        return italiana;
                     }
+                    return italiana;
                 }
             }
         } else {
