9cfaafee1f1e44c2cbc852ee8c202bf61cf22c37
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-05 15:21:21 +0100

    33_09_enporces  ∾ prgtest autocommit 33_09 #18

--- 33_09_alumnes/CercaAlumnes.java
+++ 33_09_alumnes/CercaAlumnes.java
@@ -34,7 +34,7 @@ public class CercaAlumnes {
         String notas = "";
         for (int i = 0; i < notes.length; i++) {
             if (notes[i] == -1) {
-                System.out.println("NP");
+                System.out.print("NP");
             }
             if (i == 0) {
                 notas = notas + notes[0];
