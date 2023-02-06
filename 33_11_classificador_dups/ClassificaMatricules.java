19743fa875b677238d54c64065c9574aead34aa1
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-02-06 02:10:05 +0100

    33_11_enproces

---
+++ 33_11_classificador_dups/ClassificaMatricules.java
@@ -0,0 +1,95 @@
+//Aquest exercici és una versió ampliada de l'exercici anterior. La ampliació consisteix en evitar que els fitxers de sortida tinguin repetits.
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+public class ClassificaMatricules {
+    private static int contador = 0;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader llegides = new BufferedReader(new FileReader("llegides.txt"));
+        while (true) {
+            String linia = llegides.readLine();
+            if (linia == null) {
+                break;
+            }
+            if (esItaliana(linia)) {
+                italiana(linia);
+            } else {
+                desconeguda(linia);
+            }
+        }
+    }
+    public static boolean esItaliana(String linia) throws IOException {
+        String malas = "IOQU";
+        if (!Character.isUpperCase(linia.charAt(0)) || !Character.isUpperCase(linia.charAt(1))) {
+            return false;
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < malas.length(); j++) {
+                if (linia.charAt(i) == malas.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        for (int i = 2; i < 5; i++) {
+            if (!Character.isDigit(linia.charAt(i))) {
+                return false;
+            }
+        }
+        if (!Character.isUpperCase(linia.charAt(5)) || !Character.isUpperCase(linia.charAt(6))) {
+            return false;
+        }
+        for (int i = 5; i < 7; i++) {
+            for (int j = 0; j < malas.length(); j++) {
+                if (linia.charAt(i) == malas.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void italiana(String linia) throws IOException{
+        String cami = "italianes.txt";
+        BufferedReader italianaRead = new BufferedReader(new FileReader(cami));
+        BufferedWriter italianaWrite = new BufferedWriter(new FileWriter(cami));
+        while(true) {
+            if (contador == 0) {
+                italianaWrite.write(linia.trim());
+                italianaWrite.newLine();
+            } else {
+                String italianaR = italianaRead.readLine();
+                if (italianaR == null) {
+                    break;
+                }
+                if (!italianaR.equals(linia)) {
+                    italianaWrite.write(linia.trim());
+                    italianaWrite.newLine();
+                }
+            }
+            contador++;
+        }
+    }
+    public static void desconeguda(String linia) throws IOException{
+        String cami = "desconegudes.txt";
+        BufferedReader desconegudaRead = new BufferedReader(new FileReader(cami));
+        BufferedWriter desconegudesWrite = new BufferedWriter(new FileWriter(cami));
+        while(true) {
+            if (contador == 0) {
+                desconegudesWrite.write(linia.trim());
+                desconegudesWrite.newLine();
+            } else {
+                String desconegudaR = desconegudaRead.readLine();
+                if (desconegudaRead == null) {
+                    break;
+                }
+                if (!desconegudaRead.equals(linia)) {
+                    desconegudesWrite.write(linia.trim());
+                    desconegudesWrite.newLine();
+                }
+            }
+            contador++;
+        }
+    }
+}
