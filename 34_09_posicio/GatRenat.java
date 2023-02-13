a174fd45a35064aa35c7563b42acebd74aa5f46e
Joel <cf22joel.ghanem@iesjoandaustria.org>
2023-02-13 19:12:16 +0100

    34_09_fet

---
+++ 34_09_posicio/GatRenat.java
@@ -0,0 +1,5 @@
+/*En aquesta ocasió afegirem al nostre gat Renat la possibilitat d'indicar en quina posició es troba. En concret, podrà estar dret, assegut o estirat.*/
+public class GatRenat {
+    public int vides = 7;         // vides disponibles del gat Renat
+    public String posicio = "estirat";
+}
---
+++ 34_09_posicio/UsaGatRenat.java
@@ -0,0 +1,18 @@
+/* Modifica la definició de la seva classe perquè pugui guardar aquesta informació, fent que inicialment el gat estigui estirat. La informació de l'estat la guardarem en una propietat amb el nom posicio que serà de tipus String.*/
+public class UsaGatRenat {
+    public static GatRenat creaGat() {
+        GatRenat[] renat = new GatRenat();
+        return renat;
+    }
+    public static void posicioGat() {
+        renat[].posicio = renat[].posicio"assegut";
+    }
+
+    public static void main(String[] args) {
+        GatRenat renat = creaGat();
+        System.out.println("Vides inicials: " + renat.vides);
+        System.out.println("Posició inicial: " + renat.posicio);
+        renat = posicioGat();
+        System.out.println("Posició final: " + renat.posicio);
+    }
+}
