e334dcfed0219c677ffa5ab800b367a3d8b6020d
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-13 18:35:00 +0100

    34_08_fet  ∾ prgtest autocommit 34_08 #3

--- 34_08_desdefora/UsaAscensor.java
+++ 34_08_desdefora/UsaAscensor.java
@@ -3,7 +3,6 @@ public class UsaAscensor {
     public static Ascensor[] creaAscensors(int quants) {
         Ascensor[] ascensor = new Ascensor[quants];
         ascensor[0] = new Ascensor();
-        ascensor[0].pis = 0;
         return ascensor;
     }
     public static void main(String[] args) {
