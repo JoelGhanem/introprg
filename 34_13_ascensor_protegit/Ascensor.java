561e75a6335aa365234259c7f4c0fe5181257815
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-02-14 01:59:15 +0100

    34_13_fet

---
+++ 34_13_ascensor_protegit/Ascensor.java
@@ -0,0 +1,21 @@
+/*En aquesta ocasió protegirem les propietats de l'ascensor de manera que: el pis sempre sigui un valor entre -1 i 10 el moviment sempre sigui un dels següents valors: aturat, pujant, baixant Inicialment, l'ascensor començarà a la planta -1 i aturat.*/
+public class Ascensor {
+    private int pis = -1;
+    private String moviment = "aturat";
+    public int getPis() {
+        return pis;
+    }
+    public String getMoviment() {
+        return moviment;
+    }
+    public void setPis(int nouPis) {
+        if (nouPis >= -1 && nouPis <= 10) pis = nouPis;
+    }
+    public void setMoviment(String nouMoviment) {
+        if (nouMoviment.equals("pujant")) {
+            moviment = nouMoviment;
+        } else if (nouMoviment.equals("baixant")) {
+            moviment = nouMoviment;
+        }
+    }
+}
---
+++ 34_13_ascensor_protegit/UsaAscensor.java
@@ -0,0 +1,15 @@
+/*En aquesta ocasió protegirem les propietats de l'ascensor de manera que: el pis sempre sigui un valor entre -1 i 10 el moviment sempre sigui un dels següents valors: aturat, pujant, baixant Inicialment, l'ascensor començarà a la planta -1 i aturat.*/
+public class UsaAscensor {
+    /* XXX */
+    public static void main(String[] args) {
+        Ascensor ascensor = new Ascensor();
+        System.out.println("Pis inicial: " + ascensor.getPis());
+        System.out.println("Moviment inicial: " + ascensor.getMoviment());
+        System.out.println("Introdueix nou pis:");
+        ascensor.setPis(Integer.parseInt(Entrada.readLine()));
+        System.out.println("Introdueix nou moviment:");
+        ascensor.setMoviment(Entrada.readLine());
+        System.out.println("Pis final: " + ascensor.getPis());
+        System.out.println("Moviment final: " + ascensor.getMoviment());
+    }
+}
