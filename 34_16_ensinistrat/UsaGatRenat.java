bb95fe007260279ee7119ee2b05f34a4614a2a6c
Joel <cf22joel.ghanem@iesjoandaustria.org>
2023-02-14 20:08:50 +0100

    34_16_fet

--- 34_16_ensinistrat/UsaGatRenat.java
+++ 34_16_ensinistrat/UsaGatRenat.java
@@ -1,6 +1,5 @@
 /*Afegeix tres nous mètodes: String aixecat(), String seu(), i String estirat() de manera que quan vulguem, per exemple, que en Renat s'aixequi simplement li diguem renat.aixecat();. En cas que el Renat ja estigués en l'estat que se li demana, no fa cap canvi d'estat. Aquestes funcions retornaran un missatge indicant el que ha fet el Renat. Així, els missatges que ens retornarà seran: "m'estiro", "m'assec", "m'aixeco", i "no faig res". Atenció: els nous mètodes no han d'escriure res per sortida estàndard! Fes servir el main() a UsaGatRenat per demostrar el funcionament d'aquests nous mètode*/
-public class GatRenat {
-    public String aixecat() { return "m' aixeco"; }
-    public String seu() { return "m'assec"; }
-    public String estirat() { return "m'estiro";}
+public class UsaGatRenat {
+    public static void main(String[] args) {
+    }
 }
