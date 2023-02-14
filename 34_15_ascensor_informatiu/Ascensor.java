62a61c4c2b3673bfa44bdbf59e7fb338c3054697
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-14 16:03:23 +0100

    34_15_enproces  ∾ prgtest autocommit 34_15 #5

--- 34_15_ascensor_informatiu/Ascensor.java
+++ 34_15_ascensor_informatiu/Ascensor.java
@@ -38,8 +38,8 @@ public class Ascensor {
             if (nouPis > pis) pujant = true;
             if (nouPis < pis) baixant = true;
             if (nouPis == pis) aturat = true;
-            if (nouPis == 10) amunt = true; abaix = false;
-            if (nouPis == -1) abaix = false; amunt = false;
+            if (nouPis == 10) amunt = true;
+            if (nouPis == -1) abaix = false;
             pis = nouPis;
         }
     }
