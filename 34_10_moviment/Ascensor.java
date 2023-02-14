663e6b8c2f159c1d7f7b4edbedcb613f1e4a0f26
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-02-14 01:30:19 +0100

    34_10_enproces

---
+++ 34_10_moviment/Ascensor.java
@@ -0,0 +1,5 @@
+/*Implementa els canvis necessaris per la classe Ascensor segons el següent diagrama UML: Com veus, l'ascensor disposarà de moviment. Inicialment l'ascensor estarà aturat. Altres moviments seran pujant i baixant.*/
+public class Ascensor {
+    public int pis = -1;
+    public String moviment = "aturat";
+}
---
+++ 34_10_moviment/UsaAscensor.java
@@ -0,0 +1,17 @@
+/*Implementa els canvis necessaris per la classe Ascensor segons el següent diagrama UML: Com veus, l'ascensor disposarà de moviment. Inicialment l'ascensor estarà aturat. Altres moviments seran pujant i baixant.*/
+public class UsaAscensor {
+    public static Ascensor creaAscensor() {
+        Ascensor ascensor = new Ascensor();
+        return ascensor;
+    }
+
+    public static void main(String[] args) {
+        Ascensor ascensor = creaAscensor();
+        System.out.println("Pis inicial: " + ascensor.pis);
+        System.out.println("Moviment icial: " + ascensor.moviment);
+        ascensor.moviment = "pujant";
+        System.out.println("Moviment final: " + ascensor.moviment);
+    }
+}
+
+
