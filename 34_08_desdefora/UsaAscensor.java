b4b0c326da75aac39517eb97d739c52d9fe531e9
Joel <cf22joel.ghanem@iesjoandaustria.org>
2023-02-15 19:59:31 +0100

    algo

--- 34_08_desdefora/UsaAscensor.java
+++ 34_08_desdefora/UsaAscensor.java
@@ -1,23 +1,8 @@
 /*Exercici en el que aprenem a executar programes amb el main d'un altre arxiu*/
 public class UsaAscensor { 
-    public static Ascensor[] creaAscensors(int quants) {
-        Ascensor[] ascensor = new Ascensor[quants];
-        ascensor[0] = new Ascensor();
-        ascensor[0].pis -=1;
-        return ascensor;
-    }
-
-    public static boolean cuantos(String cantidad){
-        for (int i = 0; i < cantidad.length(); i++) {
-            if (!Character.isDigit(cantidad.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-        Ascensor[] ascensors = creaAscensors(1);
-        System.out.println("L'ascensor creat desde fora está al pis " + ascensor[0].pis);
+        Ascensor ascensor;
+        ascensor = new Ascensor();
+        System.out.println("L'ascensor creat desde fora está al pis " + ascensor.pis);
     }
 }
