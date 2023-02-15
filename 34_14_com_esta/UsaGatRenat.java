ecbef81ac37bcc78dd97e8da6ce3b4023fd6406c
Joel <cf22joel.ghanem@iesjoandaustria.org>
2023-02-15 20:06:35 +0100

    cabecera mejorada

--- 34_14_com_esta/UsaGatRenat.java
+++ 34_14_com_esta/UsaGatRenat.java
@@ -1,3 +1,4 @@
+//Programa que canvia la posicio del gat renat segons la entrada rebuda//
 public class UsaGatRenat {
     public static void main(String[] args) {
         GatRenat renat = new GatRenat();
