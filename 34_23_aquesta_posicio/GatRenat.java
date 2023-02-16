70ed3342586ded39404c2d0d7956ac98d9a0965b
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-02-16 23:34:32 +0100

    34_23_fet

---
+++ 34_23_aquesta_posicio/GatRenat.java
@@ -0,0 +1,24 @@
+//Crea una nova versió de l'exercici anterior, de manera que el constructor i el setter de posició defineixin el mateix nom pel paràmetre que especifica la posició que per la propietat.
+public class GatRenat {
+    private int vides = 7;
+    private String posicio = "estirat";
+    public GatRenat(int vides, String posicio) {
+        setVides(vides);
+        setPosicio(posicio);
+    }
+    public int getVides() { return vides; }
+    public void setVides(int novesVides) {
+        if (novesVides >= 0)  {
+            vides = novesVides;
+        }
+    }
+    public String getPosicio() {return posicio; }
+    public void setPosicio(String novaPosicio) {
+        if (novaPosicio.equals("dret")) { posicio = novaPosicio;}
+    }
+    @Override
+    public String toString() { return String.format("Vides: %d. Posició: %s", vides, posicio); }
+    public static void main(String[] args) {
+        System.out.println(new GatRenat(7, "dret"));
+    }
+}
