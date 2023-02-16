13e3d54a823999636fafe847a883f2378c960242
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-02-16 23:20:03 +0100

    34_21_fet

---
+++ 34_21_constructor_parametritzat/GatRenat.java
@@ -0,0 +1,19 @@
+//Fes una nova versió de GatRenat de manera que funcioni el següent main() Fixa't que, com que no en fem res de la instància, a banda de mostrar-la, no cal ni que la assignem a una variable!
+public class GatRenat {
+    private int vides;
+    private String posicio;
+    public GatRenat(int novesVides, String novaPosicio) {
+        vides = novesVides;
+        posicio = novaPosicio;
+
+    }
+    public int getVides() { return vides; }
+
+    @Override
+    public String toString() {
+        return String.format("Vides: %d. Posicio: %s", vides, posicio);
+    }
+    public static void main(String[] args) {
+        System.out.println(new GatRenat(7, "estirat"));
+    }
+}
