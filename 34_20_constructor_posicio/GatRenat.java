8787553aad9368481fc0da5e6859d5a45380eb8a
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-02-16 23:14:42 +0100

    34_20_fet

---
+++ 34_20_constructor_posicio/GatRenat.java
@@ -0,0 +1,18 @@
+//Afegeix la propietat posicio de tipus String a la definició de GatRenat. Aquesta nova propietat serà inicialitzada des del constructor amb el valor "estirat" de la mateixa manera que ho fa ara amb vides
+public class GatRenat {
+    private int vides;
+    private String posicio;
+    public GatRenat() {
+        vides = 7;
+        posicio = "estirat";
+    }
+    @Override
+    public String toString() {
+        return String.format("Vides: %d. Posició: %s" , vides, posicio);
+    }
+    public int getVides() { return vides; }
+    public static void main(String[] args) {
+        GatRenat renat = new GatRenat();
+        System.out.println(renat);
+    }
+}
