081217945a553d1bd02d4d80acd01970cb14af0b
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-16 16:55:01 +0100

    algo  ∾ prgtest autocommit 34_16 #3

--- 34_16_ensinistrat/GatRenat.java
+++ 34_16_ensinistrat/GatRenat.java
@@ -1,55 +1,51 @@
 /*Afegeix tres nous mètodes: String aixecat(), String seu(), i String estirat() de manera que quan vulguem, per exemple, que en Renat s'aixequi simplement li diguem renat.aixecat();. En cas que el Renat ja estigués en l'estat que se li demana, no fa cap canvi d'estat. Aquestes funcions retornaran un missatge indicant el que ha fet el Renat. Així, els missatges que ens retornarà seran: "m'estiro", "m'assec", "m'aixeco", i "no faig res". Atenció: els nous mètodes no han d'escriure res per sortida estàndard! Fes servir el main() a UsaGatRenat per demostrar el funcionament d'aquests nous mètode*/
 public class GatRenat {
-    private String aixecatS = "m'aixeco";
-    private String seuS = "m'assec"; 
-    private String estiratS = "m'estiro";
-    private boolean viu = true;
-    private boolean dret = false;
-    private boolean assegut = false;
-    private boolean estirat = false;
     private int vides = 7;
     private String posicio = "estirat";
     //Strings
     public String estirat() {
-        if (estirat) estiratS = "no faig res";
-        estirat = true;
-        return estiratS; 
+        if (posicio.equals("estirat")) {
+            posicio = "no faig res";
+        }
+        return posicio; 
     }
     public String aixecat() {
-        if (dret) aixecatS = "no faig res";
-        dret = true;
-        return aixecatS;
+        if (posicio.equals("aixecat")) {
+            posicio = "no faig res";
+        }
+        return posicio;
     }
     public String seu() {
-        if (assegut) seuS = "no faig res";
-        assegut = true;
-        return seuS;
+        if (posicio.equals("aixecat")) {
+            posicio = "no faig res";
+        }
+        return posicio;
     }
 
     public String getPosicio() { return posicio; }
 
     //booleans
-    public boolean estaViu() { return viu; }
-
-    public boolean estaEstirat() { return estirat; }
-
-    public boolean estaDret() {return dret;}
-
-    public boolean estaAssegut() { return assegut;}
-
+    //public boolean estaViu() { return viu; }
+//
+    //public boolean estaEstirat() { return estirat; }
+//
+    //public boolean estaDret() {return dret;}
+//
+    //public boolean estaAssegut() { return assegut;}
+//
 
     public void setVides(int novesVides) {
-        if (novesVides <= 0) viu = false; vides = novesVides;
+        //if (novesVides <= 0) //viu = false; vides = novesVides;
     }
     public void setPosicio(String novaPosicio) {
         if (novaPosicio.equals("assegut")) {
-            posicio = novaPosicio;
-            assegut = true;
-            estirat = false;
+            //posicio = novaPosicio;
+            //assegut = true;
+            //estirat = false;
         } else if (novaPosicio.equals("dret")){
             posicio = novaPosicio;
-            dret = true;
-            estirat = false;
+           // dret = true;
+            //estirat = false;
         }
     }
 }
