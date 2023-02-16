62569cfe9e6b142c631fbeaeb62f27c34eeb2bc5
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-16 23:32:06 +0100

    34_22_enporce  ∾ prgtest autocommit 34_22 #6

--- 34_22_constructor_posicio/GatRenat.java
+++ 34_22_constructor_posicio/GatRenat.java
@@ -1,7 +1,7 @@
 //Implementa una nova versió que ofereixi la inicialització del gat Renat amb la posició indicada. En cas que la posició que rebi el constructor no sigui vàlida, el gat Renat resultant estarà en la seva posició favorita, estirat.
 public class GatRenat {
     private int vides = 7;
-    private String posicio = "estirat";
+    private String posicio;
     public GatRenat(int vides, String posicio) {
         setVides(vides);
         setPosicio(posicio);
@@ -14,7 +14,7 @@ public class GatRenat {
     }
     public String getPosicio() {return posicio; }
     public void setPosicio(String novaPosicio) {
-        if (novaPosicio.equals("dret")) { novaPosicio = posicio;}
+        if (novaPosicio.equals("dret")) { posicio = novaPosicio;}
     }
     @Override
     public String toString() { return String.format("Vides: %d. Posició: %s", vides, posicio); }
