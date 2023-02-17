0f841938486ff3381845487719a14b00f2f4c886
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-02-17 13:43:13 +0100

    34_25_enProces

---
+++ 34_25_hora/Hora.java
@@ -0,0 +1,3 @@
+//Implementa la classe Hora amb les propietats (privades) hora, minut i segon de tipus enter, i els accessors (públics) que assegurin que l'hora és sempre vàlida.Per defecte, l'hora correspondrà a 0:00:00, però la classe oferirà els mecanismes necessaris per a inicialitzar una hora amb algun altre valor vàlid. És a dir, oferirà un constructor específic amb la signatura Hora(int hores, int minuts, int segons) que, en cas de que els valors rebuts no corresponguin amb una hora vàlida, inicialitzarà l'hora amb el valor per defecte
+public class Hora {
+}
