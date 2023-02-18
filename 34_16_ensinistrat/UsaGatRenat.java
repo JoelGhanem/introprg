a074a53026d3a1ce6b1bc93157289be2b816dbc4
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-18 22:30:49 +0100

    fetn junit  ∾ prgtest autocommit 34_16 #5

--- 34_16_ensinistrat/UsaGatRenat.java
+++ 34_16_ensinistrat/UsaGatRenat.java
@@ -3,12 +3,8 @@ public class UsaGatRenat {
     public static void main(String[] args) {
         GatRenat renat = new GatRenat();
         System.out.println("El Renat diu: "+ renat.aixecat());
-        System.out.println(renat.getPosicio());
         System.out.println("El Renat diu: "+ renat.seu());
-        System.out.println(renat.getPosicio());
         System.out.println("El Renat diu: "+ renat.estirat());
-        System.out.println(renat.getPosicio());
         System.out.println("El Renat diu: "+ renat.estirat());
-        System.out.println(renat.getPosicio());
     }
 }
