20d542f987d74758c483bcf28e0a107fc87522a4
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-02-19 13:33:33 +0100

    34_17_enporceso

--- 34_17_assessinable/GatRenat.java
+++ 34_17_assessinable/GatRenat.java
@@ -1,29 +1,32 @@
 //Afegeix al gat Renat els següents mètodes: String mor(): li treu una vida si encara és viu i retorna el lamentable lament "auch" quan, un cop perduda una vida, encara és viu. En canvi, si en treure-li la vida ja no li queden més, retorna el més definitiu "ximpún". Finalment, si ja estava mort del tot, retornarà un silenci en forma de punts suspensius "...". String resuscita(): quan en Renat està mort, li afegeix una vida i retorna un crit d'alegria "guai!". Si no estava mort, però, retorna el típic silenci de punts suspensius "...". String resuscita(int): fa el mateix que resuscita() però en aquesta ocasió se li pot indicar el nombre de vides amb les que torna a la vida. Si el nombre de vides indicat és menor que un, retornarà el silenci típic. //
 public class GatRenat {
-    private String aixecatS = "m'aixeco";
-    private String seuS = "m'assec"; 
-    private String estiratS = "m'estiro";
-    private boolean viu = true;
-    private boolean dret = true;
-    private boolean assegut = false;
-    private boolean estirat = false;
     private int vides = 7;
+    private boolean viu = true;
     private String posicio = "estirat";
     //Strings
     public String estirat() {
-        if (estirat) estiratS = "no faig res";
-        estirat = true;
-        return estiratS; 
+        if (estaEstirat()) {
+            return  "no faig res";
+        } else {
+            setPosicio("estirat");
+            return "m'estiro"; 
+        }
     }
     public String aixecat() {
-        if (dret) aixecatS = "no faig res";
-        dret = true;
-        return aixecatS;
+        if (estaDret()) {
+            return  "no faig res";
+        } else {
+            setPosicio("aixecat");
+            return "m'aixeco";
+        }
     }
     public String seu() {
-        if (assegut) seuS = "no faig res";
-        assegut = true;
-        return seuS;
+        if (estaAssegut()) {
+            return "no faig res";
+        } else {
+            setPosicio("assegut");
+            return "m'assec";
+        }
     }
 
     public String getPosicio() { return posicio; }
@@ -31,11 +34,11 @@ public class GatRenat {
     //booleans
     public boolean estaViu() { return viu; }
 
-    public boolean estaEstirat() { return estirat; }
+    public boolean estaEstirat() { return posicio.equals("estirat"); }
 
-    public boolean estaDret() {return dret;}
+    public boolean estaDret() {return posicio.equals("dret");}
 
-    public boolean estaAssegut() { return assegut;}
+    public boolean estaAssegut() { return posicio.equals("assegut");}
 
     public int getVides(int novesVides) {
         novesVides = vides;
@@ -47,12 +50,10 @@ public class GatRenat {
     public void setPosicio(String novaPosicio) {
         if (novaPosicio.equals("assegut")) {
             posicio = novaPosicio;
-            assegut = true;
-            estirat = false;
         } else if (novaPosicio.equals("dret")){
             posicio = novaPosicio;
-            dret = true;
-            estirat = false;
+        } else {
+            posicio = "estirat";
         }
     }
     public String mor() {
