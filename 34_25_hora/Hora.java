87275a976776a18f17e31d51504e83faf4582b7e
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-21 23:02:21 +0100

    34_25_enProces  ∾ prgtest autocommit 34_25 #5

--- 34_25_hora/Hora.java
+++ 34_25_hora/Hora.java
@@ -24,11 +24,11 @@ public class Hora {
         return this.hores;
     }
     public int setMinuts(int minuts) {
-        if (minuts >= 60) {
-
+        if (this.minuts + minuts <= 60) {
             setHores(1);
+        } else {
+            this.minuts += minuts;
         }
-        this.minuts += minuts;
         return this.minuts;
     }
     public int setSegons(int segons) {
@@ -46,45 +46,45 @@ public class Hora {
     public void incrementa(int segons) {
         if (this.segons + segons > 60) {
 
-                    this.segons = this.segons + segons;
-        }
-        }
-        public void decrementa() {
-            this.segons--;
-        }
-        public void decrementa(int segons) {
-            this.segons = this.segons - segons;
-        }
-        public int compareTo(Hora hora2) {
-            if (this.hores > hora2.hores) return 1;
-            if (this.hores < hora2.hores) return -1;
-            if (this.minuts > hora2.minuts) return 1;
-            if (this.minuts < hora2.minuts) return -1;
-            if (this.segons > hora2.segons) return 1;
-            if (this.segons < hora2.segons) return -1;
-            return 0;
-        }
-        @Override
-        public String toString() {
-            return String.format("%d:%02d:%02d", this.hores, this.minuts, this.segons);
-        }
-        private static String composaOperadorComparacio(Hora hora1, Hora hora2) {
-            int comparacio = hora1.compareTo(hora2);
-            if (comparacio < 0) {
-                return "<";
-            } else if (comparacio > 0) {
-                return ">";
-            } else {
-                return "==";
-            }
-        }
-        public static void main (String[] args) {
-            Hora hora1 = new Hora();
-            Hora hora2 = new Hora(0,0,2);
-            System.out.printf("Inicialment hora1: %s %s hora2: %s%n",hora1, composaOperadorComparacio(hora1, hora2),hora2);
-            System.out.println("Incrementem 1 segon a la primera i decrementem 1 segon a la segona");
-            hora1.incrementa();
-            hora2.decrementa();
-            System.out.printf("Finalment hora1: %s %s hora2: %s%n",hora1, composaOperadorComparacio(hora1,hora2),hora2);
+            this.segons = this.segons + segons;
         }
     }
+    public void decrementa() {
+        this.segons--;
+    }
+    public void decrementa(int segons) {
+        this.segons = this.segons - segons;
+    }
+    public int compareTo(Hora hora2) {
+        if (this.hores > hora2.hores) return 1;
+        if (this.hores < hora2.hores) return -1;
+        if (this.minuts > hora2.minuts) return 1;
+        if (this.minuts < hora2.minuts) return -1;
+        if (this.segons > hora2.segons) return 1;
+        if (this.segons < hora2.segons) return -1;
+        return 0;
+    }
+    @Override
+    public String toString() {
+        return String.format("%d:%02d:%02d", this.hores, this.minuts, this.segons);
+    }
+    private static String composaOperadorComparacio(Hora hora1, Hora hora2) {
+        int comparacio = hora1.compareTo(hora2);
+        if (comparacio < 0) {
+            return "<";
+        } else if (comparacio > 0) {
+            return ">";
+        } else {
+            return "==";
+        }
+    }
+    public static void main (String[] args) {
+        Hora hora1 = new Hora();
+        Hora hora2 = new Hora(0,0,2);
+        System.out.printf("Inicialment hora1: %s %s hora2: %s%n",hora1, composaOperadorComparacio(hora1, hora2),hora2);
+        System.out.println("Incrementem 1 segon a la primera i decrementem 1 segon a la segona");
+        hora1.incrementa();
+        hora2.decrementa();
+        System.out.printf("Finalment hora1: %s %s hora2: %s%n",hora1, composaOperadorComparacio(hora1,hora2),hora2);
+    }
+}
