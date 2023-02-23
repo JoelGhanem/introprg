5a71f5ec732eb19ee6caf03e2f32a9f6b5bd9b8f
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-02-23 16:09:05 +0100

    34_24_amb el this  ∾ prgtest autocommit 34_25 #5

--- 34_25_hora/Hora.java
+++ 34_25_hora/Hora.java
@@ -71,8 +71,7 @@ public class Hora {
         if (this.segons == 0) {
             setMinuts(this.minuts - 1);
             this.segons = 59;
-        } else if (this.minuts == 0) {
-            setMinuts(59);
+        } else {
             this.segons--;
         }
     }
