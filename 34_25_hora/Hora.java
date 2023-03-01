99c243b6e738b48d248a652cc5b2156add4bcf97
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-03-01 20:01:32 +0100

    algo  ∾ prgtest autocommit 34_25 #12

--- 34_25_hora/Hora.java
+++ 34_25_hora/Hora.java
@@ -59,6 +59,7 @@ public class Hora {
                 setMinuts(getMinuts() + minutsHores);
                 setHores(getHores() + hores);
                 if (getHores() > 24) setHores(getHores() % 24);
+                if (getMinuts() > 24) setMinuts(getMinuts() % 60);
                     setSegons( segonsHores);
                 //si hay minutos
             } else {
