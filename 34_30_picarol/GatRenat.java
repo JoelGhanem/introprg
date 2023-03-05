4c365aa15ed6c2a7c9b27c29742b774bc346bd47
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-03-05 21:54:45 +0100

    34_30_quieroSalirDeCasa  ∾ prgtest autocommit 34_30 #7

--- 34_30_picarol/GatRenat.java
+++ 34_30_picarol/GatRenat.java
@@ -3,7 +3,7 @@ public class GatRenat {
     private int vides = 7;
     private String posicio = "estirat";
     private static GatRenat instancia;
-    Picarol picarol = null;
+    private Picarol picarol = null;
 
     public GatRenat() {
         String posicio = getPosicio(); 
