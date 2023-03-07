469871ab46b8e9a57d850e7257fefe7bdd2864ab
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-03-07 13:30:48 +0100

    34_33_EnProces

---
+++ 34_33_metodes_cridats/DemoCrides.java
@@ -0,0 +1,16 @@
+class DemoCrides {
+    public static void main(String[] args) {
+        Feli felix = new Feli();
+        Gat gat = new Gat();
+        GatRenat renat = new GatRenat();
+        felix.netejaUrpes();
+        gat.netejaUrpes();
+        renat.netejaUrpes();
+        gat.miola();
+        renat.miola();
+        Menjar menjar = new Menjar("bacallà");
+        renat.menja(menjar);
+        felix.menja(menjar);
+        gat.menja(menjar);
+    }
+}
