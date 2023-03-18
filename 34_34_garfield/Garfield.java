ab11a9117e7f3419fcbdc2930249f8a3d6b67e67
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-03-18 13:24:57 +0100

    actualitzats fitxers de suport ∾ prgtest autocommit 34_34 #36

--- 34_34_garfield/Garfield.java
+++ 34_34_garfield/Garfield.java
@@ -63,21 +63,21 @@ class Garfield extends Gat {
 		@Override 
 		public boolean estaDret() {
 				if(this.posicio.equals("dret")) {
-						System.out.println(true);
+						System.out.println("estaDret " + true);
 						return true;
 				} return false;
 		}
 		@Override
 		public boolean estaAssegut() {
 				if (this.posicio.equals("assegut")){
-						System.out.println(true);
+						System.out.println("estaAssegut " + true);
 						return true;
 				} return false;
 		}
 		@Override
 		public boolean estaEstirat() {
 				if(this.posicio.equals("estirat")) {
-						System.out.println(true);
+						System.out.println("estaEstirat " + true);
 						return true;
 				}
 				return false;
