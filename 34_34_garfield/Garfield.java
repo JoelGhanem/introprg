37dfe7fce8b2856e325265088e490d403c696164
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-03-18 14:16:16 +0100

    actualitzats fitxers de suport ∾ prgtest autocommit 34_34 #47

--- 34_34_garfield/Garfield.java
+++ 34_34_garfield/Garfield.java
@@ -1,8 +1,8 @@
 /*La classe Garfield es una subclasse de la calsse Gat, en aquest cas la utilitzem per fer experiments, encara que estiguio buida la fem servir per veure com afecta a l'execucio del programa*/
 class Garfield extends Gat {
-		private String nom = "Garfield";
-		private String posicio = "estirat";
-		private int vides = 9;
+		private String nom;
+		private String posicio;
+		private int vides;
 				public Garfield() {
 						super("Garfield",9,"estirat");
 			}
