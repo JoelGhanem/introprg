f15eeba72a2b93b5bf0cccec087adfaff72c8a55
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-03-18 14:29:37 +0100

    actualitzats fitxers de suport ∾ prgtest autocommit 34_34 #55

--- 34_34_garfield/GatRenat.java
+++ 34_34_garfield/GatRenat.java
@@ -1,8 +1,8 @@
 /*La classe GatRenat es una subclasse de la classe Gat, En aquest cas te metodes que varien segons els arguments que rebin, ens serveix per vbeure com funciona el polimorfisme*/
 class GatRenat extends Gat{
-		private int vides /*= 7*/;
-		private String posicio /*= "estirat"*/;
-		private String nom /*= "Renat"*/;
+		//private int vides /*= 7*/;
+		//private String posicio /*= "estirat"*/;
+		//private String nom /*= "Renat"*/;
 		public GatRenat() {
 				super("Renat",7,"estirat");
 		}
