d760aaee39d4f021cc6557d8a61224bf31c51e08
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-03-20 12:41:13 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 34_35 #1

--- 34_35_interfagat/Gat.java
+++ 34_35_interfagat/Gat.java
@@ -1,46 +1,46 @@
 /*Aquesta es la superclasse Gat que implementa EsserViu i que defineix el GatRenat el Garfield i el GatSalvatge*/
 class Gat implements EsserViu {
 
-		private int vides = 7;
-		private String nom = "anònim";
-		public Gat(String nom){
-				this.nom = nom;
+	private int vides = 7;
+	private String nom = "anònim";
+	public Gat(String nom){
+		this.nom = nom;
+	}
+	public Gat(String nom, int vides) {
+		this.nom = nom; 
+		this.vides = vides;
+	}
+	public String getNom() {
+		return this.nom;
+	}
+	public int getVides() {
+		return this.vides;
+	}
+	public void setVides(int vides) {
+		this.vides = vides;
+	}
+	@Override
+	public boolean estaViu() {
+		if (this.vides > 0 ) {
+			return true;
+		} 
+		return false;
+	}
+	@Override
+	public String ressuscita() {
+		if (getVides() < 0) {
+			setVides(1);
+			return "guai!";
+		} 
+		return "encara miolo";
+	}
+	@Override 
+	public String mor() {
+		if (getVides() < 0) {
+			return "ja l'he espifiada";
 		}
-		public Gat(String nom, int vides) {
-				this.nom = nom; 
-				this.vides = vides;
-		}
-		public String getNom() {
-				return this.nom;
-		}
-		public int getVides() {
-				return this.vides;
-		}
-		public void setVides(int vides) {
-				this.vides = vides;
-		}
-		@Override
-		public boolean estaViu() {
-				if (this.vides > 0 ) {
-						return true;
-				} 
-				return false;
-		}
-		@Override
-		public String ressuscita() {
-				if (getVides() < 0) {
-						setVides(1);
-						return "guai!";
-				} 
-				return "encara miolo";
-		}
-		@Override 
-		public String mor() {
-				if (getVides() < 0) {
-						return "ja l'he espifiada";
-				}
-				setVides(getVides()-1);
-				return "adéu món cruel";
-		}
+		setVides(0);
+		return "adéu món cruel";
+	}
 
 }
