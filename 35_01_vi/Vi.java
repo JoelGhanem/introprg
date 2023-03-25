755381e7bc1935f60a3164955df5814f861076d3
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-03-25 18:28:08 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 35_01 #7

--- 35_01_vi/Vi.java
+++ 35_01_vi/Vi.java
@@ -1,83 +1,85 @@
 /*Una botiga de vi sense vi no és res. Així que començarem implementant aquesta classe tant important.*/
 public class Vi {
-    private String nom;
-    private int preu;
-    private int estoc = 0;
+		private String nom;
+		private int preu;
+		private int estoc = 0;
 
-    //constructors
-    public Vi(String nom, int preu) {
-        this.estoc = 0;
-		nom = normalitzaNom(nom);
-        this.nom = nom;
-        if (preu > 0) {
-            this.preu = preu;
-        } else {
-            this.preu = -1;
-        }
-    }
-    public Vi(String nom, int preu, int estoc) {
-        this.nom = nom;
+		//constructors
+		public Vi(String nom, int preu) {
+				this.estoc = 0;
+				nom = normalitzaNom(nom);
+				this.nom = nom;
+				if (preu > 0) {
+						this.preu = preu;
+				} else {
+						this.preu = -1;
+				}
+		}
+		public Vi(String nom, int preu, int estoc) {
+				this.nom = nom;
 
-        if (preu > 0) {
-            this.preu = preu;
-        } else {
-            this.preu = -1;
-        }
+				if (preu > 0) {
+						this.preu = preu;
+				} else {
+						this.preu = -1;
+				}
 
-        if (estoc > 0) {
-            this.estoc = estoc;
-        } else {
-            this.estoc = -1;
-        }
-    }
+				if (estoc > 0) {
+						this.estoc = estoc;
+				} else {
+						this.estoc = -1;
+				}
+		}
 
-    //getters
-    public String getNom() { return this.nom; }
+		//getters
+		public String getNom() { return this.nom; }
 
-    public int getPreu() { return this.preu; }
+		public int getPreu() { return this.preu; }
 
-    public int getEstoc() { return this.estoc; }
+		public int getEstoc() { return this.estoc; }
 
-    //setters
-    public void setPreu(int preu) {
-        if (preu > 0) {
-            this.preu = preu;
-        }
-    }
+		//setters
+		public void setPreu(int preu) {
+				if (preu > 0) {
+						this.preu = preu;
+				}
+		}
 
-    public void setEstoc(int estoc) {
-        if (estoc > 0) {
-            this.estoc = estoc;
-        }
-    }
-    //altres
-    public boolean esValid() {
-        if (getPreu() < 0) {
-            return false;
-        }
-        if (getEstoc() < 0) {
-            return false;
-        }
-        //nom
-        return true;
-    }
+		public void setEstoc(int estoc) {
+				if (estoc > 0) {
+						this.estoc = estoc;
+				}
+		}
+		//altres
+		public boolean esValid() {
+				if (getPreu() < 0) {
+						return false;
+				}
+				if (getEstoc() < 0) {
+						return false;
+				}
+				if (nom.equals("NOM NO VÀLID!")) {
+						return false;
+				}
+				return true;
+		}
 
-    public static String normalitzaNom(String nom) {
-        nom.trim();
-        if (nom.isEmpty()) {
-				nom = "NOM NO VÀLID!";
-            return nom;
-        }
-        if (nom.isBlank()) {
-				nom = "NOM NO VÀLID!";
-            return nom;
-        }
-        return nom;
-    }
+		public static String normalitzaNom(String nom) {
+				nom.trim();
+				if (nom.isEmpty()) {
+						nom = "NOM NO VÀLID!";
+						return nom;
+				}
+				if (nom.isBlank()) {
+						nom = "NOM NO VÀLID!";
+						return nom;
+				}
+				return nom;
+		}
 
-    @Override 
-    public String toString() {
-			String paraula = String.format("%n    Vi: %s%n    Preu: %d%n    Estoc: %d%n" ,nom,preu,estoc);
-			return paraula;
-    }
+		@Override 
+		public String toString() {
+				String paraula = String.format("%n    Vi: %s%n    Preu: %d%n    Estoc: %d%n" ,nom,preu,estoc);
+				return paraula;
+		}
 }
