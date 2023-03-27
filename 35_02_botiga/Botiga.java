8b7dd186fcb44273748693e2854007c6b9fe2863
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-03-27 23:36:21 +0200

    35_02_primer intent  ∾ prgtest autocommit 35_02 #11

--- 35_02_botiga/Botiga.java
+++ 35_02_botiga/Botiga.java
@@ -16,7 +16,7 @@ public class Botiga {
 				}
 				for (int i = 0; i < vins.length; i++) {
 						if (vins[i] != null)  {
-								if (vins[i].getNom().equals(vi)) {
+								if (vins[i].getNom().equals(vi.getNom())) {
 										return null;
 								}
 						}
