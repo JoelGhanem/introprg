da7f95d676f1cb2f1daa65a3fd40011a10528ff8
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-04-01 21:57:31 +0200

    35_02_fet  ∾ prgtest autocommit 35_03 #28

--- 35_03_entorn/Entorn.java
+++ 35_03_entorn/Entorn.java
@@ -1,53 +1,53 @@
 /*Aquest arxiu és amb el que la senyora Estrella podrá fer servir per interaccionar amb la botiga que tenen les classes Vi i Botiga*/
 public class Entorn {
-    private final Botiga botiga = new Botiga();
-    public static void main(String[] args) {
-        Entorn entorn = new Entorn();
-        mostraBenvinguda();
-        while (true) {
-            mostraPrompt();
-            String comanda = Entrada.readLine().strip();
-            if (comanda.isEmpty()) continue;
-            if (comanda.equals("surt")) break;
-            switch (comanda) {
-                case "ajuda": mostraAjuda();
-                             break;
-                case "afegeix": entorn.processaAfegeix();
-                           break;
-                case "cerca": entorn.processaCerca();
-                             break;
-                case "modifica": entorn.processaModifica();
-                            break;
-                case "elimina": entorn.processaElimina();
-                           break;
-                default: mostraErrorComandaDesconeguda();
-            }
-        }
-        mostraComiat();
-    }
-	public static Entorn mostraBenvinguda() {
-			System.out.println("Celler La Bona Estrella. Escriviu ajuda per veure opcions.");
-			return null;
-	}
-	public static Entorn mostraComiat() {
-			System.out.println("adéu");
-			return null;
-	}
-	public static Entorn mostraPrompt() {
-			System.out.print("botiga> ");
-			return null;
-	}
-	public static Entorn mostraAjuda() {
-			System.out.println("Comandes disponibles:");
-			System.out.println("ajuda");
-			System.out.println("cerca");
-			System.out.println("afegeix");
-			System.out.println("modifica");
-			System.out.println("elimina");
-			System.out.println("surt");
-			return null;
-	}
-	public Entorn processaAfegeix() {
+		private final Botiga botiga = new Botiga();
+		public static void main(String[] args) {
+				Entorn entorn = new Entorn();
+				mostraBenvinguda();
+				while (true) {
+						mostraPrompt();
+						String comanda = Entrada.readLine().strip();
+						if (comanda.isEmpty()) continue;
+						if (comanda.equals("surt")) break;
+						switch (comanda) {
+								case "ajuda": mostraAjuda();
+											  break;
+								case "afegeix": entorn.processaAfegeix();
+												break;
+								case "cerca": entorn.processaCerca();
+											  break;
+								case "modifica": entorn.processaModifica();
+												 break;
+								case "elimina": entorn.processaElimina();
+												break;
+								default: mostraErrorComandaDesconeguda();
+						}
+				}
+				mostraComiat();
+		}
+		public static Entorn mostraBenvinguda() {
+				System.out.println("Celler La Bona Estrella. Escriviu ajuda per veure opcions.");
+				return null;
+		}
+		public static Entorn mostraComiat() {
+				System.out.println("adéu");
+				return null;
+		}
+		public static Entorn mostraPrompt() {
+				System.out.print("botiga> ");
+				return null;
+		}
+		public static Entorn mostraAjuda() {
+				System.out.println("Comandes disponibles:");
+				System.out.println("ajuda");
+				System.out.println("cerca");
+				System.out.println("afegeix");
+				System.out.println("modifica");
+				System.out.println("elimina");
+				System.out.println("surt");
+				return null;
+		}
+		public Entorn processaAfegeix() {
 				/*Afegeix*/
 				int precioInt;
 				int estocInt;
@@ -70,7 +70,7 @@ public class Entorn {
 						/*Checks if the price is an integer*/
 						if (UtilString.esEnter(precio)) {
 								precioInt = Integer.parseInt(precio);
-								/*If the stock is a negative number you skip the procedure and prints an error*/
+								/*If the price is a negative number you skip the procedure and prints an error*/
 								if (precioInt < 0) {
 										System.out.println("ERROR: el valor ha de ser un enter positiu");
 								} else {
@@ -80,18 +80,37 @@ public class Entorn {
 								System.out.println("ERROR: ha de ser un enter");
 						}
 				}
+				System.out.println("estoc (enter sense estoc)> ");
+				String estoc = Entrada.readLine();
+				/*Checks if stock is empty and sets the price to 0*/
+				if (estoc.isEmpty()) {
+						vino.setEstoc(0);
+				} else {
+						/*Checks if the price is integer*/
+						if (UtilString.esEnter(estoc)) {
+								estocInt = Integer.parseInt(estoc);
+								/*If the stock is a negativ number you skip the procedure and prints an error*/
+								if (estocInt <0) {
+										System.out.println("ERROR: el valor ha de ser un enter positiu");
+								} else {
+										vino.setEstoc(estocInt);
+								}
+						} else {
+								System.out.println("ERROR: ha de ser un enter");
+						}
+				}
 				return null;
 		}
-	public Entorn processaCerca() {
-			return null;
-	}
-	public Entorn processaModifica() {
-			return null;
-	}
-	public Entorn processaElimina() {
-			return null;
-	}
-	public static Entorn mostraErrorComandaDesconeguda() {
-			return null;
-	}
+		public Entorn processaCerca() {
+				return null;
+		}
+		public Entorn processaModifica() {
+				return null;
+		}
+		public Entorn processaElimina() {
+				return null;
+		}
+		public static Entorn mostraErrorComandaDesconeguda() {
+				return null;
+		}
 }
