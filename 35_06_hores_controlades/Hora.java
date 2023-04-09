a5b37e865db90b1e0e72b8df2ab4d8e5185bd063
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-04-09 14:02:06 +0200

    35_06_V1

---
+++ 35_06_hores_controlades/Hora.java
@@ -0,0 +1,31 @@
+/*Classe hora que incorpora els metodes getHores,minuts i segons a mes de els metodes setHores,minuts,segons i e metode toString*/
+public class Hora {
+		private int hores;
+		private int minuts;
+		private int segons;
+		public int getHores() { return this.hores; }
+		public int getMinuts() { return this.minuts; }
+		public int getSegons() { return this.segons; }
+
+		public boolean setHores(int hores) {
+				if (hores >= 0 && hores <= 24) {
+						this.hores = hores;
+						return true;
+				}
+				return false;
+		}
+		public boolean setMinuts(int minuts) {
+				if( minuts >=0 && minuts <60) {
+						this.minuts = minuts;
+						return true;
+				}
+				return false;
+		}
+		public boolean setSegons(int segons) {
+				if (segons >= 0 && segons <60) {
+						this.segons = segons;
+						return true;
+				}
+				return false;
+		}
+}
