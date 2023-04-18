0df9562dbd1dbe7d30dfaa9ac7ace61e46d48b90
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-04-18 20:00:30 +0200

    35_10_enProces  ∾ prgtest autocommit 35_10 #2

--- 35_10_gatposicio/GatPosicio.java
+++ 35_10_gatposicio/GatPosicio.java
@@ -1,18 +1,11 @@
 /*Aquesta classe ens permet a aprendre a tractar amb els enums */
-public class GatPosicio {
-	enum posicio {
-		DRET,
-		ASSEGUT,
-		ESTIRAT;
-	}
+public enum GatPosicio {
+	DRET, ASSEGUT, ESTIRAT;
 	public static GatPosicio fromString(String algo) {
-		GatPosicio posicio = new GatPosicio();
-		for(posicio position : GatPosicio.posicio.values()) {
-			if(position.name().equalsIgnoreCase(algo)) {
-				return posicio;
-			}
+		try{
+			return valueOf(algo.toUpperCase());
+		} catch (IllegalArgumentException e){
+			throw new IllegalArgumentException("nanai de la xina");
 		}
-		throw new IllegalArgumentException("nanai de la xina");
 	}
-
 }
