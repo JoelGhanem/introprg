ccb3fc1d7886818fde4c48bc4c7e7220170a8113
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-04-20 23:14:40 +0200

    35_13_benFet  ∾ prgtest autocommit 35_05 #13

--- 35_05_vi_ampliat/Vi.java
+++ 35_05_vi_ampliat/Vi.java
@@ -1,132 +1,140 @@
 /*Una botiga de vi sense vi no és res. Així que començarem implementant aquesta classe tant important. Aquesta classe es la classe Vi on veiem els noms, els preus i la quantitat d'estoc segones el vi*/
 public class Vi {
-	private String ref;
-	private String nom;
-	private int preu;
-	private int estoc = 0;
-	private String lloc;
-	private String origen;
-	private String tipus;
-	private String collita;
+		private String ref;
+		private String nom;
+		private int preu;
+		private int estoc = 0;
+		private String lloc;
+		private String origen;
+		private String tipus;
+		private String collita;
 
-	//constructors
-	public Vi(String referencia, String nom, int preu, int estoc, String lloc, String origen, String tipus, String collita) {
-		this.ref= ref;
-		this.nom = normalitzaString(nom);
-		if (preu > 0) {
-			this.preu = preu;
-		} else {
-			this.preu = -1;
+		//constructors
+		public Vi(String referencia, String nom, int preu, int estoc, String lloc, String origen, String tipus, String collita) {
+				this.ref= ref;
+				this.nom = normalitzaString(nom);
+				if (preu > 0) {
+						this.preu = preu;
+				} else {
+						this.preu = -1;
+				}
+				if (estoc > 0) {
+						this.estoc = estoc;
+				} else {
+						this.estoc = -1;
+				}
+				this.lloc = lloc;
+				this.origen = origen;
+				this.tipus = tipus;
+				this.collita = collita;
 		}
-		if (estoc > 0) {
-			this.estoc = estoc;
-		} else {
-			this.estoc = -1;
+
+		//getters
+		public String getRef() {return this.ref;}
+
+		public String getNom() { return this.nom; }
+
+		public int getPreu() { return this.preu; }
+
+		public int getEstoc() { return this.estoc; }
+
+		public String getLloc() {return this.lloc;}
+
+		public String getOrigen() { return this.origen;}
+
+		public String getTipus() { return this.tipus;}
+
+		public String getCollita() { return this.collita;}
+
+		//setters
+		public void setRef(String ref) {
+				this.ref = ref;
 		}
-		this.lloc = lloc;
-		this.origen = origen;
-		this.tipus = tipus;
-		this.collita = collita;
-	}
 
-	//getters
-	public String getRef() {return this.ref;}
-
-	public String getNom() { return this.nom; }
-
-	public int getPreu() { return this.preu; }
-
-	public int getEstoc() { return this.estoc; }
-
-	public String getLloc() {return this.lloc;}
-
-	public String getOrigen() { return this.origen;}
-
-	public String getTipus() { return this.tipus;}
-
-	public String getCollita() { return this.collita;}
-
-	//setters
-	public void setRef(String ref) {
-		this.ref = ref;
-	}
-
-	public void setNom(String nom){
-		this.nom = nom;
-	}
-	public void setPreu(int preu) {
-		if (preu > 0) {
-			this.preu = preu;
+		public void setNom(String nom){
+				this.nom = nom;
 		}
-	}
+		public void setPreu(int preu) {
+				if (preu > 0) {
+						this.preu = preu;
+				}
+		}
 
-	public void setEstoc(int estoc) {
-		if (estoc > 0) {
-			this.estoc = estoc;
+		public void setEstoc(int estoc) {
+				if (estoc > 0) {
+						this.estoc = estoc;
+				}
 		}
-	}
 
-	public void setLloc(String lloc) {
-		this.lloc = lloc;
-	}
+		public void setLloc(String lloc) {
+				this.lloc = lloc;
+		}
 
-	public void setOrigen(String origen) {
-		this.origen = origen;
-	}
+		public void setOrigen(String origen) {
+				this.origen = origen;
+		}
 
-	public void setTipus(String tipus) {
-		this.tipus = tipus;
-	}
+		public void setTipus(String tipus) {
+				this.tipus = tipus;
+		}
 
-	public void setCollita(String collita) {
-		this.collita = collita;
-	}
-	//altres
-	public boolean esValid() {
-		if (getRef() == null) {
-			return false;
+		public void setCollita(String collita) {
+				this.collita = collita;
 		}
-		if (getPreu() < 0) {
-			return false;
+		//altres
+		public boolean esValid() {
+				if (getRef() == null) {
+						return false;
+				}
+				if (getPreu() < 0) {
+						return false;
+				}
+				if (getEstoc() < 0) {
+						return false;
+				}
+				if (getNom() == null) {
+						return false;
+				}
+				if (getLloc() == null) {
+						return false;
+				}
+				if (getOrigen() == null) {
+						return false;
+				}
+				if (getTipus() == null) {
+						return false;
+				}
+				if (getCollita() == null) {
+						return false;
+				}
+				return true;
 		}
-		if (getEstoc() < 0) {
-			return false;
-		}
-		if (getNom() == null) {
-			return false;
-		}
-		if (getLloc() == null) {
-			return false;
-		}
-		if (getOrigen() == null) {
-			return false;
-		}
-		if (getTipus() == null) {
-			return false;
-		}
-		if (getCollita() == null) {
-			return false;
-		}
-		return true;
-	}
 
-	public static String normalitzaString(String nom) {
-		String nomBien;
-		nom.trim();
-		if (nom.isEmpty()||nom.isBlank() || nom == null) {
-			return null;
+		public static String normalitzaString(String nom) {
+				String nomBien;
+				nom.trim();
+				if (nom.isEmpty()||nom.isBlank() || nom == null) {
+						return null;
+				}
+				nomBien = nom.trim().replaceAll("\\s+", " ");
+				return nomBien;
 		}
-		nomBien = nom.trim().replaceAll("\\s+", " ");
-		return nomBien;
-	}
 
-	@Override 
-	public String toString() {
-		String paraula = String.format("%n   Referència:%s%n  Vi: %s%n    Preu: %d%n    Estoc: %d%n    Lloc:%s%n     Origen:%s%n  Tipus:%s%n   Collita%s%n" ,this.ref,this.nom,preu,estoc, this.lloc, this.origen, this.tipus, this.collita);
-		return paraula;
-	}
-	public static Vi deArrayString(String[] strings) {
-		if(strings.length != 8 || !UtilString.esEnter(strings[1]) || !UtilString.esEnter(strings[2])) { System.out.println("es nulo a la primera");return null; }
+		@Override 
+		public String toString() {
+				String paraula = String.format("%n   Referència:%s%n  Vi: %s%n    Preu: %d%n    Estoc: %d%n    Lloc:%s%n     Origen:%s%n  Tipus:%s%n   Collita%s%n" ,this.ref,this.nom,preu,estoc, this.lloc, this.origen, this.tipus, this.collita);
+				return paraula;
+		}
+		public static Vi deArrayString(String[] strings) {
+				if(strings.length != 8) {
+						System.out.println("es nulo a la primera");
+				return null;}
+		if (!UtilString.esEnter(strings[1])) {
+				System.out.println("es nulo a la segunda");
+				return null;} 
+		if (!UtilString.esEnter(strings[2])) {
+				System.out.println("es nulo a la tercera");
+				return null;}
 		if(Integer.parseInt(strings[1]) < 0) { return null;}
 		if(Integer.parseInt(strings[2]) < 0) { return null;}
 		Vi vinito = new Vi("","",0,0,"","","","");
@@ -140,21 +148,21 @@ public class Vi {
 		vinito.setCollita(normalitzaString(strings[7]));
 		if(vinito.esValid()) {
 				//System.out.println(vinito.esValid());
-			return vinito;
+				return vinito;
 		}
 		return null;
-	}
+}
 
-	public String[] aArrayString() {
+public String[] aArrayString() {
 		String[] strings = new String[7];
 		strings[0] = this.ref;
 		strings[1] = this.nom;
-	strings[2]= Integer.toString(this.preu);
+		strings[2]= Integer.toString(this.preu);
 		strings[3] = Integer.toString(this.estoc);
 		strings[4] = this.lloc;
 		strings[5] = this.origen;
 		strings[6] = this.tipus;
 		strings[7] = this.collita;
 		return strings;
-	}
+}
 }
