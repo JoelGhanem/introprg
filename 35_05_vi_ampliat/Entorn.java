0de82c535dda547ca76c1822e6000089cfe87b9b
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-04-26 00:10:45 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 35_05 #19

--- 35_05_vi_ampliat/Entorn.java
+++ 35_05_vi_ampliat/Entorn.java
@@ -241,7 +241,7 @@ public class Entorn {
 						System.out.print("Trobat:") ;
 						System.out.println(botiga.cerca(vino).toString());
 						} else {
-												System.out.println("ERROR: el valor havia de ser un enter positiu");
+												System.out.println("ERROR: el valor ha de ser un enter positiu");
 						}
 				} else if(refVino.equals("!")) {
 						return null;
