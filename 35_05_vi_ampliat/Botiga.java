b6c4b47924c4a845f9432fb6aec45a038bbd8555
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-04-26 11:10:54 +0200

    35_05_ el test 13 esta raro porque dice que si no encuentra el vino que pide devuelvas el primero pero el prgtest devuelve el 5 y el mio el 1  ∾ prgtest autocommit 35_05 #1

--- 35_05_vi_ampliat/Botiga.java
+++ 35_05_vi_ampliat/Botiga.java
@@ -79,33 +79,28 @@ public Botiga() {
         if (! plantilla.getRef().isEmpty() && ! plantilla.getRef().equalsIgnoreCase(vi.getRef())) continue;
 		}
 		if(plantilla.getNom() != null) {
-		//System.out.println(plantilla.getRef() + " || + " + vi.getRef());
         if (! plantilla.getNom().isEmpty() && ! plantilla.getNom().equalsIgnoreCase(vi.getNom())) continue;
 		}
 		if(plantilla.getPreu() < 0) {
-		//System.out.println(plantilla.getNom() + " || + " + vi.getNom());
+				int preuMax = plantilla.getPreu();
+				int preuVi = vi.getPreu();
+				if (preuMax >= vi.getPreu()) continue;
         if (plantilla.getPreu() >= 0 && plantilla.getPreu() >= vi.getPreu()) continue;
 		}
 		if(plantilla.getEstoc() < 0) {
-		//System.out.println(plantilla.getPreu() + " || + " + vi.getPreu());
 		if (plantilla.getEstoc() >= 0 && plantilla.getEstoc() <= vi.getEstoc()) continue;
 		}
 		if(plantilla.getLloc() != null) {
-		//System.out.println(plantilla.getEstoc() + " || + " + vi.getEstoc());
         if (! plantilla.getLloc().isEmpty() && ! plantilla.getLloc().equalsIgnoreCase(vi.getLloc())) continue;
 		}
 		if(plantilla.getOrigen() != null) {
-		//System.out.println(plantilla.getLloc() + " || + " + vi.getLloc());
         if (! plantilla.getOrigen().isEmpty() && ! plantilla.getOrigen().equalsIgnoreCase(vi.getOrigen())) continue;
 		}
 		if(plantilla.getTipus() != null) {
-		//System.out.println(plantilla.getOrigen() + " || + " + vi.getOrigen());
         if (! plantilla.getTipus().isEmpty() && ! plantilla.getTipus().equalsIgnoreCase(vi.getTipus())) continue;
 		}
 		if(plantilla.getCollita() != null) {
-		//System.out.println(plantilla.getTipus() + " || + " + vi.getTipus());
         if (! plantilla.getCollita().isEmpty() && ! plantilla.getCollita().equalsIgnoreCase(vi.getCollita())) continue;
-		//System.out.println(plantilla.getCollita() + " || + " + vi.getCollita());
 		}
         return vi;
     }
