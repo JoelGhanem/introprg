fce8af2135afbc97cfdd5dcd575689c92d5a8c7e
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-04-29 15:10:56 +0200

    35_05_Junit

---
+++ 35_11_viprotegit/Tipus.java
@@ -0,0 +1,7 @@
+/*Aquest enum ens permet especificar el tipus de vi, pot ser blanc negre rosat o altre*/
+public enum Tipus {
+		BLANC("Blanc"),
+		NEGRE("Negre"),
+		ROSAT("Rosat"),
+		ALTRE("Altre");
+}
