9f83c5c7ae5fa58deb4405b9e49415064dc7060f
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-04-29 14:00:30 +0200

    prueba

--- 35_11_viprotegit/BotigaException.java
+++
@@ -1,9 +0,0 @@
-/*Aquesta classe es la excepcio botiga que hem creat per aquesta botiga en concret*/
-public class BotigaException extends Exception{
-		public BotigaException() {
-				super("Botiga plena");
-		}
-		public BotigaException(String text) {
-				super(text);
-		}
-}
