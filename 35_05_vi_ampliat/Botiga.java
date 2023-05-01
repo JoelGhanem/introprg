ef6bdda90236530d9247fc251646ab0550cdb4d6
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-01 17:43:26 +0200

    pull  ∾ prgtest autocommit 35_05 #7

--- 35_05_vi_ampliat/Botiga.java
+++ 35_05_vi_ampliat/Botiga.java
@@ -53,6 +53,10 @@ public class Botiga {
 										//creo una copia del vino que hay que borrar
 										Vi vinito = new Vi("","",-1,-1,"","","","");
 										vinito = vins[i];
+										if (vinito.getEstoc() > 0) {
+												return null;
+										}
+										//borro el vino del array
 										vins[i] = null;
 										//devuelvo el vino que he borrado
 										return vinito;
