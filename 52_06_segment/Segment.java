d4c8dfb2d8da9b20b4a267d819120cbb6f9f82da
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-02 13:53:36 +0200

    52_06_v1  ∾ prgtest autocommit 52_06 #1

--- 52_06_segment/Segment.java
+++ 52_06_segment/Segment.java
@@ -2,10 +2,6 @@
 public class Segment {
 		private Punt p1;
 		private Punt p2;
-		public int x1 = p1.getX();
-		public int y1 = p1.getY();
-		public int x2 = p2.getX();
-		public int y2 = p2.getY();
 		//constructor
 		public Segment() {
 		}
@@ -21,6 +17,10 @@ public class Segment {
 		public void setP2(Punt p2) { this.p2 = p2;}
 		//Otros
 		public Double longitud() {
+		int x1 = p1.getX();
+		int y1 = p1.getY();
+		int x2 = p2.getX();
+		int y2 = p2.getY();
 				double longitud = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
 				return longitud;
 		}
