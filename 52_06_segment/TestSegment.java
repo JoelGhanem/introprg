e763fd0602134d778c3e96dad2df7c7f92800fc3
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-03 12:51:50 +0200

    52_06_enProces  ∾ prgtest autocommit 52_06 #10

--- 52_06_segment/TestSegment.java
+++ 52_06_segment/TestSegment.java
@@ -6,8 +6,28 @@ import jdk.jfr.Timestamp;
 
 public class TestSegment {
 		@Test
-		public void TestSegment(){
+		public void TestSegmentDefault(){
 				Segment s = new Segment();
+				assertAll(
+								() -> assertEquals(new Punt(), s.getP1()),
+								() -> assertEquals(new Punt(), s.getP2())
+						 );
+		}
+		@Test 
+		public void TestEspecific() {
+				Punt p1 = new Punt(1,2);
+				Punt p2 = new Punt(3,4);
+				Segment s = new Segment(p1,p2);
+				assertAll(
+								() -> assertEquals(p1, s.getP1()),
+								() -> assertEquals(p2, s.getP2())
+						 );
+
+		}
+		@Test
+		public void TestLongitud() {
+				Segment s = new Segment(new Punt(0,0), new Punt(0,0));
+				assertEquals(0, s.longitud());
 		}
 
 }
