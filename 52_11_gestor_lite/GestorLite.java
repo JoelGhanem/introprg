5109d6f0f778999f9185acf6c1cfea752651dbf4
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-08 18:56:33 +0200

    52_11_en_Proces  ∾ prgtest autocommit 52_11 #7

--- 52_11_gestor_lite/GestorLite.java
+++ 52_11_gestor_lite/GestorLite.java
@@ -11,7 +11,7 @@ public class GestorLite {
 		public static void mostraClient(Client client) {
 			System.out.printf("Client: %s%n        %s%n        %s%nLloguers: %s%n", client.getNom(),client.getNif(),client.getTelefon(),client.getSize());
 			for (int i = 0; i < client.getSize(); i++) {
-				 System.out.printf("%d. vehicle: %s%s%n        dies llogats: %d%n", i+1,client.getLloguers(i).getVehicle().getMarca(),client.getLloguers(i).getVehicle().getModel(),client.getLloguers(i).getDies());
+				 System.out.printf("        %d. vehicle: %s%s%n        dies llogats: %d%n", i+1,client.getLloguers(i).getVehicle().getMarca(),client.getLloguers(i).getVehicle().getModel(),client.getLloguers(i).getDies());
 			}
 		}
 }
