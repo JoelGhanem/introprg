761dff7f0ccdb073f5fe2391b7d367dfa2378e71
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-09 12:41:56 +0200

    52_13_v1  ∾ prgtest autocommit 52_13 #8

--- 52_13_init_test/Client.java
+++ 52_13_init_test/Client.java
@@ -26,9 +26,17 @@ public class Client {
 		public void setNom(String nom) { this.nom = nom; }
 		public void setTelefon(String telefon) { this.telefon = telefon; }
 		public String informe() {
+				double precio;
 				System.out.println("Informe de lloguers del client "+ getNom() + " (" + getNif() + ")");
 				for(int i = 0; i < lloguers.size(); i++) {
-						System.out.printf("  	%s %s: %s€%n",getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel(),"13");
+						if (getLloguers(i).getVehicle().getMarca().equals("Seat")) {
+								precio = 90.0;
+						} else if (getLloguers(i).getVehicle().getMarca().equals("Tata")) {
+								precio = 180.0;
+						} else {
+								precio = 540.0;
+						}
+						System.out.printf("  	%s %s: %d€%n",getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel(),precio);
 				}
 				return "";
 		}
