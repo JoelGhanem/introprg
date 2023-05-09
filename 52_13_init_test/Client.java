89864d43a022280fcb211b1995737790b2de2b78
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-09 12:32:47 +0200

    52_13_v1  ∾ prgtest autocommit 52_13 #4

--- 52_13_init_test/Client.java
+++ 52_13_init_test/Client.java
@@ -1,35 +1,35 @@
 /*Aquest arxiu ens representa un client amb un nif, un nom, un telefon i un informe*/
 import java.util.ArrayList;
 public class Client {
-    private String nif;
-    private String nom;
-    private String telefon;
-    private ArrayList<Lloguer> lloguers;
-    public Client(String nif, String nom, String telefon) {
-        this.nif = nif;
-        this.nom = nom;
-        this.telefon = telefon;
-        this.lloguers = new ArrayList<Lloguer>();
-        Lloguer lloguer = new Lloguer(new Vehicle("Seat","600",1), 2);
-        lloguers.add(lloguer);
-        Lloguer lloguer1 = new Lloguer(new Vehicle("Tata","Vista",1), 5);
-        lloguers.add(lloguer1);
-        Lloguer lloguer2 = new Lloguer(new Vehicle("Fiat","Uno",1), 3);
-        lloguers.add(lloguer2);
-    }
-    public String getNif()     { return nif; }
-    public String getNom()     { return nom; }
-    public String getTelefon() { return telefon; }
-    public Lloguer getLloguers(int posicio) { return lloguers.get(posicio); }
-    public int getSize() { return lloguers.size();}
-    public void setNif(String nif) { this.nif = nif; }
-    public void setNom(String nom) { this.nom = nom; }
-    public void setTelefon(String telefon) { this.telefon = telefon; }
-    public String informe() {
-System.out.print("Informe de lloguers del client ");
-			for(int i = 0; i < lloguers.size(); i++) {
-			System.out.printf("%s (%s)%n	%s %s: %s€",getNom(),getNif(),getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel(),"13");
-			}
-			return "";
-    }
+		private String nif;
+		private String nom;
+		private String telefon;
+		private ArrayList<Lloguer> lloguers;
+		public Client(String nif, String nom, String telefon) {
+				this.nif = nif;
+				this.nom = nom;
+				this.telefon = telefon;
+				this.lloguers = new ArrayList<Lloguer>();
+				Lloguer lloguer = new Lloguer(new Vehicle("Seat","600",1), 2);
+				lloguers.add(lloguer);
+				Lloguer lloguer1 = new Lloguer(new Vehicle("Tata","Vista",1), 5);
+				lloguers.add(lloguer1);
+				Lloguer lloguer2 = new Lloguer(new Vehicle("Fiat","Uno",1), 3);
+				lloguers.add(lloguer2);
+		}
+		public String getNif()     { return nif; }
+		public String getNom()     { return nom; }
+		public String getTelefon() { return telefon; }
+		public Lloguer getLloguers(int posicio) { return lloguers.get(posicio); }
+		public int getSize() { return lloguers.size();}
+		public void setNif(String nif) { this.nif = nif; }
+		public void setNom(String nom) { this.nom = nom; }
+		public void setTelefon(String telefon) { this.telefon = telefon; }
+		public String informe() {
+				System.out.print("Informe de lloguers del client "+ getNom() + "(" + getNif() + ")");
+				for(int i = 0; i < lloguers.size(); i++) {
+						System.out.printf("  	%s %s: %s€",getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel(),"13");
+				}
+				return "";
+		}
 }
