729ce02d76824d695b4ac795b50ddb01a1a3b93d
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-09 12:43:23 +0200

    52_13_v1  ∾ prgtest autocommit 52_13 #10

--- 52_13_init_test/Client.java
+++ 52_13_init_test/Client.java
@@ -36,7 +36,7 @@ public class Client {
 						} else {
 								precio = 540.0;
 						}
-						System.out.printf("  	%s %s: %d€%n",getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel(),precio);
+						System.out.printf("  	%s %s: %f€%n",getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel(),precio);
 				}
 				return "";
 		}
