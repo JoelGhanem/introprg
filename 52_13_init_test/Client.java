2ab78c6eaefd54e350a076acf1d4668d86e57c60
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-10 13:44:23 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 52_13 #1

--- 52_13_init_test/Client.java
+++ 52_13_init_test/Client.java
@@ -26,21 +26,10 @@ public class Client {
 		public void setNom(String nom) { this.nom = nom; }
 		public void setTelefon(String telefon) { this.telefon = telefon; }
 		public String informe() {
-				int precio = 0;
-				String preu = "";
 				System.out.println("Informe de lloguers del client "+ getNom() + " (" + getNif() + ")");
 				for(int i = 0; i < lloguers.size(); i++) {
-						if (getLloguers(i).getVehicle().getMarca().equals("Seat")) {
-								precio = 90;
-						} else if (getLloguers(i).getVehicle().getMarca().equals("Tata")) {
-								precio = 180;
-						} else {
-								precio = 540;
-						}
-						System.out.printf("	%s %s: %d%s€%n",getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel(),precio,".0");
-}
-						System.out.println("Import a pagar: 810.0€");
-						System.out.println("Punts guanyats: 4");
+						System.out.printf("  	%s %s: %s€%n",getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel());
+				}
 				return "";
 		}
 }
