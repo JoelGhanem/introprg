c77318dd107603fcdb0c6c0e9022f76d77d1e1d1
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-10 20:08:52 +0200

    52_13_enProces  ∾ prgtest autocommit 52_13 #4

--- 52_13_init_test/TestGestor.java
+++ 52_13_init_test/TestGestor.java
@@ -17,12 +17,13 @@ public class TestGestor {
 		lloguers.add(lloguer1);
 		lloguers.add(lloguer2);
 		lloguers.add(lloguer3);
-		String resultatEsperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n "+
+		String resultatEsperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)%n "+
 				"	Seat 600: 90€%n" +
 				"	Tata Vista: 180.0€%n" +
 				"	Fiat Uno: 540.0€%n" +
 				"Import a pagar 810.0€%n" +
 				"Punt% Guanyats: 4%n";
+		System.out.println(resultatEsperat);
 		assertEquals(resultatEsperat, client.informe());
 		}
 }
