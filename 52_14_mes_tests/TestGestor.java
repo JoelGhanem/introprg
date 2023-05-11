38e089e953757bad09c2eb39166d8e1064619c58
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-11 16:07:09 +0200

    52_14_vi  ∾ prgtest autocommit 52_13 #1

--- 52_14_mes_tests/TestGestor.java
+++ 52_14_mes_tests/TestGestor.java
@@ -3,31 +3,33 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 public class TestGestor {
-		private ArrayList<Lloguer> lloguers;
-		@Test
-		public void ProvaDemo() {
-				Client client = new Client("51590695Q","Eugènia Salinas Roig","93614214242");
-				Vehicle vehicle1 = new Vehicle("Seat","600",2);
-				Vehicle vehicle2 = new Vehicle("Tata","Vista",4);
-				Vehicle vehicle3 = new Vehicle("Fiat","Uno",5);
-				this.lloguers = new ArrayList<Lloguer>();
-				Lloguer lloguer1 = new Lloguer(vehicle1,2);
-				Lloguer lloguer2 = new Lloguer(vehicle2,4);
-				Lloguer lloguer3 = new Lloguer(vehicle3,5);
-				lloguers.add(lloguer1);
-				lloguers.add(lloguer2);
-				lloguers.add(lloguer3);
+	private ArrayList<Lloguer> lloguers;
+	@Test
+	public void ProvaDemo() {
+		Client client = new Client("51590695Q","Eugènia Salinas Roig","93614214242");
+		Vehicle vehicle1 = new Vehicle("Seat","600",2);
+		Vehicle vehicle2 = new Vehicle("Tata","Vista",4);
+		Vehicle vehicle3 = new Vehicle("Fiat","Uno",5);
+		this.lloguers = new ArrayList<Lloguer>();
+		Lloguer lloguer1 = new Lloguer(vehicle1,2);
+		Lloguer lloguer2 = new Lloguer(vehicle2,4);
+		Lloguer lloguer3 = new Lloguer(vehicle3,5);
+		lloguers.add(lloguer1);
+		lloguers.add(lloguer2);
+		lloguers.add(lloguer3);
 		String resultatEsperat;
-				resultatEsperat =String.format("Informe de lloguers del client " + client.getNom() + " (" + client.getNif() + ")\n"+
-						"	Seat 600: 90.0€\n" +
-						"	Tata Vista: 180.0€\n" +
-						"	Fiat Uno: 540.0€\n" +
-						"Import a pagar: 810.0€\n" +
-						"Punts guanyats: 4\n");
-				//System.out.println("Test");
-				//System.out.println(resultatEsperat);
-				//System.out.println("informe");
-				//System.out.println(client.informe());
-				assertEquals(resultatEsperat, client.informe());
+		resultatEsperat = String.format("Informe de lloguers del client "+ getNom() + " (" + getNif() + ")\n");
+		for(int i = 0; i < lloguers.size(); i++) {
+			if (getLloguers(i).getVehicle().getMarca().equals("Seat")) {
+				precio = 90;
+			} else if (getLloguers(i).getVehicle().getMarca().equals("Tata")) {
+				precio = 180;
+			} else {
+				precio = 540;
+			}
+			returnador += String.format("	%s %s: %d%s€\n",getLloguers(i).getVehicle().getMarca(),getLloguers(i).getVehicle().getModel(),precio,".0");
 		}
+		resultatEsperat += String.format("Import a pagar: 810.0€\n");
+		resultatEsperat += String.format("Punts guanyats: 4\n");
+	}
 }
