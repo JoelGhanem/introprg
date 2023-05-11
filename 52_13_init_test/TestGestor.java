d4755bd9633f2e6373f27e3680d159a671cc8e6a
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-11 13:15:05 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 52_13 #0

--- 52_13_init_test/TestGestor.java
+++ 52_13_init_test/TestGestor.java
@@ -6,27 +6,27 @@ public class TestGestor {
 		private ArrayList<Lloguer> lloguers;
 		@Test
 		public void ProvaDemo() {
-		Client client = new Client("51590695Q","Eugènia Salinas Roig","93614214242");
-		Vehicle vehicle1 = new Vehicle("Seat","600",1);
-		Vehicle vehicle2 = new Vehicle("Tata","Vista",1);
-		Vehicle vehicle3 = new Vehicle("Fiat","Uno",1);
-		 this.lloguers = new ArrayList<Lloguer>();
-		Lloguer lloguer1 = new Lloguer(vehicle1,2);
-		Lloguer lloguer2 = new Lloguer(vehicle2,4);
-		Lloguer lloguer3 = new Lloguer(vehicle3,5);
-		lloguers.add(lloguer1);
-		lloguers.add(lloguer2);
-		lloguers.add(lloguer3);
-		String resultatEsperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n"+
-				"	Seat 600: 90.0€\n" +
-				"	Tata Vista: 180.0€\n" +
-				"	Fiat Uno: 540.0€\n" +
-				"Import a pagar: 810.0€\n" +
-				"Punts guanyats: 4\n";
-		System.out.println("Test");
-		System.out.println(resultatEsperat);
-		System.out.println("informe");
-		System.out.println(client.informe());
-		assertEquals(resultatEsperat, client.informe());
+				Client client = new Client("51590695Q","Eugènia Salinas Roig","93614214242");
+				Vehicle vehicle1 = new Vehicle("Seat","600",1);
+				Vehicle vehicle2 = new Vehicle("Tata","Vista",1);
+				Vehicle vehicle3 = new Vehicle("Fiat","Uno",1);
+				this.lloguers = new ArrayList<Lloguer>();
+				Lloguer lloguer1 = new Lloguer(vehicle1,2);
+				Lloguer lloguer2 = new Lloguer(vehicle2,4);
+				Lloguer lloguer3 = new Lloguer(vehicle3,5);
+				lloguers.add(lloguer1);
+				lloguers.add(lloguer2);
+				lloguers.add(lloguer3);
+				String resultatEsperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n"+
+						"	Seat 600: 90.0€\n" +
+						"	Tata Vista: 180.0€\n" +
+						"	Fiat Uno: 540.0€\n" +
+						"Import a pagar: 810.0€\n" +
+						"Punts guanyats: 4\n";
+				System.out.println("Test");
+				System.out.println(resultatEsperat);
+				System.out.println("informe");
+				System.out.println(client.informe());
+				assertEquals(resultatEsperat, client.informe());
 		}
 }
