41574355e08cc8a4d8f5b7756dadd635076e7fa2
Joel <cf22joel.ghanem@iesjoandaustria.org>
2023-05-22 18:42:58 +0200

    36_02_fet

---
+++ 36_02_drop/Animal.java
@@ -0,0 +1,3 @@
+/*És tracta de la classe que defineix els objectes que manipularem a la nostra demostració. Disposa de dos camps: el nom i la categoria A banda, inclou un identificador de base de dades. Serà un valor que generarà el nostre SGBD i que ens permetrà vincular les instàncies de Animal amb la taula corresponent.*/
+public class Animal {
+}
