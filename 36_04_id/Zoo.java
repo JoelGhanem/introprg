88641239972304377a319728fc62a26167812c4c
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-23 20:27:13 +0200

    36_04_v1  ∾ prgtest autocommit 36_04 #6

--- 36_04_id/Zoo.java
+++ 36_04_id/Zoo.java
@@ -52,7 +52,7 @@ public class Zoo {
     }
     public void afegeixCategoria(Categoria categoria) throws SQLException {
         String sql = String.format(
-            "INSERT INTO CATEGORIES  VALUES (%d,'%s')",numId,
+            "INSERT INTO CATEGORIES (id,nom) VALUES (%d,'%s')",numId,
             categoria.getNom());
         numId++;
         Statement st = null;
