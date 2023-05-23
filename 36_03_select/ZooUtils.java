7b2de99e9a6fe42abf108320ea78197e7ff79cf3
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-23 19:42:02 +0200

    36_03_v1  ∾ prgtest autocommit 36_03 #8

--- 36_03_select/ZooUtils.java
+++ 36_03_select/ZooUtils.java
@@ -10,7 +10,7 @@ public class ZooUtils {
         if (categories.isEmpty()) {
             System.out.println("Cap categoria");
         } else {
-        System.out.println("Nombre de categories " + categories.size());
+        System.out.println("Nombre de categories: " + categories.size());
         }
         for (Categoria categoria : categories) {
             System.out.println(categoria);
