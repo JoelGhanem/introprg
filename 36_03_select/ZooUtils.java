16835de82f1a2b905d808a8fa21ab56213593e30
Joel <cf22joel.ghanem@iesjoandaustria.org>
2023-05-23 19:31:09 +0200

    36_03_v1

---
+++ 36_03_select/ZooUtils.java
@@ -0,0 +1,14 @@
+/*Classe en la que tenim utilitats pel Zoo com mostrarCategories*/
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+public class ZooUtils {
+    public static void mostraCategories(List<Categoria> categories) {
+        //System.out.println("Esto son las categorias");
+        for (Categoria categoria : categories) {
+            System.out.println(categoria);
+        }
+    }
+}
