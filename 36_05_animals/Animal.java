a4901db9a66eb70a157a3406cbbfc4c0a6ad67df
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-24 19:10:56 +0200

    corregit suport 36_06 ∾ prgtest autocommit 36_04 #0

---
+++ 36_05_animals/Animal.java
@@ -0,0 +1,27 @@
+/*És tracta de la classe que defineix els objectes que manipularem a la nostra demostració. Disposa de dos camps: el nom i la categoria A banda, inclou un identificador de base de dades. Serà un valor que generarà el nostre SGBD i que ens permetrà vincular les instàncies de Animal amb la taula corresponent.*/
+public class Animal {
+    private int id;
+    private String nom;
+    private Categoria categoria;
+    public Animal(String nom, Categoria categoria) throws IllegalArgumentException {
+        if(nom == null) {
+            throw new IllegalArgumentException("El nom no pot ser null ni blanc");
+        }
+        if (categoria == null) {
+            throw new IllegalArgumentException("La categoria no pot ser null");
+        }
+        this.nom = nom;
+        this.categoria = categoria;
+    }
+    public Animal(int id, String nom, categoria categoria) {
+        this.id = id;
+        this.nom = nom;
+        this.categoria = categoria;
+    }
+    //getters
+    public int getId() {if (!idIndefinit()) { return this.id;}}
+    public String getNom() {return this.nom;}
+    public Categoria getCategoria() {return this.categoria;}
+    public boolean idIndefinit() {if (id > 0) { return false;}}
+
+    }
---
+++ 36_05_animals/Zoo.java
@@ -0,0 +1,111 @@
+/*Una classe contenidor que simplement realitzarà accessos a la base de dades.*/
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Set;
+import java.sql.ResultSet;
+
+public class Zoo {
+    public int numId = 1;
+    private static final String NOM_BASE_DE_DADES = "animals.bd";
+    private static final String CADENA_DE_CONNEXIO = "jdbc:sqlite:" +
+    NOM_BASE_DE_DADES;
+    private Connection conn = null;
+    public void connecta() throws SQLException {
+        if (conn != null) return;   // ja connectat
+        conn = DriverManager.getConnection(CADENA_DE_CONNEXIO);
+    }
+    public void desconnecta() throws SQLException {
+        if (conn == null) return; // ja desconnectat
+        conn.close();
+        conn = null;
+    }
+    public void creaTaulaCategories() throws SQLException {
+        String sql = "CREATE TABLE  IF NOT EXISTS CATEGORIES (" +
+        "       id        INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "       nom       VARCHAR(40))";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    public void eliminaTaulaCategories() throws SQLException{
+        String sql = "DROP TABLE IF EXISTS CATEGORIES";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st!= null) {
+                st.close();
+            }
+        }
+    }
+    public void afegeixCategoria(Categoria categoria) throws SQLException {
+        String sql = String.format(
+            "INSERT INTO CATEGORIES (id,nom) VALUES (%d,'%s')",numId,
+            categoria.getNom());
+        categoria.setId(numId);
+        numId++;
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    public List<Categoria> recuperaCategories() throws SQLException {
+        String sql = "SELECT * FROM CATEGORIES ORDER BY nom";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            List<Categoria> categories = new LinkedList<>();
+            while (rs.next()) {
+                int bdId = rs.getInt("id");
+                String nom = rs.getString("nom");
+                Categoria categoria = new Categoria(bdId, nom);
+                categories.add(categoria);
+            }
+            rs.close();
+            return categories;
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    public Categoria obteCategoriaPerNom(String nom) throws SQLException{
+        String sql = "SELECT id,nom FROM CATEGORIES WHERE NOM = '"+nom+"' ORDER BY id LIMIT 1";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            int idC = rs.getInt("id");
+            String nomC = rs.getString("nom");
+            if (nomC == null) {
+                return null;
+            } else if (idC == 0) {
+                return null;
+            }
+            Categoria categoria = new Categoria(idC,nomC);
+            return categoria;
+        } finally {
+            if (st!=null) {
+                st.close();
+            }
+        }
+    }
+}
