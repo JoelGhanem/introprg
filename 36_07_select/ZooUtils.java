41c61cc7f27f6ab6c22e9c33e2d4ab34ce821d48
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2023-05-26 22:10:14 +0200

    prueba

---
+++ 36_07_select/ZooUtils.java
@@ -0,0 +1,19 @@
+/*Classe en la que tenim utilitats pel Zoo com mostrarCategories*/
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+public class ZooUtils {
+    public static void mostraCategories(List<Categoria> categories) {
+        //System.out.println("Esto son las categorias");
+        if (categories.isEmpty()) {
+            System.out.println("Cap categoria");
+        } else {
+        System.out.println("Nombre de categories: " + categories.size());
+        }
+        for (Categoria categoria : categories) {
+            System.out.println("\t" + categoria);
+        }
+    }
+}
