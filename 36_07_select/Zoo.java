3d65b342bca050b59101ba2b82b4f6a7c28bdde0
prgtest <cf22joel.ghanem@iesjoandaustria.org>
2023-05-29 17:00:38 +0200

    a ver ahora  ∾ prgtest autocommit 36_07 #9

--- 36_07_select/Zoo.java
+++ 36_07_select/Zoo.java
@@ -185,6 +185,7 @@ public class Zoo {
         afegeixCategoria(animal.getCategoria());
         Categoria categoria = obteCategoriaPerNom(animal.getCategoria().getNom());
         categoria.setId(categoria.getId());
+        animal.setId(numIdA);
       }
       String sql = String.format(
         "INSERT INTO ANIMALS (id,nom, categoria) VALUES (%d,'%s',%d)",
